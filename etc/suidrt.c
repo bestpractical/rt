c476ee3f568821a2f88bab8ae5280949e320b1bc
Jesse Vincent <jesse@bestpractical.com>
1998-01-08 07:48:58 +0000

    added back suidrt.c. blech.

---
+++ etc/suidrt.c
@@ -0,0 +1,43 @@
+/* Credits:  7/19/93 - Thanks to Michael G. Reed for submitting the
+ *           code to trap core dump and make this section more dynamic.
+ *
+ * This code is derived from Argonne National Labs' anlpasswd suite.
+ * 
+ * Jesse Vincent hacked at it and generalized things a bit for RT.
+ *
+ */
+#include <stdio.h>
+#include <string.h>
+
+int main (argc, argv, envp)
+int argc;
+char *argv[],*envp[];
+
+{
+  int    i;
+  char  *program_name;
+  char **eargv;
+  
+  if (program_name = strrchr (argv[0], '/'))   /* Get root program name */
+    program_name++;
+  else
+    program_name = argv[0];
+
+  if (!(eargv = (char **) malloc ((argc + 3) * sizeof (char *))))
+    {
+      fprintf (stderr, "%s: Failed to obtain memory.\n", program_name);
+      exit (1);                                  /* Trap core dump! */
+    }
+  eargv[0]= PERL;
+  eargv[1] = "-T";
+  eargv[2] = RT_PERL_MUX;
+  eargv[3] = program_name;
+
+  for (i = 1; i < argc; i++)
+    eargv[i+3] = argv[i];
+  execve(PERL, eargv, envp);
+  fprintf (stderr, "%s: Failed to launch RT program.\n", program_name);
+  perror (program_name);
+}
+
+
