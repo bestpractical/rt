ef1c9bd4b96e1d495c93e8e268d996999499da05
Jesse Vincent <jesse@bestpractical.com>
1998-04-02 10:25:27 +0000

    split mail program and mail program flags hopefully fixed make dist

--- lib/rt/support/mail.pm
+++ lib/rt/support/mail.pm
@@ -43,7 +43,7 @@ sub template_mail{
 	return("template_mail:No Recipient Specified!");
     }
 
-    open (MAIL, "|$rt::mailprog -f$rt::mail_alias");
+    open (MAIL, "|$rt::mailprog -f$rt::mail_alias $rt::mail_options");
 
     print  MAIL "Subject: [$rt::rtname \#". $in_serial_num . "] ($in_queue_id) $in_subject
 Reply-To: $rt::mail_alias
