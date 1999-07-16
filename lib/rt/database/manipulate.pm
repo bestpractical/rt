61926e325e4c88c2b4dc519d8fbbd0e15451636c
Tobias Brox <tobix@funcom.com>
1999-07-16 19:40:24 +0000

    1. Hacked a bit to allow cc and bcc to be sent upon creation of request. Only supported by rt-mailgate so far.
    2. If somebody that don't have access to reply tries to reply, it is
    set as a comment. My original idea was also to bounce or mail back a
    warning about this, but that also struck some requestors that was
    operating with several emails - and I don't want to send bounces to
    the requestors, so it's commented out as of now.
    
    3. A template correspondence-official is used for the correspondence
    that goes to the requestor. This way it's possible to differ the
    template to the requestor(s) from the internal templates, and also
    avoid sending the headers to the requestors.
    
    4. Changed an error message for debugging purposes.
    
    5. Changed the kill-sub to not kill anymore. Read the comments why.

--- lib/rt/database/manipulate.pm
+++ lib/rt/database/manipulate.pm
@@ -22,6 +22,8 @@ sub add_new_request {
     my $in_date_due  = shift;
     my $in_content = shift;
     my $in_current_user = shift;
+    my $in_cc = shift;
+    my $in_bcc= shift;
 
     my $msg;
         
@@ -34,6 +36,11 @@ sub add_new_request {
     ($in_requestors,$msg) = &norm_requestors($in_requestors);
     return (0,0,$msg) if $msg;
     
+    # This should be handled better.
+    if ($in_cc) {
+      $content="Cc: $in_cc\n$content";
+    }
+    
     #add the fact to each_req    
     $serial_num=&add_request($in_queue_id, $in_area, $in_requestors, $in_alias, $in_owner, $in_subject, $in_final_priority, $in_priority, $in_status, $in_date_created, $in_date_told, $in_date_due, $in_current_user);
     
@@ -41,7 +48,9 @@ sub add_new_request {
     $transaction_num=&add_transaction($serial_num, $in_current_user, 'create','',$in_content,$time,1,$in_current_user);
 
     if ($queues{$in_queue_id}{m_members_correspond}) {
-	&rt::template_mail ('correspondence',$in_queue_id,"$queues{$in_queue_id}{dist_list}","","", "$serial_num" ,"$transaction_num","$in_subject", "$in_current_user",'');
+      &rt::template_mail ('correspondence',$in_queue_id,"$queues{$in_queue_id}{dist_list}","$in_cc","$in_bcc", "$serial_num" ,"$transaction_num","$in_subject", "$in_current_user",'');
+    } elsif ($in_cc || $in_bcc) {
+      &rt::template_mail ('correspondence',$in_queue_id,"","$in_cc","$in_bcc", "$serial_num" ,"$transaction_num","$in_subject", "$in_current_user",'');
     }
 
     if ( $queues{$in_queue_id}{m_user_create}) {
@@ -133,17 +142,35 @@ sub add_correspondence {
     my $in_notify = shift; # if we want to update the "user notified" field
                            # make this a 1.
     my $in_current_user = shift;
-    my ($transaction_num,$requestors);
+    my ($transaction_num,$requestors,$isnotrequestor);
     
-    # is there a reason we might want to restrict comment access? I'd just as soon let
-    # anybody comment on things
-    
-    #  if (!(&can_manipulate_request($in_serial_num,$in_current_user))) {
-    #	return (0,"You don't have permission to modify request \#$in_serial_num");
-    #   }
-
-
     &req_in($in_serial_num, '_rt_system');
+    
+    $isnotrequestor=&is_not_a_requestor($in_current_user,$in_serial_num);
+
+    # Those RT comments that are inserted into the content should also
+    # have been handled more elegant.
+    # Everybody can comment things, but only the support personell should send official replies:
+    if ($isnotrequestor && !&can_manipulate_request($in_serial_num,$in_current_user)) { 
+
+	my ($resc, $ress)=&comment
+	    ( $in_serial_num, 
+	     "(Reply NOT sent to requestor due to access restrictions)\n\n$in_content", 
+	     $in_subject, $in_cc, $in_bcc, $in_current_user);
+
+	# If you want to bounce back an error, then replace $resc to 1
+	# below. Be warned this will also bounce to requestors if they
+	# changes their email during the progress.
+	return 
+	    (
+	     $resc,
+"No permission to reply to \#$in_serial_num - your mail is recorded as a comment.\n$ress"
+	     );
+    }
+    if (!$isnotrequestor) {
+	$in_content = "(inbound)\n\n$in_content";
+    }
+
     $requestors=$rt::req[$in_serial_num]{'requestors'};
 
     $queue_id=$rt::req[$in_serial_num]{queue_id};
@@ -161,9 +188,9 @@ sub add_correspondence {
     }
     
     #if it's coming from somebody other than the user, send them a copy
-    if ( (&is_not_a_requestor($in_current_user,$in_serial_num))) {
+    if ($isnotrequestor) {
 	    &update_each_req($in_serial_num, 'date_told', $rt::time);
-	    $tem=&rt::template_mail('correspondence', $queue_id, "$requestors", $in_cc, $in_bcc, "$in_serial_num", "$transaction_num", "$in_subject", "$in_current_user",'');
+	    $tem=&rt::template_mail('correspondence-official', $queue_id, "$requestors", $in_cc, $in_bcc, "$in_serial_num", "$transaction_num", "$in_subject", "$in_current_user",'');
     }
     
     if ($queues{$queue_id}{m_members_correspond}) {
@@ -227,7 +254,7 @@ sub resolve {
     my  ($in_serial_num, $in_current_user) = @_;
     my ($transaction_num);
     if (!(&can_manipulate_request($in_serial_num,$in_current_user))) {
-	return (0,"You don't have permission to modify request \#$in_serial_num");
+	return (0,"You ($in_current_user) don't have permission to modify request \#$in_serial_num");
     }
  
     $transaction_num=&update_request($in_serial_num,'status', 'resolved', $in_current_user);
@@ -267,6 +294,13 @@ sub kill {
  
 
     ($transaction_count)=&transaction_history_in($in_serial_num,$in_current_user);
+
+    # This is not working at my place. Perhaps it would be smarter to
+    # use 'localtime' than 'parse_time' below? I'm commenting it out
+    # even though - I think it's stupid deleting the transactions
+    # without deleting from each_req
+
+    if (0) { 
     for ($counter=0;$counter<$transaction_count;$counter++) {
 	($weekday, $month, $monthday, $hour, $min, $sec, $TZ, $year)=&parse_time($rt::req[$in_serial_num]{'trans'}[$counter]{'time'});
 	$filename="$transaction_dir/$year/$month/$monthday/$in_serial_num.$transaction[$counter]{'id'}";
@@ -274,16 +308,26 @@ sub kill {
 	if (-f $filename) {
 	    unlink($filename);
 	}
-	$query_string = "DELETE from transactions where effective_sn = $in_serial_num";
-	
-	
-	$dbh->Query($query_string) or warn "Query had some problem: $Mysql::db_errstr\n";;
+      }
+  }
+
+# I would consider deleting the DB content without deleting the files
+# as a bug, so I've commented out those. Perhaps some
+# "trash-can"-functionality to clear up deleted requests (files +
+# each_req + transactions) had been smart. It would be sort of cool
+# with some 'tmpwatch' functionality in crontab, where all dead
+# requests that hasn't been accessed for one month gets killed. Or
+# what do you think?
+#	$query_string = "DELETE from transactions where effective_sn = $in_serial_num";
+#	$dbh->Query($query_string) or warn "Query had some problem: $Mysql::db_errstr\n";;
 	
     }
     $transaction_num=&update_request($in_serial_num,'status','dead', $in_current_user);    
     return ($transaction_num,"Request #$in_serial_num has been killed.");
 
 }
+
+
 sub merge {
     my  ($in_serial_num, $in_merge_into, $in_current_user) = @_;
     my ($new_requestors, $old_requestors, @requestors_list, $user); 
@@ -331,7 +375,6 @@ sub merge {
     return ($transaction_num,"Request #$in_serial_num has been merged into request #$in_merge_into.");
 
 }
-
 
 sub change_queue {
     my  ($in_serial_num, $in_queue, $in_current_user) = @_;
