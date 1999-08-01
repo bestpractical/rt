c31b44746784cf11dee516e2c7878a738361c9bf
Jesse Vincent <jesse@bestpractical.com>
1999-08-01 20:23:13 +0000

    Added the start of the Transaction object. don't even pretend to try to use it.

---
+++ lib/rt/Transaction.pm
@@ -0,0 +1,59 @@
+package rt::Transaction;
+@ISA= qw(DBIx::RecordObject;
+
+sub new {
+  my $proto = shift;
+  my $class = ref($proto) || $proto;
+  my $self  = {};
+  bless ($self, $class);
+
+  $self->{'table'} = "transactions";
+  $self->{'user'} = shift;
+  return $self;
+}
+
+
+#take simple args and call MKIA::Database::Record to do the real work.
+sub create {
+  my $self = shift;
+
+  my %args = ( id => '',
+	       effective_sn => '',
+	       serial_num => '',
+               actor => '',
+               type => '',
+	       trans_data => '',
+	       trans_date => '',
+	       content => '',
+	       @_
+	     );
+  return (0) if (! $args{'article'});
+  my $id = $self->SUPER::create(article => $args{'article'},
+				url => $args{'url'},
+				title => $args{'title'},
+				comment => $args{'comment'});
+  return ($id);
+}
+
+
+
+#Table specific data accessors/ modifiers
+sub title {
+  my $self = shift;
+  return($self->_set_and_return('title',@_));
+}
+
+sub url {
+  my $self = shift;
+  return($self->_set_and_return('url',@_));
+}
+sub comment {
+  my $self = shift;
+  return($self->_set_and_return('comment',@_));
+}
+
+sub article {
+  my $self=shift;
+  return($self->_set_and_return('article',@_));
+}
+1;
