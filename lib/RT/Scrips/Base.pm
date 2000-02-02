4aa3849ef08c6919477ee81d86662702ea961af0
Jesse Vincent <jesse@bestpractical.com>
2000-02-02 13:05:28 +0000

    Added a baseclass for all sorts of scrips.
    it's got the default new and init methods. we'll probably need
    to add some sort of per-class init method.x

---
+++ lib/RT/Scrips/Base.pm
@@ -0,0 +1,81 @@
+# $Header$
+
+package RT::Scrips::Base;
+
+
+
+sub new {
+  my $proto = shift;
+  my $class = ref($proto) || $proto;
+  my $self  = {};
+  bless ($self, $class);
+  $self->_Init;
+  return $self;
+}
+
+sub _Init {
+  my $self = shift;
+  my %args = ( Transaction => undef,
+	       Ticket => undef,
+	       Template => undef,
+	       Argument => undef,
+	       Type => undef);
+  
+  
+  $self->{'Argument'} = $args{'Argument'};
+  $self->{'Ticket'} = $args{'Ticket'};
+  $self->{'Transaction'} = $args{'Transaction'};
+  $self->{'Template'} = $args{'Template'};
+  $self->{'Type'} = $args{'Type'};
+}
+
+#Access Scripwide data
+sub Argument {
+  my $self = shift;
+  return($self->{'Argument'});
+}
+sub Ticket {
+  my $self = shift;
+  return($self->{'Ticket'});
+}
+sub Transaction {
+  my $self = shift;
+  return($self->{'Transaction'});
+}
+sub Template {
+  my $self = shift;
+  return($self->{'Template'});
+}
+
+sub Type {
+  my $self = shift;
+  return($self->{'Type'});
+}
+
+
+
+#
+# Scrip methods
+#
+
+
+#Do what we need to do and send it out.
+sub Commit {
+  my $self = shift;
+  return(0,"Commit Stubbed");
+}
+
+
+#What does this type of Scrip do
+sub Describe {
+  my $self = shift;
+  return (1, "This is a baseclass for a Scrip.");
+}
+
+#Parse the templates, get things ready to go.
+sub Prepare {
+  my $self = shift;
+  return (0,"Prepare Stubbed");
+}
+
+
