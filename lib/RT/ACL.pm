e6d884da37633a1a5cc318b5c6cbfc3e711def8f
Jesse Vincent <jesse@bestpractical.com>
2000-04-30 21:50:03 +0000

    Initial sketches at RT::ACL.pm. there's no running code here yet.

--- lib/RT/ACL.pm
+++ lib/RT/ACL.pm
@@ -1,6 +1,12 @@
-package RT::Articles;
+# $Header$
+# Distributed under the terms of the GNU GPL
+# Copyright (c) 2000 Jesse Vincent <jesse@fsck.com>
+
+package RT::ACL;
 use DBIx::EasySearch;
 @ISA= qw(DBIx::EasySearch);
+
+
 
 
 # {{{ sub new 
@@ -24,14 +30,121 @@ sub Limit  {
 }
 # }}}
 
+# {{{ sub LimitToQueueACL
+
+sub LimitToQueueACL {
+}
+
+# }}}
+
+
+sub LimitPrincipals {
+  my $self = shift;
+  my $user = shift;
+  my $ticket = shift;
+
+  
+  
+  
+  $self->LimitPrinicpalsToUser($user);
+  $self->LimitPrincipalsToWatchers($user);
+  if (defined $self->Ticket) {
+    $self->LimitPrincipalsToOwner($user, $ticket);
+  }
+  
+}
+
+# {{{ sub LimitPrinicpalsToUser 
+
+sub LimitPrincipalsToUser {
+  my $self = shift;
+  my $user = shift;
+  
+  
+  return (" ( ACE.PrincipalScope = 'User') AND 
+	    ( ACE.PrincipalId = $user OR ACE.PrincipalId = 0))");
+}
+
+# }}}
+
+# {{{ sub LimitPrincipalsToOwner
+sub LimitPrincipalsToOwner {
+  my $self = shift;
+  my $user = shift;
+  my $ticket = shift
+  return (" ( ACE.PrinciaplScope = 'Owner') AND ( Tickets.Owner = $user ) AND ( Tickets.Id = $ticket)");
+}
+# }}}
+
+# {{{ sub LimitPrincipalsToWatchers
+sub LimitPrincipalsToWatchers {
+  my $self = shift;
+  my $user = shift;
+return ("( ACE.PrincipalScope = Watchers.Scope ) AND 
+         ( ACE.PrincipalType = Watchers.Type ) AND 
+         ( ACL.PrincipalId = Watchers.Value ) AND 
+	 ( Watchers.Owner = $User )");
+}
+
+# }}}
+
+# {{{ sub LimitToQueueObjects 
+sub LimitToQueueObjects {
+  my $self = shift;
+  my $queue = shift if (@_);
+  
+  $QueueObject = "(ACE.ObjectType = 'Queue') and ( ACE.ObjectId = 0";
+  if (defined $queue) {
+    $QueueObject .= "OR ACE.ObjectId = ".$self->quote($queue) ;
+  }
+  
+  $QueueObject .= ")";
+ 
+  return ("($QueueObject)");
+}
+
+# }}}
+
+# This select statement would figure out if A user has $Right at the queue level
+
+SELECT ACE.id from ACE, Watchers, Tickets WHERE ( 
+	     $QueueObject
+	     AND ( ACE.Right = $Right) 
+	     AND ($Principals))
+
+# This select statement would figure outif a user has $Right for the "System"
+
+SELECT ACE.id from ACE, Watchers, Tickets WHERE ( 
+	     ($SystemObject) AND ( ACE.Right = $Right ) AND ($Principals))
+
+
+# {{{ sub LimitToRight 
+sub LimitToRight {
+  my $self = shift;
+  my $right = shift;
+  my $RightClause = "(ACE.Right = ."$self->quote($right)".)";
+}  
+# }}}
+
+# {{{ sub LimitToSystemObjects
+sub LimitToSystemObjects {
+  my $self = shift;
+  
+  $SystemObject = "( ACE.ObjectType = 'System' )";
+
+}
+
+# }}}
+
 # {{{ sub NewItem 
 sub NewItem  {
   my $self = shift;
   my $Handle = shift;
   my $item;
-  $item = new RT::ACE($self->{'user'}, $Handle);
+  $item = new RT::ACE($self->CurrentUser, $Handle);
   return($item);
 }
 # }}}
-  1;
+
+1;
 
