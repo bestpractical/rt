c5c1783f068aa98c1ee9b38afb4637cd7a4d0295
Tobias Brox <tobix@funcom.com>
2000-07-20 10:11:01 +0000

    I'm considering to move things from Display.html and ProcessUpdate.html to Web.pm.  Comments?

---
+++ lib/RT/Interface/Web.pm
@@ -0,0 +1,8 @@
+# $Header$
+# Copyright 2000 Tobias Brox <tobix@fsck.com>
+# Request Tracker is Copyright 1996-2000 Jesse Vincent <jesse@fsck.com>
+
+# This is a library of static subs to be used by the Mason web
+# interface to RT, and to be used by webrt.cgi / webmux.pl.
+
+
