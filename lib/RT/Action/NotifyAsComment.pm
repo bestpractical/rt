08bdea00ce1afa1168791906ebf67f1a8ac6ace4
Jesse Vincent <jesse@bestpractical.com>
2001-03-21 07:21:10 +0000

    NotifyAsComment should now DTRT and send mail from the comments email address

--- lib/RT/Action/NotifyAsComment.pm
+++ lib/RT/Action/NotifyAsComment.pm
@@ -1,6 +1,24 @@
+#$Header$
+
 package RT::Action::NotifyAsComment;
 require RT::Action::Notify;
 @ISA = qw(RT::Action::Notify);
 
+
+=head2 Prepare
+
+Tell SendEmail that this message should come out as a comment. Then call SUPER::Prepare
+
+=cut
+
+sub Prepare {
+	my $self = shift;
+	
+	#TODO: this should be cleaner.
+	#Tell RT::Action::SendEmail that this should come from the relevant comment email address.
+	$self->{'comment'} = 1;
+	
+	return($self->SUPER::Prepare(@_));	
+}
 1;
 
