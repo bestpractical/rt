3e8783dfd501058b161738824f8c27f2144d878b
Jesse Vincent <jesse@bestpractical.com>
2002-04-28 20:39:11 +0000

    Fixed a tiny typo

--- lib/RT.pm
+++ lib/RT.pm
@@ -64,7 +64,7 @@ sub InitLogging {
 			 min_level=> $RT::LogToFile,
 			 filename=> $filename,
 			 mode=>'append',
-            callbacks => sub {my %p=@_; return ."[".gmtime(time)."] [".$p{level}."]: $p{message}\n"}
+            callbacks => sub {my %p=@_; return "[".gmtime(time)."] [".$p{level}."]: $p{message}\n"}
 
 		       ));
     }
