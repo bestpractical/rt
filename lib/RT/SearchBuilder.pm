93bc31a412a1e3359959da08c805b201919a3bc0
Audrey Tang <audreyt@audreyt.org>
2004-04-26 19:02:48 +0000

    correct EMPTY and NULL support for $Tickets->LimitAttribute().
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/branches/rt-3.3@745 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/SearchBuilder.pm
+++ lib/RT/SearchBuilder.pm
@@ -109,14 +109,42 @@ sub LimitToDeleted {
 Takes NAME, OPERATOR and VALUE to find records that has the
 matching Attribute.
 
+If EMPTY is set, also select rows with an empty string as
+Attribute's Content.
+
+If NULL is set, also select rows without the named Attribute.
+
 =cut
+
+my %Negate = qw(
+    =		!=
+    !=		=
+    >		<=
+    <		>=
+    >=		<
+    <=		>
+    LIKE	NOT LIKE
+    NOT LIKE	LIKE
+    IS		IS NOT
+    IS NOT	IS
+);
 
 sub LimitAttribute {
     my ($self, %args) = @_;
+    my $clause = 'ALIAS';
+    my $operator = ($args{OPERATOR} || '=');
+    
+    if ($args{NULL} and exists $args{VALUE}) {
+	$clause = 'LEFTJOIN';
+	$operator = $Negate{$operator};
+    }
+    elsif ($args{NEGATE}) {
+	$operator = $Negate{$operator};
+    }
     
     my $alias = $self->Join(
 	TYPE   => 'left',
-	ALIAS1 => 'main',
+	ALIAS1 => $args{ALIAS} || 'main',
 	FIELD1 => 'id',
 	TABLE2 => 'Attributes',
 	FIELD2 => 'ObjectId'
@@ -126,13 +154,13 @@ sub LimitAttribute {
     $type =~ s/(?:s|Collection)$//; # XXX - Hack!
 
     $self->Limit(
-	ALIAS	   => $alias,
+	$clause	   => $alias,
 	FIELD      => 'ObjectType',
 	OPERATOR   => '=',
 	VALUE      => $type,
     );
     $self->Limit(
-	ALIAS	   => $alias,
+	$clause	   => $alias,
 	FIELD      => 'Name',
 	OPERATOR   => '=',
 	VALUE      => $args{NAME},
@@ -141,23 +169,29 @@ sub LimitAttribute {
     return unless exists $args{VALUE};
 
     $self->Limit(
-	ALIAS	   => $alias,
+	$clause	   => $alias,
 	FIELD      => 'Content',
-	OPERATOR   => ($args{OPERATOR} || '='),
+	OPERATOR   => $operator,
 	VALUE      => $args{VALUE},
-	ENTRYAGGREGATOR => 'OR',
     );
 
-    if ($args{EMPTY}) {
-	# Capture rows without the attribute defined by testing IS NULL.
-	$self->Limit(
-	    ALIAS      => $alias,
-	    FIELD      => $_,
-	    OPERATOR   => 'IS',
-	    VALUE      => 'NULL',
-	    ENTRYAGGREGATOR => 'OR',
-	) for qw( ObjectType Name Content );
-    }
+    # Capture rows with the attribute defined as an empty string.
+    $self->Limit(
+	$clause    => $alias,
+	FIELD      => 'Content',
+	OPERATOR   => '=',
+	VALUE      => '',
+	ENTRYAGGREGATOR => $args{NULL} ? 'AND' : 'OR',
+    ) if $args{EMPTY};
+
+    # Capture rows without the attribute defined
+    $self->Limit(
+	%args,
+	ALIAS      => $alias,
+	FIELD	   => 'id',
+	OPERATOR   => ($args{NEGATE} ? 'IS NOT' : 'IS'),
+	VALUE      => 'NULL',
+    ) if $args{NULL};
 }
 # }}}
 
