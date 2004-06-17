b23afa2f0ba73ee55c9be7e703a4d0d1e31d89ba
Linda Julien <leira@bestpractical.com>
2004-06-17 18:10:41 +0000

    RT-Ticket: 4355 RT-Status: resolved RT-Action: comment
    Added new "On Priority Change" condition and "Notify Ccs" and "NotifyCcs as Comment" action.
    
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/branches/rt-3.1@1102 e417ac7c-1bcc-0310-8ffa-8f5827389a85

---
+++ lib/RT/Condition/PriorityChange.pm
@@ -0,0 +1,58 @@
+# BEGIN LICENSE BLOCK
+# 
+# Copyright (c) 1996-2003 Jesse Vincent <jesse@bestpractical.com>
+# 
+# (Except where explictly superceded by other copyright notices)
+# 
+# This work is made available to you under the terms of Version 2 of
+# the GNU General Public License. A copy of that license should have
+# been provided with this software, but in any event can be snarfed
+# from www.gnu.org.
+# 
+# This work is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+# 
+# Unless otherwise specified, all modifications, corrections or
+# extensions to this work which alter its source code become the
+# property of Best Practical Solutions, LLC when submitted for
+# inclusion in the work.
+# 
+# 
+# END LICENSE BLOCK
+ 
+
+
+package RT::Condition::PriorityChange;
+require RT::Condition::Generic;
+
+use strict;
+use vars qw/@ISA/;
+@ISA = qw(RT::Condition::Generic);
+
+
+=head2 IsApplicable
+
+If the argument passed in is equivalent to the new value of
+the Priority Obj
+
+=cut
+
+sub IsApplicable {
+    my $self = shift;
+    if ($self->TransactionObj->Field eq 'Priority') {
+	return(1);
+    } 
+    else {
+	return(undef);
+    }
+}
+
+eval "require RT::Condition::PriorityChange_Vendor";
+die $@ if ($@ && $@ !~ qr{^Can't locate RT/Condition/PriorityChange_Vendor.pm});
+eval "require RT::Condition::PriorityChange_Local";
+die $@ if ($@ && $@ !~ qr{^Can't locate RT/Condition/PriorityChange_Local.pm});
+
+1;
+
