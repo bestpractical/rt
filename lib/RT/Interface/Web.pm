bdb6ba15b961ac675d15970cde97c2af365bd217
Audrey Tang <audreyt@audreyt.org>
2004-06-26 20:38:48 +0000

    backport utf8 fixes for TicketSQL strings from 3.3
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/branches/rt-3.1@1151 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/Interface/Web.pm
+++ lib/RT/Interface/Web.pm
@@ -112,7 +112,8 @@ does a css-busting but minimalist escaping of whatever html you're passing in.
 
 sub EscapeUTF8  {
         my  $ref = shift;
-        my $val = (Encode::is_utf8($$ref) ? Encode::encode_utf8($$ref) : $$ref);                                                                                                                                
+        my $val = $$ref;
+        use bytes;
         $val =~ s/&/&#38;/g;
         $val =~ s/</&lt;/g; 
         $val =~ s/>/&gt;/g;
@@ -121,6 +122,8 @@ sub EscapeUTF8  {
         $val =~ s/"/&#34;/g;
         $val =~ s/'/&#39;/g;
         $$ref = $val;
+        Encode::_utf8_on($$ref);
+
 
 }
 
