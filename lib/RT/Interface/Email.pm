2746f8cb51564d954cb8bf5df5761b79c9b08aca
Jesse Vincent <jesse@bestpractical.com>
2004-11-16 16:28:35 +0000

    RT-Ticket: 6239 RT-Status: resolved RT-Update: correspond
    Applied patch from Rudolph Pereira to allow email plugins to change ticket ID
    
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/branches/3.2-RELEASE@1909 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/Interface/Email.pm
+++ lib/RT/Interface/Email.pm
@@ -662,7 +662,7 @@ EOT
     my $Ticket = RT::Ticket->new($CurrentUser);
 
     # {{{ If we don't have a ticket Id, we're creating a new ticket
-    if ( !$args{'ticket'} ) {
+    if ( !$SystemTicket || !$SystemTicket->Id) {
 
         # {{{ Create a new ticket
 
