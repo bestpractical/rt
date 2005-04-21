89fccbcc97ba5d41f36ee1494511383b35998066
Jesse Vincent <jesse@bestpractical.com>
2005-04-21 05:38:19 +0000

    Bugfixes, now charts by owner work
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/branches/QUEBEC-EXPERIMENTAL@2797 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/Report/Tickets.pm
+++ lib/RT/Report/Tickets.pm
@@ -3,6 +3,19 @@ use base qw/RT::Tickets/;
 use RT::Report::Tickets::Entry;
 
 
+# Override the AddRecord from DBI::SearchBuilder::Unique. id isn't id here
+# wedon't want to disambiguate all the items with a count of 1.
+sub AddRecord {
+    my $self = shift;
+    my $record = shift;
+    push @{$self->{'items'}}, $record;
+    $self->{'rows'}++;
+}
+
+1;
+
+
+
 # Gotta skip over RT::Tickets->Next, since it does all sorts of crazy magic we 
 # don't want.
 sub Next {
