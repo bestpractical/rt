0f9df3a63707c17ef8ea5dfb45cceec15464ae22
Thomas Sibley <trs@bestpractical.com>
2005-07-06 22:44:22 +0000

    JS improvements and better TitleBox rollup/down
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/branches/3.5-TESTING@3406 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- html/NoAuth/js/util.js
+++ html/NoAuth/js/util.js
@@ -1,18 +1,59 @@
 % $r->content_type('application/x-javascript');
 
+function rollup(link, id) {
+    var e = document.getElementById(id);
+    
+    if (e.className.match(/\bhidden\b/)) {
+        show(e);
+        link.className = link.className.replace(/\s?\brolled-up\b/, '');
+    }
+    else {
+        hide(e);
+        if (link.className)
+            link.className += ' rolled-up';
+        else
+            link.className = 'rolled-up';
+    }
+    link.focus(0);
+    return false;
+}
+
 function hideshow(id) {
-    e = document.getElementById(id);
+    var e = document.getElementById(id);
     
     if (e.className.match(/\bhidden\b/))
-        e.className = e.className.replace(/\s?\bhidden\b/, '');
-    else {
-        if (e.className)
-            e.className += ' hidden';
-        else
-            e.className = 'hidden';
-    }
+        show(e);
+    else
+        hide(e);
+
     return false;
-}   
+}
+
+function show(e) {
+    e.className = e.className.replace(/\s?\bhidden\b/, '');
+}
+
+function hide(e) {    
+    if (e.className)
+        e.className += ' hidden';
+    else
+        e.className = 'hidden';
+}
+
+function switchVisibility(id1, id2) {
+    // Show both and then hide the one we want
+    show(document.getElementById(id1));
+    show(document.getElementById(id2));
+    
+    hide(document.getElementById(id2));
+    
+    return false;
+}
+
+function setFocus(id) {
+    var tmp = (document.getElementsByName(id));
+    if (tmp.length > 0) tmp[tmp.length-1].focus();
+}
 
 function openCalWindow(field) {
     var objWindow = window.open('<%$RT::WebPath%>/CalPopup.html?field='+field, 'Pick', 'height=400,width=400,scrollbars=1');
