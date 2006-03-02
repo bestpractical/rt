a7936fafbaadaadba4b6e153242ccdcbded604ef
Ruslan Zakirov <ruz@bestpractical.com>
2006-03-02 19:11:17 +0000

    return back changes that was backed out by wrong merge
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/branches/3.7-EXPERIMENTAL@4657 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/Date.pm
+++ lib/RT/Date.pm
@@ -655,12 +655,13 @@ sub ISO {
     my $self = shift;
     my %args = ( Date => 1,
                  Time => 1,
-                 Timezone => 'GMT',
+                 Timezone => '',
                  Seconds => 1,
                  @_,
                );
        #  0    1    2     3     4    5     6     7      8      9
-    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$ydaym,$isdst,$offset) = $self->Localtime($args{'Timezone'});
+    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$ydaym,$isdst,$offset) =
+                            $self->Localtime($args{'Timezone'});
 
     #the month needs incrementing, as gmtime returns 0-11
     $mon++;
@@ -800,17 +801,9 @@ sub Timezone {
     my $self = shift;
     my $context = lc(shift);
 
-
-
-
-
-
-
-
-    $context = 'utc' unless $context =~ /^(?:utc|server|user)$/;
+    $context = 'utc' unless $context =~ /^(?:utc|server|user)$/i;
 
     my $tz;
-
     if( $context eq 'user' ) {
         $tz = $self->CurrentUser->UserObj->Timezone;
     } elsif( $context eq 'server') {
