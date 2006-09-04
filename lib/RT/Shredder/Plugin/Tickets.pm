6817e16d599b4464b8971cbcf07ec8911c912b0d
Ruslan Zakirov <ruz@bestpractical.com>
2006-09-04 00:42:32 +0000

    ::Plugin::Tickets add option 'query' drop options queue, status and updated_before
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/branches/3.7-EXPERIMENTAL@5844 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/Shredder/Plugin/Tickets.pm
+++ lib/RT/Shredder/Plugin/Tickets.pm
@@ -10,47 +10,35 @@ RT::Shredder::Plugin::Tickets - search plugin for wiping tickets.
 
 =head1 ARGUMENTS
 
-=head2 queue - queue name
+=head2 query - query string
 
-Search tickets only in particular queue.
+Search tickets with query string.
+Examples:
+  Queue = 'my queue' AND ( Status = 'deleted' OR Status = 'rejected' )
+  LastUpdated < '2003-12-31 23:59:59'
 
-=head2 status - ticket status
+B<Hint:> You can construct query with the query builder in RT's web
+interface and then open advanced page and copy query string.
 
-Search tickets with specified status only.
-'deleted' status is also supported.
-
-=head2 updated_before - date
-
-Search tickets that were updated before some date.
-Example: '2003-12-31 23:59:59'
+Arguments C<queue>, C<status> and C<updated_before> have been dropped
+as you can easy make the same search with the C<query> option.
+See examples above.
 
 =cut
 
-sub SupportArgs { return $_[0]->SUPER::SupportArgs, qw(queue status updated_before) }
+sub SupportArgs { return $_[0]->SUPER::SupportArgs, qw(query) }
 
 sub TestArgs
 {
     my $self = shift;
     my %args = @_;
     my $queue;
-    if( $args{'queue'} ) {
-        $queue = RT::Queue->new( $RT::SystemUser );
-        $queue->Load( $args{'queue'} );
-        return( 0, "Couldn't load queue '$args{'queue'}'" ) unless $queue->id;
-        $args{'queue'} = $queue->id;
-    }
-    if( $args{'status'} ) {
-        $queue ||= RT::Queue->new( $RT::SystemUser );
-        my @statuses = qw(new open stalled deleted rejected);
-        @statuses = $queue->StatusArray if $queue->can('StatusArray');
-        unless( grep lc $_ eq lc $args{'status'}, @statuses ) {
-            return( 0, "Invalid status '$args{status}'" );
-        }
-    }
-    if( $args{'updated_before'} ) {
-        unless( $args{'updated_before'} =~ /\d\d\d\d-\d\d-\d\d(?:\s\d\d:\d\d:\d\d)?/ ) {
-            return( 0, "Invalid date '$args{updated_before}'" );
-        }
+    if( $args{'query'} ) {
+        my $objs = RT::Tickets->new( $RT::SystemUser );
+        $objs->{'allow_deleted_search'} = 1;
+        my ($status, $msg) = $queue->FromSQL( $args{'query'} );
+        return( 0, "Bad query argument, error: $msg" ) unless $status;
+        $args{'query'} = $objs;
     }
     return $self->SUPER::TestArgs( %args );
 }
@@ -58,20 +46,8 @@ sub TestArgs
 sub Run
 {
     my $self = shift;
-    my $objs = RT::Tickets->new( $RT::SystemUser );
-    $objs->{'allow_deleted_search'} = 1;
-    if( $self->{'opt'}{'status'} ) {
-        $objs->LimitStatus( VALUE => $self->{'opt'}{'status'} );
-    }
-    if( $self->{'opt'}{'queue'} ) {
-        $objs->LimitQueue( VALUE => $self->{'opt'}{'queue'} );
-    }
-    if( $self->{'opt'}{'updated_before'} ) {
-        $objs->LimitLastUpdated(
-            OPERATOR => '<',
-            VALUE => $self->{'opt'}{'updated_before'},
-        );
-    }
+    my $objs = $self->{'opt'}{'query'}
+        or return (1, undef);
     if( $self->{'opt'}{'limit'} ) {
         $objs->RowsPerPage( $self->{'opt'}{'limit'} );
     }
