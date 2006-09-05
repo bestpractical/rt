a63488f9e9a29be6a891c7830800910acc680ce8
Ruslan Zakirov <ruz@bestpractical.com>
2006-09-05 16:59:11 +0000

    ::Shredder::PLugin::Tickets add 'with_linked' option add 'apply_query_to_linked' option tests
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/branches/3.7-EXPERIMENTAL@5864 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/Shredder/Plugin/Tickets.pm
+++ lib/RT/Shredder/Plugin/Tickets.pm
@@ -24,9 +24,19 @@ Arguments C<queue>, C<status> and C<updated_before> have been dropped
 as you can easy make the same search with the C<query> option.
 See examples above.
 
+=head2 with_linked - boolen
+
+Deletes all tickets that are linked to tickets that match L<query>.
+
+=head2 apply_query_to_linked - boolean
+
+Delete linked tickets only if those too match L<query>.
+
+See also L<with_linked>.
+
 =cut
 
-sub SupportArgs { return $_[0]->SUPER::SupportArgs, qw(query) }
+sub SupportArgs { return $_[0]->SUPER::SupportArgs, qw(query with_linked apply_query_to_linked) }
 
 sub TestArgs
 {
@@ -38,21 +48,58 @@ sub TestArgs
         $objs->{'allow_deleted_search'} = 1;
         my ($status, $msg) = $objs->FromSQL( $args{'query'} );
         return( 0, "Bad query argument, error: $msg" ) unless $status;
-        $args{'query'} = $objs;
+        $self->{'opt'}{'objects'} = $objs;
     }
+    $args{'with_linked'} = 1 if $args{'apply_query_to_linked'};
+
     return $self->SUPER::TestArgs( %args );
 }
 
 sub Run
 {
     my $self = shift;
-    my $objs = $self->{'opt'}{'query'}
+    my $objs = $self->{'opt'}{'objects'}
         or return (1, undef);
-    if( $self->{'opt'}{'limit'} ) {
-        $objs->RowsPerPage( $self->{'opt'}{'limit'} );
-    }
+
     $objs->OrderByCols( { FIELD => 'id', ORDER => 'ASC' } );
-    return (1, $objs);
+    unless ( $self->{'opt'}{'with_linked'} ) {
+        if( $self->{'opt'}{'limit'} ) {
+            $objs->RowsPerPage( $self->{'opt'}{'limit'} );
+        }
+        return (1, $objs);
+    }
+
+    my (@top, @linked, %seen);
+    $self->FetchNext($objs, 1);
+    while ( my $obj = $self->FetchNext( $objs ) ) {
+        next if $seen{ $obj->id }++;
+        push @linked, $self->GetLinked( Object => $obj, Seen => \%seen );
+        push @top, $obj;
+        last if $self->{'opt'}{'limit'}
+                && @top >= $self->{'opt'}{'limit'};
+    }
+    return (1, @top, @linked);
+}
+
+sub GetLinked
+{
+    my $self = shift;
+    my %arg = @_;
+    my @res = ();
+    my $query = 'Linked = '. $arg{'Object'}->id;
+    if ( $self->{'opt'}{'apply_query_to_linked'} ) {
+        $query .= " AND ( ". $self->{'opt'}{'query'} ." )";
+    }
+    my $objs = RT::Tickets->new( $RT::SystemUser );
+    $objs->{'allow_deleted_search'} = 1;
+    $objs->FromSQL( $query );
+    $self->FetchNext( $objs, 1 );
+    while ( my $linked_obj = $self->FetchNext( $objs ) ) {
+        next if $arg{'Seen'}->{ $linked_obj->id }++;
+        push @res, $self->GetLinked( %arg, Object => $linked_obj );
+        push @res, $linked_obj;
+    }
+    return @res;
 }
 
 1;
