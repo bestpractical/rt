86b8c61322c2ce42c5168c1836c25fef74263502
Ruslan Zakirov <ruz@bestpractical.com>
2006-11-24 12:42:09 +0000

    Scrip management error messages preserve values of the fields between calls correctly report id in the tab fix #7445
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/branches/3.6-RELEASE@6513 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/Scrip_Overlay.pm
+++ lib/RT/Scrip_Overlay.pm
@@ -139,10 +139,10 @@ sub Create {
         CustomPrepareCode      => undef,
         CustomCommitCode       => undef,
         CustomIsApplicableCode => undef,
+        @_
+    );
 
-        @_ );
-
-    if ( !$args{'Queue'} ) {
+    unless ( $args{'Queue'} ) {
         unless ( $self->CurrentUser->HasRight( Object => $RT::System,
                                                Right  => 'ModifyScrips' )
           ) {
@@ -151,9 +151,9 @@ sub Create {
         $args{'Queue'} = 0;    # avoid undef sneaking in
     }
     else {
-        my $QueueObj = new RT::Queue( $self->CurrentUser );
+        my $QueueObj = RT::Queue->new( $self->CurrentUser );
         $QueueObj->Load( $args{'Queue'} );
-        unless ( $QueueObj->id() ) {
+        unless ( $QueueObj->id ) {
             return ( 0, $self->loc('Invalid queue') );
         }
         unless ( $QueueObj->CurrentUserHasRight('ModifyScrips') ) {
@@ -165,28 +165,28 @@ sub Create {
     #TODO +++ validate input
 
     require RT::ScripAction;
-    my $action = new RT::ScripAction( $self->CurrentUser );
-    if ( $args{'ScripAction'} ) {
-        $action->Load( $args{'ScripAction'} );
-    }
+    return ( 0, $self->loc("Action is mandatory argument") )
+        unless $args{'ScripAction'};
+    my $action = RT::ScripAction->new( $self->CurrentUser );
+    $action->Load( $args{'ScripAction'} );
     return ( 0, $self->loc( "Action [_1] not found", $args{'ScripAction'} ) )
-      unless $action->Id;
+        unless $action->Id;
 
     require RT::Template;
-    my $template = new RT::Template( $self->CurrentUser );
-    if ( $args{'Template'} ) {
-        $template->Load( $args{'Template'} );
-    }
-    return ( 0, $self->loc('Template not found') ) unless $template->Id;
+    return ( 0, $self->loc("Template is mandatory argument") )
+        unless $args{'Template'};
+    my $template = RT::Template->new( $self->CurrentUser );
+    $template->Load( $args{'Template'} );
+    return ( 0, $self->loc('Template not found') )
+        unless $template->Id;
 
     require RT::ScripCondition;
-    my $condition = new RT::ScripCondition( $self->CurrentUser );
-    if ( $args{'ScripCondition'} ) {
-        $condition->Load( $args{'ScripCondition'} );
-    }
-    unless ( $condition->Id ) {
-        return ( 0, $self->loc('Condition not found') );
-    }
+    return ( 0, $self->loc("Condition is mandatory argument") )
+        unless $args{'ScripCondition'};
+    my $condition = RT::ScripCondition->( $self->CurrentUser );
+    $condition->Load( $args{'ScripCondition'} );
+    return ( 0, $self->loc('Condition not found') )
+        unless $condition->Id;
 
     my ( $id, $msg ) = $self->SUPER::Create(
         Queue                  => $args{'Queue'},
@@ -198,9 +198,8 @@ sub Create {
         CustomPrepareCode      => $args{'CustomPrepareCode'},
         CustomCommitCode       => $args{'CustomCommitCode'},
         CustomIsApplicableCode => $args{'CustomIsApplicableCode'},
-
     );
-    if ($id) {
+    if ( $id ) {
         return ( $id, $self->loc('Scrip Created') );
     }
     else {
@@ -595,15 +594,17 @@ sub HasRight {
                  Principal => undef,
                  @_ );
 
-    if (     ( defined $self->SUPER::_Value('Queue') )
-         and ( $self->SUPER::_Value('Queue') != 0 ) ) {
-        return ( $args{'Principal'}->HasRight( Right  => $args{'Right'},
-                                               Object => $self->QueueObj ) );
-
+    if ( $self->SUPER::_Value('Queue') ) {
+        return $args{'Principal'}->HasRight(
+            Right  => $args{'Right'},
+            Object => $self->QueueObj
+        );
     }
     else {
-        return ( $args{'Principal'}
-                 ->HasRight( Object => $RT::System, Right => $args{'Right'} ) );
+        return $args{'Principal'}->HasRight(
+            Object => $RT::System,
+            Right  => $args{'Right'},
+        );
     }
 }
 
