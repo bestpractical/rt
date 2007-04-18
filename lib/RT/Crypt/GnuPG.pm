23a809b1f080dfef40eae4dd8b7544d12659da7b
Ruslan Zakirov <ruz@bestpractical.com>
2007-04-18 14:50:46 +0000

    we need real object, the method doesn't work as class-method
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/branches/3.7-EXPERIMENTAL@7540 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/Crypt/GnuPG.pm
+++ lib/RT/Crypt/GnuPG.pm
@@ -544,8 +544,10 @@ sub DecryptRFC3156 {
 
     seek $tmp_fh, 0, 0;
     my $parser = new MIME::Parser;
-    RT::EmailParser->_SetupMIMEParser( $parser );
+    my $rt_parser = new RT::EmailParser;
+    $rt_parser->_SetupMIMEParser( $parser );
     my $decrypted = $parser->parse( $tmp_fh );
+    $decrypted->{'__store_link_to_object_to_avoid_early_cleanup'} = $rt_parser;
     $args{'Top'}->parts( [] );
     $args{'Top'}->add_part( $decrypted );
     $args{'Top'}->make_singlepart;
