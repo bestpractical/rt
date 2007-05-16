e023f2023301a0bd650fa87244d97785360a0de2
Ruslan Zakirov <ruz@bestpractical.com>
2007-05-16 04:13:42 +0000

    replace with equivalent tests, but: use internal scopes add tests for create, add and other ops, not only searches
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rtfm/branches/2.1-TESTING@7883 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- t/05cfsearch.pl
+++ t/05cfsearch.pl
@@ -1,79 +1,101 @@
 #!/usr/bin/env perl
+
 use strict;
 use warnings;
-use Test::More tests => 4;;
 
-use lib ("/opt/rt3/lib","/opt/rt3/local/lib");
-use RT;
+use Test::More tests => 12;
+BEGIN { require 't/utils.pl' }
 
-# Load the config file
+use_ok 'RT';
 RT::LoadConfig();
-
-#Connect to the database and get RT::SystemUser and RT::Nobody loaded
 RT::Init();
 
-my $suffix = "-".$$;
+my $suffix = '-'. $$;
 
-
-#Get the current user all loaded
 my $CurrentUser = $RT::SystemUser;
 
-use RT::FM::Class;
-use RT::FM::Article;
-use RT::CustomField;
+use_ok 'RT::FM::Class';
+use_ok 'RT::FM::Article';
+use_ok 'RT::CustomField';
 
-my $class = new RT::FM::Class($CurrentUser);
-my $cf = new RT::CustomField($CurrentUser);
-my $article1 = new RT::FM::Article($CurrentUser);
-my $article2 = new RT::FM::Article($CurrentUser);
-
-my $classname = 'TestClass'.$suffix;
-my $cfname = 'TestCF'.$suffix;
-my $article1name = 'TestArticle1'.$suffix;
-my $article2name = 'TestArticle2'.$suffix;
-
-  #create class
-my ($val,$msg) =  $class->Create( Name => $classname, Description => 'class for cf tests');
-
-ok ($val,$msg);
+my $classname = 'TestClass';
+my $class = RT::FM::Class->new( $CurrentUser );
+{
+    $class->Load( $classname );
+    unless ( $class->Id ) {
+        my ($id, $msg) = $class->Create(
+            Name => $classname,
+            Description => 'class for cf tests',
+        );
+        ok $id, "created class '$classname' #$id"
+            or diag "error: $msg";
+    } else {
+        ok 1, "class '$classname' exists";
+    }
+}
 
 # create cf
-$cf->Create(Name => $cfname, LookupType => 'RT::FM::Class-RT::FM::Article', Type => 'Select', MaxValues => 1, Description => 'singleselect cf for tests');
+my $cfname = 'TestCF'. $suffix;
+my $cf = RT::CustomField->new( $CurrentUser );
+{
+    my ($id, $msg) = $cf->Create(
+        Name => $cfname,
+        LookupType => 'RT::FM::Class-RT::FM::Article',
+        Type => 'Select', MaxValues => 1,
+        Description => 'singleselect cf for tests',
+    );
+    ok $id, "created cf '$cfname' #$id"
+        or diag "error: $msg";
+}
 
 # attach cf to class
-$cf->AddToObject( $class);
+{
+    my ($status, $msg) = $cf->AddToObject( $class );
+    ok $status, "attached the cf to the class"
+        or diag "error: $msg";
+}
   
 # create two cf-values
-$cf->AddValue( Name => 'Value1');
-$cf->AddValue( Name => 'Value2');
+{
+    my ($status, $msg) = $cf->AddValue( Name => 'Value1' );
+    ok $status, "added a value to the cf" or diag "error: $msg";
 
-# create articles
+    ($status, $msg) = $cf->AddValue( Name => 'Value2' );
+    ok $status, "added a value to the cf" or diag "error: $msg";
+}
+
+my $article1name = 'TestArticle1'.$suffix;
+my $article1 = new RT::FM::Article($CurrentUser);
 $article1->Create( Name => $article1name, Summary => 'Test', Class => $class->Id);
-$article2->Create( Name => $article2name, Summary => 'Test', Class => $class->Id);
-  
-# attach 1st cf-value to article
 $article1->AddCustomFieldValue(Field => $cf->Id, Value => 'Value1');
+
+my $article2name = 'TestArticle2'.$suffix;
+my $article2 = new RT::FM::Article($CurrentUser);
+$article2->Create( Name => $article2name, Summary => 'Test', Class => $class->Id);
 $article2->AddCustomFieldValue(Field => $cf->Id, Value => 'Value2');
 
 # search for articles containing 1st value
-my $articles = new RT::FM::ArticleCollection($CurrentUser);
-$articles->Limit( FIELD => "Class", SUBCLAUSE => 'ClassMatch', VALUE => $class->Id);
+{
+    my $articles = RT::FM::ArticleCollection->new( $CurrentUser );
+    $articles->UnLimit;
+    $articles->Limit( FIELD => "Class", SUBCLAUSE => 'ClassMatch', VALUE => $class->Id);
+    $articles->LimitCustomField( FIELD => $cf->Id, VALUE => 'Value1' );
+    is $articles->Count, 1, 'found correct number of articles';
+}
 
-$articles->LimitCustomField( FIELD => $cf->Id, VALUE => 'Value1' );
+{
+    my $articles = new RT::FM::ArticleCollection($CurrentUser);
+    $articles->UnLimit;
+    $articles->Limit( FIELD => "Class", SUBCLAUSE => 'ClassMatch', VALUE => $class->Id);
+    $articles->LimitCustomField( FIELD => $cf, VALUE => 'Value1' );    
+    is $articles->Count, 1, 'found correct number of articles';
+}
 
-is($articles->Count,1, "Found ".$articles->Count);
+{
+    my $articles = new RT::FM::ArticleCollection($CurrentUser);
+    $articles->UnLimit( );
+    $articles->Limit( FIELD => "Class", SUBCLAUSE => 'ClassMatch', VALUE => $class->Id);
+    $articles->LimitCustomField( FIELD => $cf->Name, VALUE => 'Value1' );
+    is $articles->Count, 1, 'found correct number of articles';
+}
 
-my $articles2 = new RT::FM::ArticleCollection($CurrentUser);
-
-$articles2->Limit( FIELD => "Class", SUBCLAUSE => 'ClassMatch', VALUE => $class->Id);
-
-$articles2->LimitCustomField( FIELD => $cf, VALUE => 'Value1' );
-
-is($articles2->Count, 1,  "CF search by Object is ");
-
-my $articles3 = new RT::FM::ArticleCollection($CurrentUser);
-$articles3->Limit( FIELD => "Class", SUBCLAUSE => 'ClassMatch', VALUE => $class->Id);
-$articles3->LimitCustomField( FIELD => $cf->Name, VALUE => 'Value1' );
-is($articles3->Count, 1,  "CF search by Name is ");
-
-1;
