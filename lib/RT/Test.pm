dc100d5162ad8f57e6bfd83996b98f199019a0e8
Ruslan Zakirov <ruz@bestpractical.com>
2007-08-09 00:42:24 +0000

    move send_via_mailgate from lib/t/utils.pl deprecate lib/t/utils.pl, but leav for backward compatibility   for a while
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/branches/3.7-EXPERIMENTAL-TUNIS@8462 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/Test.pm
+++ lib/RT/Test.pm
@@ -287,4 +287,23 @@ sub run_mailgate {
     return ($?, $result);
 }
 
+sub send_via_mailgate {
+    my $self = shift;
+    my $message = shift;
+    my %args = (@_);
+
+    my ($status, $gate_result) = $self->run_mailgate( message => $message, %args );
+
+    my $id;
+    unless ( $status >> 8 ) {
+        ($id) = ($gate_result =~ /Ticket:\s*(\d+)/i);
+        unless ( $id ) {
+            diag "Couldn't find ticket id in text:\n$gate_result" if $ENV{'TEST_VERBOSE'};
+        }
+    } else {
+        diag "Mailgate output:\n$gate_result" if $ENV{'TEST_VERBOSE'};
+    }
+    return ($status, $id);
+}
+
 1;
--- lib/t/utils.pl
+++ lib/t/utils.pl
@@ -6,23 +6,15 @@ use warnings;
 use RT;
 RT::LoadConfig();
 
+warn "lib/t/utils.pl has been deprecated. Use RT::Test module instead";
+
 sub run_gate {
     require RT::Test;
     return RT::Test->run_mailgate(@_);
 }
 
 sub create_ticket_via_gate {
-    my $message = shift;
-    my ($status, $gate_result) = run_gate( message => $message, @_ );
-    my $id;
-    unless ( $status >> 8 ) {
-        ($id) = ($gate_result =~ /Ticket:\s*(\d+)/i);
-        unless ( $id ) {
-            diag "Couldn't find ticket id in text:\n$gate_result" if $ENV{'TEST_VERBOSE'};
-        }
-    } else {
-        diag "Mailgate output:\n$gate_result" if $ENV{'TEST_VERBOSE'};
-    }
-    return ($status, $id);
+    require RT::Test;
+    return RT::Test->send_via_mailgate(@_);
 }
 
