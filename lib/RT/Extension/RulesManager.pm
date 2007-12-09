a8fa49ed7ba2baa63f539f6f448c47bc4eb3bcf3
Audrey Tang <audreyt@audreyt.org>
2007-12-09 22:17:54 +0000

    Some stub methods for RulesManager
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/branches/3.6-EXPERIMENTAL-ABERDEEN@9862 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/Extension/RulesManager.pm
+++ lib/RT/Extension/RulesManager.pm
@@ -6,18 +6,42 @@ sub create {
 }
 
 sub load {
-    my $self = shift;
-    my $id   = shift;
+    my $self  = shift;
+    my $id    = shift;
+    my $rules = $self->rules;
+    return undef if $id <= 0 or $id >= @$rules;
+    return $rules->[$id-1];
+}
+
+sub raise {
+    my $self  = shift;
+    my $id    = shift;
+    my $rules = $self->rules;
+    return undef if $id <= 1 or $id >= @$rules;
+    @{$rules}[$id-1, $id-2] = @{$rules}[$id-2, $id-1];
+    $rules->[$id-1]{_pos} = $id-1;
+    $rules->[$id-2]{_pos} = $id-2;
+    return $id;
 }
 
 sub named {
     my $self = shift;
     my $name = shift;
+    foreach my $rule (@{$self->rules}) {
+        return $rule if $rule->name eq $name;
+    }
+    return undef;
 }
 
 sub rules {
     my $self = shift;
-    my $rules = $self->_load;
+    my $rules = $self->_load || [];
+    for my $i (0..$#$rules) {
+        $rules->[$i]{_pos} = $i;
+        $rules->[$i]{_root} = $rules;
+        bless $rules->[$i] => 'RT::Extension::RuleManager::Rule';
+    }
+    return $rules;
 }
 
 1;
