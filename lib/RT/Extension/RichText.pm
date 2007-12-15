03ed84ec5d24c212afd6e84830ed0c224238166f
Audrey Tang <audreyt@audreyt.org>
2007-12-15 20:42:23 +0000

    RichText commit (temp)
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/branches/3.6-EXPERIMENTAL-ABERDEEN@9969 e417ac7c-1bcc-0310-8ffa-8f5827389a85

---
+++ lib/RT/Extension/RichText.pm
@@ -0,0 +1,49 @@
+# For ABERDEEN BRANCH ONLY!
+
+package RT::Extension::RichText;
+use 5.008;
+
+our $VERSION = '0.01';
+
+__END__
+=pod
+
+=head1 NAME 
+
+RT::Extension::RichText - Rich Text Edit support for RT
+
+=head1 DESCRIPTION
+
+This extension enables HTML-based rich text editing to text areas in RT's web UI.
+
+=head1 AUTHORS
+
+Audrey Tang E<lt>cpan@audreyt.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2007 by Audrey Tang E<lt>cpan@audreyt.orgE<gt>.
+
+This software is released under the MIT license cited below.
+
+=head2 The "MIT" License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+
+=cut
