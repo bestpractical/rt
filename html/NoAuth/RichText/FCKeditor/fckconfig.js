3c5a5518497db69e88af05643724d40a6d921c4c
Audrey Tang <audreyt@audreyt.org>
2008-02-07 18:20:47 +0000

    Turn on Firefox spell checking for DCKEditor.
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/branches/3.6-EXPERIMENTAL-ABERDEEN@10759 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- html/NoAuth/RichText/FCKeditor/fckconfig.js
+++ html/NoAuth/RichText/FCKeditor/fckconfig.js
@@ -141,7 +141,7 @@ FCKConfig.Keystrokes = [
 ] ;
 
 FCKConfig.ContextMenu = ['Generic','Link','Anchor','Image','Flash','Select','Textarea','Checkbox','Radio','TextField','HiddenField','ImageButton','Button','BulletedList','NumberedList','Table','Form'] ;
-FCKConfig.BrowserContextMenuOnCtrl = false ;
+FCKConfig.BrowserContextMenuOnCtrl = true ;
 
 FCKConfig.EnableMoreFontColors = true ;
 FCKConfig.FontColors = '000000,993300,333300,003300,003366,000080,333399,333333,800000,FF6600,808000,808080,008080,0000FF,666699,808080,FF0000,FF9900,99CC00,339966,33CCCC,3366FF,800080,999999,FF00FF,FFCC00,FFFF00,00FF00,00FFFF,00CCFF,993366,C0C0C0,FF99CC,FFCC99,FFFF99,CCFFCC,CCFFFF,99CCFF,CC99FF,FFFFFF' ;
@@ -156,7 +156,7 @@ FCKConfig.TemplatesXmlPath	= FCKConfig.EditorPath + 'fcktemplates.xml' ;
 FCKConfig.SpellChecker			= 'ieSpell' ;	// 'ieSpell' | 'SpellerPages'
 FCKConfig.IeSpellDownloadUrl	= 'http://www.iespell.com/download.php' ;
 FCKConfig.SpellerPagesServerScript = 'server-scripts/spellchecker.php' ;	// Available extension: .php .cfm .pl
-FCKConfig.FirefoxSpellChecker	= false ;
+FCKConfig.FirefoxSpellChecker	= true ;
 
 FCKConfig.MaxUndoLevels = 15 ;
 
