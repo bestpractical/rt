3e5371fdeeeb9c4569913efe7ac5eb440e6ca7d9
sunnavy <sunnavy@bestpractical.com>
2008-04-24 19:54:56 +0000

    moved the big widget option hash to  Installer.pm, which(hashref) can be retrieved by RT::Installer->Option
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/branches/3.8-TESTING@11882 e417ac7c-1bcc-0310-8ffa-8f5827389a85

---
+++ lib/RT/Installer.pm
@@ -0,0 +1,156 @@
+package RT::Installer;
+use strict;
+use warnings;
+
+require UNIVERSAL::require;
+my %Option = (
+    DatabaseType => {
+        Widget          => '/Widgets/Form/Select',
+        WidgetArguments => {
+            Description => 'Type of the database where RT will store its data',
+            Values => [
+                grep {
+                    my $m = 'DBD::' . $_;
+                    $m->require ? 1 : 0
+                  } qw/mysql Pg SQLite Oracle/
+            ],
+            ValuesLabel => {
+                mysql  => 'MySQL',                                          #loc
+                Pg     => 'PostgreSQL',                                     #loc
+                SQLite => 'SQLite (for experiments and development only)',  #loc
+                Oracle => 'Oracle',                                         #loc
+            },
+        },
+    },
+    DatabaseHost => {
+        Widget          => '/Widgets/Form/String',
+        WidgetArguments => {
+            Description => 'The domain name of your database server',       #loc
+        },
+    },
+    DatabasePort => {
+        Widget          => '/Widgets/Form/Integer',
+        WidgetArguments => {
+            Description => 'Port number database server listen to',         #loc
+            Default     => 1,
+            DefaultLabel =>
+              'Leave empty to use default value of the RDBMS',              #loc
+        },
+    },
+    DatabaseName => {
+        Widget          => '/Widgets/Form/String',
+        WidgetArguments => {
+            Description => 'Name of the database',                          #loc
+        },
+    },
+    DatabaseUser => {
+        Widget          => '/Widgets/Form/String',
+        WidgetArguments => {
+            Description =>
+              'The name of the user RT will use to connect to the DB',      #loc
+        },
+    },
+    DatabasePassword => {
+        Widget          => '/Widgets/Form/String',
+        WidgetArguments => {
+            Description =>
+              'Password of the above user RT will use to connect to the DB'
+            ,                                                               #loc
+        },
+    },
+    DatabaseRequireSSL => {
+        Widget          => '/Widgets/Form/Boolean',
+        WidgetArguments => {
+            Description => 'Connecting DB requires SSL',    # loc
+        },
+    },
+    rtname => {
+        Widget          => '/Widgets/Form/String',
+        WidgetArguments => {
+            Description => 'rtname',                        #loc
+        },
+    },
+    Organization => {
+        Widget          => '/Widgets/Form/String',
+        WidgetArguments => {
+            Description => 'Organization',                  #loc
+        },
+    },
+    MinimumPasswordLength => {
+        Widget          => '/Widgets/Form/Integer',
+        WidgetArguments => {
+            Description => 'MinimumPasswordLength',         #loc
+        },
+    },
+    MaxAttachmentSize => {
+        Widget          => '/Widgets/Form/Integer',
+        WidgetArguments => {
+            Description => 'MaxAttachmentSize',             #loc
+        },
+    },
+    OwnerEmail => {
+        Widget          => '/Widgets/Form/String',
+        WidgetArguments => {
+            Description => 'OwnderEmail',                   #loc
+        },
+    },
+    CommentAddress => {
+        Widget          => '/Widgets/Form/String',
+        WidgetArguments => {
+            Description => 'CommentAddress',                #loc
+        },
+    },
+    CorrespondAddress => {
+        Widget          => '/Widgets/Form/String',
+        WidgetArguments => {
+            Description => 'CorrespondAddress',             #loc
+        },
+    },
+    MailCommand => {
+        Widget          => '/Widgets/Form/String',
+        WidgetArguments => {
+            Description => 'MailCommand',                   #loc
+        },
+    },
+    SendmailArguments => {
+        Widget          => '/Widgets/Form/String',
+        WidgetArguments => {
+            Description => 'SendmailArguments',             #loc
+        },
+    },
+    SendmailBounceArguments => {
+        Widget          => '/Widgets/Form/String',
+        WidgetArguments => {
+            Description => 'SendmailBounceArguments',       #loc
+        },
+    },
+    SendmailPath => {
+        Widget          => '/Widgets/Form/String',
+        WidgetArguments => {
+            Description => 'SendmailPath',                  #loc
+        },
+    },
+
+);
+
+sub Option {
+    return \%Option;
+}
+
+=head1 NAME
+
+    RT::Installer - RT's Installer
+
+=head1 SYNOPSYS
+
+    use RT::Installer;
+    my $option = RT::Installer->Option;
+
+=head1 DESCRIPTION
+
+C<RT::Installer> class provides access to RT Installer Options
+
+=cut
+
+1;
+
