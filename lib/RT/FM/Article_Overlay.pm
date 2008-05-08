61d26c3a15eb20697952de723dec583e7410ce4f
Kevin Falcone <falcone@bestpractical.com>
2008-05-08 20:51:11 +0000

    remove Template Parsing code (maintain code cleanup we merged from 2.2)
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rtfm/branches/2.3-EXPERIMENTAL@12156 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/FM/Article_Overlay.pm
+++ lib/RT/FM/Article_Overlay.pm
@@ -506,64 +506,6 @@ sub DeleteTopic {
     }
 }
 
-=head2 ParseTemplate $CONTENT, %TEMPLATE_ARGS
-
-Parses $CONTENT string as a template (L<Text::Template>).
-$Article and other arguments from %TEMPLATE_ARGS are
-available in code of the template as perl variables.
-
-=cut
-
-sub ParseTemplate {
-    my $self = shift;
-    my $content = shift;
-    my %args = (
-        Ticket => undef,
-        @_
-    );
-
-    return ($content) unless defined $content && length $content;
-
-    $args{'Article'} = $self;
-    $args{'rtname'}  = $RT::rtname;
-    if ( $args{'Ticket'} ) {
-        my $t = $args{'Ticket'}; # avoid memory leak
-        $args{'loc'} = sub { $t->loc(@_) };
-    } else {
-        $args{'loc'} = sub { $self->loc(@_) };
-    }
-
-    foreach my $key ( keys %args ) {
-        next unless ref $args{ $key };
-        next if ref $args{ $key } =~ /^(ARRAY|HASH|SCALAR|CODE)$/;
-        my $val = $args{ $key };
-        $args{ $key } = \$val;
-    }
-
-    # We need to untaint the content of the template, since we'll be working
-    # with it
-    $content =~ s/^(.*)$/$1/;
-    my $template = Text::Template->new(
-        TYPE   => 'STRING',
-        SOURCE => $content
-    );
-
-    my $is_broken = 0;
-    my $retval = $template->fill_in(
-        HASH => \%args,
-        BROKEN => sub {
-            my (%args) = @_;
-            $RT::Logger->error("Article parsing error: $args{error}")
-                unless $args{error} =~ /^Died at /; # ignore intentional die()
-            $is_broken++;
-            return undef;
-        },
-    );
-    return ( undef, $self->loc('Article parsing error') ) if $is_broken;
-
-    return ($retval);
-}
-
 =head2 CurrentUserHasRight
 
 Returns true if the current user has the right for this article, for the whole system or for this article's class
