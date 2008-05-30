acd3d7c6d477494ad73f6b0cfc74815f68cf8d28
sunnavy <sunnavy@bestpractical.com>
2008-05-30 15:51:50 +0000

    revert RT_Site_Config.pm
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/branches/3.8-TESTING@12764 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- etc/RT_SiteConfig.pm
+++ etc/RT_SiteConfig.pm
@@ -1,20 +1,19 @@
-Set( $DatabaseUser, 'rt_user' );
-Set( $SendmailBounceArguments, '-f "<>"' );
-Set( $Timezone, '' );
-Set( $CorrespondAddress, 'RT_CorrespondAddressNotSet' );
-Set( $rtname, 'example.com' );
-Set( $DatabaseRequireSSL, '0' );
-Set( $SendmailArguments, '-oi -t' );
-Set( $Organization, 'example.com' );
-Set( $MaxAttachmentSize, '10000' );
-Set( $DatabaseType, 'mysql' );
-Set( $DatabasePassword, 'rt_pass' );
-Set( $DatabaseAdmin, 'root' );
-Set( $SendmailPath, '/usr/sbin/sendmail' );
-Set( $MailCommand, 'sendmailpipe' );
-Set( $CommentAddress, 'RT_CommentAddressNotSet' );
-Set( $DatabaseHost, 'localhost' );
-Set( $MinimumPasswordLength, '5' );
-Set( $DatabaseName, 'rt3' );
-Set( $OwnerEmail, 'root' );
+# Any configuration directives you include  here will override 
+# RT's default configuration file, RT_Config.pm
+#
+# To include a directive here, just copy the equivalent statement
+# from RT_Config.pm and change the value. We've included a single
+# sample value below.
+#
+# This file is actually a perl module, so you can include valid
+# perl code, as well.
+#
+# The converse is also true, if this file isn't valid perl, you're
+# going to run into trouble. To check your SiteConfig file, use
+# this comamnd:
+#
+#   perl -c /path/to/your/etc/RT_SiteConfig.pm
+
+Set( $rtname, 'example.com');
+#Set(@Plugins,(qw(Extension-QuickDelete)));
 1;
