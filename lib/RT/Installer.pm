f47f4e16bf785bf6d8236760a4dc6f4fe72f27f8
sunnavy <sunnavy@bestpractical.com>
2008-06-03 16:35:50 +0000

    do not clobber document of RT_Site_Config.pm
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/branches/3.8-TESTING@12836 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/Installer.pm
+++ lib/RT/Installer.pm
@@ -257,6 +257,24 @@ sub SaveConfig {
     my $file = $class->ConfigFile;
 
     if ( open my $fh, '>', $file  ) {
+        print $fh <<EOF;
+# Any configuration directives you include  here will override 
+# RT's default configuration file, RT_Config.pm
+#
+# To include a directive here, just copy the equivalent statement
+# from RT_Config.pm and change the value. We've included a single
+# sample value below.
+#
+# This file is actually a perl module, so you can include valid
+# perl code, as well.
+#
+# The converse is also true, if this file isn't valid perl, you're
+# going to run into trouble. To check your SiteConfig file, use
+# this comamnd:
+#
+#   perl -c /path/to/your/etc/RT_SiteConfig.pm
+
+EOF
         for ( keys %{$RT::Installer->{InstallConfig}} ) {
             if (defined $RT::Installer->{InstallConfig}{$_}) {
                 print $fh "Set( \$$_, '$RT::Installer->{InstallConfig}{$_}' );\n";
