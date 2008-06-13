07ec5dc4f526b438ef8adb2c26ef433e89aed95f
Jacky Chang <kyoki@bestpractical.com>
2008-06-13 02:34:48 +0000

    RT-Ticket: 9624 RT-Status: Resolved RT-Update: Correspond Username displays are now customizable to either concise or verbose, default being concise
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/branches/3.8-TESTING@13265 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/Config.pm
+++ lib/RT/Config.pm
@@ -182,6 +182,19 @@ our %META = (
             },
         },
     },
+    UsernameFormat => {
+        Section         => 'General',
+        Overridable     => 1,
+	Widget          =>'/Widgets/Form/Select',
+	WidgetArguments => {
+	   Description  => 'Username format',
+	   Values       => [qw(concise verbose)],
+           ValueLabel   => {
+                concise     => 'Short usernames',
+                verbose     => 'Name and email address',
+                },
+	},
+    },
     DefaultQueue => {
         Section         => 'General',
         Overridable     => 1,
