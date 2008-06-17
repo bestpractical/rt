7b7a1892d6a4be71310d6b7bdbbddc56b4934aab
Ruslan Zakirov <ruz@bestpractical.com>
2008-06-17 01:48:21 +0000

    use 'our' instead of 'my' in lib/RT/Graph/Tickets.pm try to workaround mysterious bug
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/3.8/trunk@13346 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/Graph/Tickets.pm
+++ lib/RT/Graph/Tickets.pm
@@ -75,14 +75,14 @@ our %ticket_status_style = (
     deleted  => { fontcolor => '#A9A9A9' },
 );
 
-my %link_style = (
+our %link_style = (
     MemberOf  => { style => 'solid' },
     DependsOn => { style => 'dashed' },
     RefersTo  => { style => 'dotted' },
 );
 
 # We don't use qw() because perl complains about "possible attempt to put comments in qw() list"
-my @fill_colors = split ' ',<<EOT;
+our @fill_colors = split ' ',<<EOT;
     #0000FF #8A2BE2 #A52A2A #DEB887 #5F9EA0 #7FFF00 #D2691E #FF7F50
     #6495ED #FFF8DC #DC143C #00FFFF #00008B #008B8B #B8860B #A9A9A9
     #A9A9A9 #006400 #BDB76B #8B008B #556B2F #FF8C00 #9932CC #8B0000
@@ -110,7 +110,7 @@ sub gv_escape($) {
 
 our (%fill_cache, @available_colors) = ();
 
-my %property_cb = (
+our %property_cb = (
     Queue => sub { return $_[0]->QueueObj->Name || $_[0]->Queue },
     CF    => sub {
         my $values = $_[0]->CustomFieldValues( $_[1] );
