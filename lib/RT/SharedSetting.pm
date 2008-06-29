da9f3da28088d9a814c53138358c288136d80bc9
Shawn Moore <sartak@bestpractical.com>
2008-06-29 19:50:05 +0000

    Demote the invalid privacy string error to a debug, it's not actually end-of-world bad
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/3.8/trunk@13684 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/SharedSetting.pm
+++ lib/RT/SharedSetting.pm
@@ -373,7 +373,7 @@ sub _GetObject {
 
     unless ($obj_type && $obj_id) {
         $privacy = '(undef)' if !defined($privacy);
-        $RT::Logger->error("Invalid privacy string '$privacy'");
+        $RT::Logger->debug("Invalid privacy string '$privacy'");
         return undef;
     }
 
