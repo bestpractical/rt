80a9e1126f07eb805721443a44e2cecb2b70723e
sunnavy <sunnavy@bestpractical.com>
2008-07-19 15:48:12 +0000

    RT->system_user returns RT::CurrentUser object, not RT::Model::User
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/branches/3.999-DANGEROUS@14318 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/Bootstrap.pm
+++ lib/RT/Bootstrap.pm
@@ -111,7 +111,7 @@ sub insert_initial_data {
         else {
             my $ace = RT::Model::ACE->new( current_user => RT->system_user );
             my ( $val, $msg ) = $ace->_bootstrap_create(
-                principal_id   => acl_equiv_group_id( RT->system_user ),
+                principal_id   => acl_equiv_group_id( RT->system_user->id ),
                 principal_type => 'Group',
                 right_name     => 'SuperUser',
                 object_type    => 'RT::System',
