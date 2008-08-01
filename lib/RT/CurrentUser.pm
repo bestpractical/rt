7238104cf932bb7f0383e12dfa65d97d174a72a1
Kevin Falcone <falcone@bestpractical.com>
2008-08-01 17:04:28 +0000

    we call loc() without real CurrentUsers (no id) which   causes this code to fail when running the shredder   tests or running make initdb
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/3.8/trunk@14712 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/CurrentUser.pm
+++ lib/RT/CurrentUser.pm
@@ -219,7 +219,7 @@ sub LanguageHandle {
         if ( my $lang = $self->Lang ) {
             push @_, $lang;
         }
-        elsif ( $self->id == $RT::SystemUser->id || $self->id == $RT::Nobody->id ) {
+        elsif ( !$self->id || $self->id == $RT::SystemUser->id || $self->id == $RT::Nobody->id ) {
             # don't use ENV magic for system users
             push @_, 'en';
         }
