2b04f5d5e638151353e673a741bb15a65edb1407
Kevin Falcone <falcone@bestpractical.com>
2008-08-20 00:54:32 +0000

    create an RTFM_Config.pm for our one config option now that   our plugin infrastructure supports it
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rtfm/trunk@15259 e417ac7c-1bcc-0310-8ffa-8f5827389a85

---
+++ etc/RTFM_Config.pm
@@ -0,0 +1,18 @@
+# This module is intended to be used to set RTFM options.
+# It is loaded after RT_Config.pm
+#
+# This file is actually a perl module, so you can include valid
+# perl code, as well.
+#
+# The converse is also true, if this file isn't valid perl, you're
+# going to run into trouble. To check your SiteConfig file, use
+# this comamnd:
+#
+#   perl -c /path/to/your/etc/RTFM_Config.pm
+
+#,  Set this to 1 to display the RTFM interface on the Ticket Create page
+# in addition to the Reply/Comment page
+
+Set($RTFM_TicketCreate, 0);
+
+1;
