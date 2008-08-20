4e67d9d8d6b39e5886b65a63b7bebc0ebc135ffd
Kevin Falcone <falcone@bestpractical.com>
2008-08-20 00:54:54 +0000

    make sure to mention that this is 3.8.1 only
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rtfm/trunk@15260 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- etc/RTFM_Config.pm
+++ etc/RTFM_Config.pm
@@ -10,8 +10,9 @@
 #
 #   perl -c /path/to/your/etc/RTFM_Config.pm
 
-#,  Set this to 1 to display the RTFM interface on the Ticket Create page
-# in addition to the Reply/Comment page
+# Set this to 1 to display the RTFM interface on the Ticket Create page
+# in addition to the Reply/Comment page.
+# This will only work with 3.8.1 or greater
 
 Set($RTFM_TicketCreate, 0);
 
