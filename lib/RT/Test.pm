3c7f5b0070861acb2b90f4d26235cd7fd5c7fa60
Ruslan Zakirov <ruz@bestpractical.com>
2008-08-22 22:44:20 +0000

    minor
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/3.8/branches/parallel-testing@15383 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/Test.pm
+++ lib/RT/Test.pm
@@ -69,10 +69,12 @@ RT::Test - RT Testing
 
 To run the rt test suite with coverage support, install L<Devel::Cover> and run:
 
-  make test RT_DBA_USER=.. RT_DBA_PASSWORD=.. HARNESS_PERL_SWITCHES=-MDevel::Cover
- cover -ignore_re '^var/mason_data/' -ignore_re '^t/'
+    make test RT_DBA_USER=.. RT_DBA_PASSWORD=.. HARNESS_PERL_SWITCHES=-MDevel::Cover
+    cover -ignore_re '^var/mason_data/' -ignore_re '^t/'
 
-The coverage tests have DevelMode turned off, and have C<named_component_subs> enabled for L<HTML::Mason> to avoid an optimizer problem in Perl that hides the top-level optree from L<Devel::Cover>.
+The coverage tests have DevelMode turned off, and have
+C<named_component_subs> enabled for L<HTML::Mason> to avoid an optimizer
+problem in Perl that hides the top-level optree from L<Devel::Cover>.
 
 =cut
 
