203ccaf341df97a7dcc6c555ed7ce7f57487c0c3
sunnavy <sunnavy@bestpractical.com>
2008-10-06 12:40:53 +0000

    added basics fields
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/branches/3.999-DANGEROUS@16216 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/Action/UpdateTicket.pm
+++ lib/RT/Action/UpdateTicket.pm
@@ -8,6 +8,16 @@ sub record_class { 'RT::Model::Ticket' }
 
 use constant report_detailed_messages => 1;
 
+sub arguments {
+    my $self = shift;
+
+    my $args = $self->SUPER::arguments();
+    $args->{status}{valid_values} = $self->_compute_possible_statuses;
+    $args->{queue}{valid_values} = $self->_compute_possible_queues;
+    $args->{owner}{valid_values} = $self->_compute_possible_owners;
+    return $args;
+}
+
 =head2 take_action
 
 =cut
@@ -19,6 +29,7 @@ sub take_action {
     foreach my $field (@date_fields) {
         my $value = $self->argument_value($field);
         if ( defined $value ) {
+
             # convert date to be as utc
             my $date = RT::Date->new();
             $date->set(
@@ -30,8 +41,86 @@ sub take_action {
         }
     }
 
+    my @time_fields = qw/time_left time_estimated time_worked/;
+
+    foreach my $field (@time_fields) {
+        my $value = $self->argument_value($field);
+        if ( defined $value ) {
+            if ( $value =~ /([\d.]+)h(our)?/ ) {
+                $value = int $1 * 60;
+            }
+            elsif ( $value =~ /(\d+)/ ) {
+                $value = $1;
+            }
+            $self->argument_value( $field, $value );
+        }
+    }
+
     $self->SUPER::take_action;
     return 1;
+}
+
+sub _compute_possible_owners {
+    my $self = shift;
+
+    my @objects = ( $self->record, $self->record->queue );
+
+    my %user_uniq_hash;
+
+    my $isSU = Jifty->web->current_user->has_right(
+        right  => 'SuperUser',
+        object => RT->system
+    );
+
+    foreach my $object (@objects) {
+        my $Users = RT::Model::UserCollection->new;
+        $Users->who_have_right(
+            right                 => 'OwnTicket',
+            object                => $object,
+            include_system_rights => 1,
+            include_superusers    => $isSU
+        );
+        while ( my $user = $Users->next() ) {
+            next
+              if ( $user->id == $RT::nobody->id )
+              ;    # skip nobody here, so we can make them first later
+            $user_uniq_hash{ $user->id() } = $user;
+        }
+    }
+
+    my $owners = [
+        map { { display => $_->name, value => $_->id } }
+          sort { uc( $a->name ) cmp uc( $b->name ) } values %user_uniq_hash
+    ];
+    unshift @$owners, { display => 'Nobody', value => $RT::nobody->id };
+
+    return $owners;
+}
+
+sub _compute_possible_queues {
+    my $self = shift;
+
+    my $q = RT::Model::QueueCollection->new();
+    $q->find_all_rows;
+    
+    my $queues;
+    while (my $queue = $q->next) {
+        if ($queue->current_user_has_right('CreateTicket')) {
+            push @$queues, { display => $queue->name, value => $queue->id };
+        }
+    }
+
+    return $queues;
+}
+
+sub _compute_possible_statuses {
+    my $self = shift;
+
+    return [
+        map { { display => $_, value => $_ } }
+          RT::Model::Queue->active_status_array,
+        RT::Model::Queue->inactive_status_array
+    ];
 }
 
 1;
