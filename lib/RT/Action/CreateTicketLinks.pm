82ce68879e2e5fcbe19b175e7c8661199b3d7631
sunnavy <sunnavy@bestpractical.com>
2008-10-07 14:15:56 +0000

    added action CreateTicketLinks
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/branches/3.999-DANGEROUS@16234 e417ac7c-1bcc-0310-8ffa-8f5827389a85

---
+++ lib/RT/Action/CreateTicketLinks.pm
@@ -0,0 +1,78 @@
+package RT::Action::CreateTicketLinks;
+use strict;
+use warnings;
+
+use constant report_detailed_messages => 1;
+use Text::Naming::Convention qw/renaming/;
+
+use Jifty::Param::Schema;
+use Jifty::Action schema {
+    param
+      id => render as 'hidden',
+      default is defer {
+        my $id = Jifty->web->request->argument('id');
+        $id = $id->[0] if ref $id eq 'ARRAY';
+        $id;
+      };
+    param merge_into     => label is 'merge into';
+    param depends_on     => label is 'depends on';
+    param depended_on_by => label is 'depended on by';
+    param member_of      => label is 'parents';
+    param has_member     => label is 'children';
+    param refers_to      => label is 'refers_to';
+    param referred_to_by => label is 'referred_to_by';
+};
+
+=head2 take_action
+
+=cut
+
+
+sub take_action {
+    my $self = shift;
+    my %map  = (
+        depends_on => 'depended_on_by',
+        member_of  => 'has_member',
+        refers_to  => 'referred_to_by',
+    );
+
+    $self->result->content->{'detailed_messages'} ||= {};
+    if ( $self->argument_value('id') ) {
+        my $ticket =
+          RT::Model::Ticket->new( current_user => Jifty->web->current_user );
+        my $id = $self->argument_value('id');
+        $ticket->load($id);
+        for my $field ( keys %map ) {
+            my $type = renaming( $field, { convention => 'UpperCamelCase' } );
+
+            for my $value ( split / /, $self->argument_value($field) ) {
+                next unless $value;
+                my ( $val, $msg ) = $ticket->add_link(
+                    target => $value,
+                    type   => $type,
+                );
+                $self->result->content('detailed_messages')->{$field} .= $msg;
+            }
+            for my $value ( split / /, $self->argument_value( $map{$field} ) ) {
+                next unless $value;
+                my ( $val, $msg ) = $ticket->add_link(
+                    base => $value,
+                    type => $type,
+                );
+                $self->result->content('detailed_messages')->{ $map{field} } .=
+                  "$msg.";
+            }
+        }
+
+        # now we handle merge_into stuff
+        if ( my $merge_into = $self->argument_value('merge_into') ) {
+            $merge_into =~ s/\s+//g;
+            my ( $val, $msg ) = $ticket->merge_into( $merge_into );
+            $self->result->content('detailed_messages')->{'merge_into'} = $msg;
+        }
+    }
+
+    return 1;
+}
+
+1;
