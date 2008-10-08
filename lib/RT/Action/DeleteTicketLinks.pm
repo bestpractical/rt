bca1adb6ee6c2ad0ada45f39690c1354963bcfcb
sunnavy <sunnavy@bestpractical.com>
2008-10-08 22:05:45 +0000

    added DeleteTicketLinks action
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/branches/3.999-DANGEROUS@16247 e417ac7c-1bcc-0310-8ffa-8f5827389a85

---
+++ lib/RT/Action/DeleteTicketLinks.pm
@@ -0,0 +1,51 @@
+package RT::Action::DeleteTicketLinks;
+use Jifty::Param::Schema;
+use Jifty::Action schema {};
+
+use Text::Naming::Convention qw/renaming/;
+use constant report_detailed_messages => 1;
+
+sub take_action {
+    my $self = shift;
+
+    $self->result->content->{'detailed_messages'} ||= {};
+    my %map = (
+        depends_on => 'depended_on_by',
+        member_of  => 'has_member',
+        refers_to  => 'referred_to_by',
+    );
+
+    if ( $self->argument_value('id') ) {
+        my $ticket =
+          RT::Model::Ticket->new( current_user => Jifty->web->current_user );
+        my $id = $self->argument_value('id');
+        $ticket->load($id);
+
+        for my $field ( keys %map ) {
+            my $type = renaming( $field, { convention => 'UpperCamelCase' } );
+            if ( my $v = $self->argument_value($field) ) {
+                for my $value ( ref $v eq 'ARRAY' ? @$v : $v ) {
+                    my ( $val, $msg ) = $ticket->delete_link(
+                        target => $value,
+                        type   => $type,
+                    );
+                    $self->result->content('detailed_messages')->{$field} .=
+                      $msg;
+                }
+            }
+            if ( my $v = $self->argument_value( $map{$field} ) ) {
+                for my $value ( ref $v eq 'ARRAY' ? @$v : $v ) {
+                    my ( $val, $msg ) = $ticket->delete_link(
+                        base => $value,
+                        type => $type,
+                    );
+                    $self->result->content('detailed_messages')->{ $map{field} } .= "$msg.";
+                }
+            }
+        }
+    }
+
+    return 1;
+}
+
+1;
