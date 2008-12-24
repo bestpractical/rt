08f990cfa1db2f1cd7bed14d510155658b493bcc
Ruslan Zakirov <ruz@bestpractical.com>
2008-12-24 15:33:14 +0000

    add lib/RT/I18N/ru.pm
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/3.8/trunk@17324 e417ac7c-1bcc-0310-8ffa-8f5827389a85

---
+++ lib/RT/I18N/ru.pm
@@ -0,0 +1,74 @@
+# BEGIN BPS TAGGED BLOCK {{{
+# 
+# COPYRIGHT:
+# 
+# This software is Copyright (c) 1996-2008 Best Practical Solutions, LLC
+#                                          <jesse@bestpractical.com>
+# 
+# (Except where explicitly superseded by other copyright notices)
+# 
+# 
+# LICENSE:
+# 
+# This work is made available to you under the terms of Version 2 of
+# the GNU General Public License. A copy of that license should have
+# been provided with this software, but in any event can be snarfed
+# from www.gnu.org.
+# 
+# This work is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301 or visit their web page on the internet at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html.
+# 
+# 
+# CONTRIBUTION SUBMISSION POLICY:
+# 
+# (The following paragraph is not intended to limit the rights granted
+# to you to modify and distribute this software under the terms of
+# the GNU General Public License and is only of importance to you if
+# you choose to contribute your changes and enhancements to the
+# community by submitting them to Best Practical Solutions, LLC.)
+# 
+# By intentionally submitting any modifications, corrections or
+# derivatives to this work, or any other work intended for use with
+# Request Tracker, to Best Practical Solutions, LLC, you confirm that
+# you are the copyright holder for those contributions and you grant
+# Best Practical Solutions,  LLC a nonexclusive, worldwide, irrevocable,
+# royalty-free, perpetual, license to use, copy, create derivative
+# works based on those contributions, and sublicense and distribute
+# those contributions and any derivatives thereof.
+# 
+# END BPS TAGGED BLOCK }}}
+
+package RT::I18N::ru;
+
+sub quant {
+    my($handle, $num, @forms) = @_;
+
+    return $num unless @forms;
+    return $forms[3] if !$num && $forms[3];
+
+    return $num .' '. $handle->numerate($num, @forms);
+}
+
+sub numerate {
+    my($handle, $n, @forms) = @_;
+
+    my $form = 0;
+    if ( $n%10 == 1 && $n%100 != 11 ) {
+        $form = 0;
+    } elsif ( $n%10 <=4 && ($n%100 < 10 || $n%100 >= 20) ) {
+        $form = 1;
+    } else {
+        $form = 2;
+    }
+    return $forms[$form];
+}
+
+1;
