bestpractical/rt

commit 97cc3d9a396c2f2fdc3f61dfa07bdcaf3bdc51b9

Add transactions when enabling/disabling users/groups/queues so admins can tracks those changes through "history" links.
git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/3.8/trunk@17674 e417ac7c-1bcc-0310-8ffa-8f5827389a85

lib/RT/Queue_Overlay.pm
@@ -394,6 +394,29 @@ Takes a boolean.
 
 =cut
 
+sub SetDisabled {
+    my $self = shift;
+    my $val = shift;
+
+    $RT::Handle->BeginTransaction();
+    my $set_err = $self->PrincipalObj->SetDisabled($val);
+    unless ($set_err) {
+        $RT::Handle->Rollback();
+        $RT::Logger->warning("Couldn't ".($val == 1) ? "disable" : "enable"." queue ".$self->PrincipalObj->Id);
+        return (undef);
+    }
+    $self->_NewTransaction( Type => ($val == 1) ? "Disabled" : "Enabled" );
+
+    $RT::Handle->Commit();
+
+    if ( $val == 1 ) {
+        return (1, $self->loc("Queue disabled"));
+    } else {
+        return (1, $self->loc("Queue enabled"));
+    }
+
+}
+
 # }}}
 
 # {{{ sub Load 

lib/RT/Group_Overlay.pm
@@ -714,6 +714,8 @@ This routine finds all the cached group members that are members of this group  
         }
     }
 
+    $self->_NewTransaction( Type => ($val == 1) ? "Disabled" : "Enabled" );
+
     $RT::Handle->Commit();
     if ( $val == 1 ) {
         return (1, $self->loc("Group disabled"));

lib/RT/Transaction_Overlay.pm
@@ -574,6 +574,12 @@ sub BriefDescription {
     if ( $type eq 'Create' ) {
         return ( $self->loc( "[_1] created", $obj_type ) );
     }
+    elsif ( $type eq 'Enabled' ) {
+        return ( $self->loc( "[_1] enabled", $obj_type ) );
+    }
+    elsif ( $type eq 'Disabled' ) {
+        return ( $self->loc( "[_1] disabled", $obj_type ) );
+    }
     elsif ( $type =~ /Status/ ) {
         if ( $self->Field eq 'Status' ) {
             if ( $self->NewValue eq 'deleted' ) {

lib/RT/User_Overlay.pm
@@ -1122,10 +1122,28 @@ user will fail. The user will appear in no user listings.
 
 sub SetDisabled {
     my $self = shift;
+    my $val = shift;
     unless ( $self->CurrentUser->HasRight(Right => 'AdminUsers', Object => $RT::System) ) {
         return (0, $self->loc('Permission Denied'));
     }
-    return $self->PrincipalObj->SetDisabled(@_);
+
+    $RT::Handle->BeginTransaction();
+    my $set_err = $self->PrincipalObj->SetDisabled($val);
+    unless ($set_err) {
+        $RT::Handle->Rollback();
+        $RT::Logger->warning("Couldn't ".($val == 1) ? "disable" : "enable"." user ".$self->PrincipalObj->Id);
+        return (undef);
+    }
+    $self->_NewTransaction( Type => ($val == 1) ? "Disabled" : "Enabled" );
+
+    $RT::Handle->Commit();
+
+    if ( $val == 1 ) {
+        return (1, $self->loc("User disabled"));
+    } else {
+        return (1, $self->loc("User enabled"));
+    }
+
 }
 
 =head2 Disabled

share/html/Admin/Users/Modify.html
@@ -353,7 +353,7 @@ if ($UserObj->Id && $id ne 'new') {
 
     if  ( ($SetEnabled) and ( $Disabled != $UserObj->Disabled) ) { 
         my  ($code, $msg) = $UserObj->SetDisabled($Disabled);
-        push @results, loc('Enabled status [_1]', loc_fuzzy($msg));
+        push @results, $msg;
     }
 
 

commit 928386787a842aba73d82c8ee744c13af17c83bf

Fix incorrect Queue->SetDisabled introduced in r17674.
git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/3.8/trunk@17839 e417ac7c-1bcc-0310-8ffa-8f5827389a85

lib/RT/Queue_Overlay.pm
@@ -399,7 +399,7 @@ sub SetDisabled {
     my $val = shift;
 
     $RT::Handle->BeginTransaction();
-    my $set_err = $self->PrincipalObj->SetDisabled($val);
+    my $set_err = $self->SUPER::SetDisabled($val);
     unless ($set_err) {
         $RT::Handle->Rollback();
         $RT::Logger->warning("Couldn't ".($val == 1) ? "disable" : "enable"." queue ".$self->PrincipalObj->Id);