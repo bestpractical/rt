4456dbe429e3902a15d107f672d6b31ab3b7b0fb
Ruslan Zakirov <ruz@bestpractical.com>
2009-02-13 17:24:25 +0000

    prefer plural forms on system level
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/3.8/trunk@18369 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/System.pm
+++ lib/RT/System.pm
@@ -129,6 +129,12 @@ sub AvailableRights {
 
     # Build a merged list of all system wide rights, queue rights and group rights.
     my %rights = (%{$RIGHTS}, %{$gr}, %{$qr}, %{$cr});
+
+    # prefer plural forms
+    foreach my $right ( grep !/s$/, keys %rights ) {
+        delete $rights{ $right } if exists $rights{ $right .'s' };
+    }
+
     return(\%rights);
 }
 
