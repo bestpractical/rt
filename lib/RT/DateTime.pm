a4b01c8a3694a2a91d0d7f57c327546d58d60f72
Shawn Moore <sartak@bestpractical.com>
2009-02-25 03:23:03 +0000

    Stringify RT::DateTime objects to "unset" if their epoch is 0
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/3.999/trunk@18583 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/DateTime.pm
+++ lib/RT/DateTime.pm
@@ -8,6 +8,13 @@ use RT::DateTime::Duration;
 
 use constant duration_class => 'RT::DateTime::Duration';
 
+sub _stringify {
+    my $self = shift;
+
+    return "unset" if $self->epoch == 0;
+    return $self->SUPER::_stringify(@_);
+}
+
 sub age {
     my $self  = shift;
     my $until = shift || RT::DateTime->now;
