7358e0fd24ad36b6f1abb3206c0c1836a7dcadb7
Shawn Moore <sartak@bestpractical.com>
2009-02-25 01:59:46 +0000

    Add an RT::DateTime->age method to return the duration from then til now (or some other time)
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/3.999/trunk@18569 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/DateTime.pm
+++ lib/RT/DateTime.pm
@@ -4,5 +4,12 @@ use warnings;
 package RT::DateTime;
 use base 'Jifty::DateTime';
 
+sub age {
+    my $self  = shift;
+    my $until = shift || RT::DateTime->now;
+
+    return $until - $self;
+}
+
 1;
 
