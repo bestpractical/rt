e3e0151c1246ea4a6b25b46c002ab477bb11a0da
Shawn Moore <sartak@bestpractical.com>
2009-03-05 01:17:45 +0000

    UTC not utc
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/3.999/trunk@18673 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/Action/UpdateTicket.pm
+++ lib/RT/Action/UpdateTicket.pm
@@ -30,7 +30,7 @@ sub take_action {
         my $value = $self->argument_value($field);
         if ( defined $value ) {
 
-            # convert date to be as utc
+            # convert date to be as UTC
             my $date = RT::Date->new_from_string($value);
             $self->argument_value( $field, $date->iso );
         }
