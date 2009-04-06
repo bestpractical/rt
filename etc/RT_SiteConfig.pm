1d0f6bcdd30a5c080b570ba62efbd1a6d146f024
Jesse Vincent <jesse@bestpractical.com>
2009-04-06 14:19:18 +0000

    Fixing mistaken siteconfig commit, adding example of second item in list
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/3.8/trunk@19055 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- etc/RT_SiteConfig.pm
+++ etc/RT_SiteConfig.pm
@@ -15,5 +15,5 @@
 #   perl -c /path/to/your/etc/RT_SiteConfig.pm
 
 Set( $rtname, 'example.com');
-Set(@Plugins,(qw(Extension::QuickDelete RT::AjaxyReplyPage)));
+#Set(@Plugins,(qw(Extension::QuickDelete RT::FM)));
 1;
