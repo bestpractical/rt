71993d1e577599e5c12ae1dd909a3d5a954b3420
Kevin Falcone <falcone@bestpractical.com>
2009-04-29 19:03:21 +0000

    remove a bunch of buttons that you shouldn't want/need when writing email
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/3.8/trunk@19371 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- share/html/NoAuth/RichText/FCKeditor/fckconfig.js
+++ share/html/NoAuth/RichText/FCKeditor/fckconfig.js
@@ -100,21 +100,21 @@ FCKConfig.ToolbarLocation = 'In' ;
 
 FCKConfig.ToolbarSets["Default"] = [
 //	['Source','DocProps','-','Save','NewPage','Preview','-','Templates'],
-	['Source','DocProps','-','Preview','-','Templates'],
+	['Source','DocProps'],
 	['Cut','Copy','Paste','PasteText','PasteWord','-','Print','SpellCheck'],
-	['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+	['Undo','Redo','-','Replace','-','SelectAll','RemoveFormat'],
 //	['Form','Checkbox','Radio','TextField','Textarea','Select','Button','ImageButton','HiddenField'],
 	'/',
 	['Bold','Italic','Underline','StrikeThrough','-','Subscript','Superscript'],
 	['OrderedList','UnorderedList','-','Outdent','Indent','Blockquote','CreateDiv'],
 	['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
-	['Link','Unlink','Anchor'],
+	['Link','Unlink'],
 //	['Image','Flash','Table','Rule','Smiley','SpecialChar','PageBreak'],
-	['Table','Rule','SpecialChar','PageBreak'],
+	['Rule','SpecialChar'],
 	'/',
 	['Style','FontFormat','FontName','FontSize'],
 	['TextColor','BGColor'],
-	['FitWindow','ShowBlocks','-','About']		// No comma for the last row.
+	['FitWindow','-','About']		// No comma for the last row.
 ] ;
 
 FCKConfig.ToolbarSets["Basic"] = [
