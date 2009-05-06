24b5c6cc1ca49cdbb6890f5551240cfa9b536f70
Shawn Moore <sartak@bestpractical.com>
2009-05-06 18:30:16 +0000

    Fix %negate heheheh
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/3.999/trunk@19472 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/Collection.pm
+++ lib/RT/Collection.pm
@@ -103,17 +103,17 @@ If NULL is set, also select rows without the named Attribute.
 
 =cut
 
-my %Negate = qw(
-    =		!=
-    !=		=
-    >		<=
-    <		>=
-    >=		<
-    <=		>
-    LIKE	NOT LIKE
-    NOT LIKE	LIKE
-    IS		IS NOT
-    IS NOT	IS
+my %Negate = (
+    '='        => '!=',
+    '!='       => '=',
+    '>'        => '<=',
+    '<'        => '>=',
+    '>='       => '<',
+    '<='       => '>',
+    'LIKE'     => 'NOT LIKE',
+    'NOT LIKE' => 'LIKE',
+    'IS'       => 'IS NOT',
+    'IS NOT'   => 'IS',
 );
 
 sub limit_attribute {
