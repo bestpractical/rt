0e917d7839af7763df6fb5c5c93a0aa8a9b8c895
Jesse Vincent <jesse@bestpractical.com>
2009-05-26 13:33:25 +0000

    Moved Config.pm -> Config.pod for its new role as only a data holder
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rt/3.999/trunk@19756 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/Action/ConfigSystem.pm
+++ lib/RT/Action/ConfigSystem.pm
@@ -44,7 +44,7 @@ sub meta {
     my $meta = {};
     require Pod::POM;
     my $parser = Pod::POM->new;
-    my $pom    = $parser->parse_file( $RT::BASE_PATH . '/lib/RT/Config.pm' )
+    my $pom    = $parser->parse_file( $RT::BASE_PATH . '/lib/RT/Config.pod' )
       or die $parser->error;
     require Pod::POM::View::HTML;
     require Pod::POM::View::Text;
