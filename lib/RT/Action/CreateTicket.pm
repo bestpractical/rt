2579a6529ea4f509ffda6421109f9634b93d5dff
Shawn M Moore <sartak@bestpractical.com>
2009-09-02 08:20:43 -0400

    Recast CreateTicket to be a QueueBased action

--- lib/RT/Action/CreateTicket.pm
+++ lib/RT/Action/CreateTicket.pm
@@ -1,34 +1,23 @@
 package RT::Action::CreateTicket;
 use strict;
 use warnings;
-use base 'Jifty::Action::Record::Create';
+use base 'RT::Action::QueueBased', 'Jifty::Action::Record::Create';
 
 use constant record_class => 'RT::Model::Ticket';
 use constant report_detailed_messages => 1;
 
 use Jifty::Param::Schema;
 use Jifty::Action schema {
-    param queue =>
-        render as 'text',
-        render_mode is 'read',
-        default is defer {
-            my $queue = Jifty->web->request->argument('queue');
-            $queue = $queue->[0] if ref $queue eq 'ARRAY';
-            $queue;
-        };
+    param status =>
+        render as 'select',
+        valid_values are 'new', 'open'; # XXX
 };
 
-sub set_queue {
+sub after_set_queue {
     my $self  = shift;
     my $queue = shift;
-
-    if (!ref($queue)) {
-        my $queue_obj = RT::Model::Queue->new;
-        $queue_obj->load($queue);
-        $queue = $queue_obj;
-    }
-
-    my @valid_statuses = $queue->status_schema->valid;
+    $self->SUPER::after_set_queue(@_);
+    # set valid statuses
 }
 
 1;
