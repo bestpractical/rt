aed96fe83b0bed55ad5c17e76856d7b6d8bd5ff4
Chia-liang Kao <clkao@bestpractical.com>
2009-09-10 12:08:16 +0900

    rulebuilder context object.

--- share/web/static/js/rulebuilder.js
+++ share/web/static/js/rulebuilder.js
@@ -11,7 +11,7 @@ RuleBuilder = function (sel) {
                    that.functions = response;
                    that.init();
                },
-               'json'); // handle errors.
+               'json'); // XXX: handle errors.
 };
 
 RuleBuilder.expressions = [
@@ -28,17 +28,17 @@ RuleBuilder.prototype.init = function () {
     var ebuilder = jQuery(sel);
     var that = this;
 
-    jQuery._div({'class': 'application'})
-            ._h3_().text("New Expression")
-            ._div_({'class': 'application-function function'})
-           ._div_({'class': 'application-params signature'})
-          .div_()
+    this.ebuilder = ebuilder;
+    
+    jQuery._div_({'class': 'context top-context'})
         .appendTo(ebuilder);
 
-    jQuery("#add-expression")
-		.appendTo(jQuery(".application"));
-
-
+    this.top_context = new RuleBuilder.Context(
+        'Bool',
+        jQuery(".top-context").get(0),
+        null,
+        this
+    );
     this.update_expressions();
 
     ebuilder.append('<div class="functions">');
@@ -53,8 +53,9 @@ RuleBuilder.prototype.init = function () {
     jQuery(this.sel+' div.function').click(
         function(e) {
             var func_name = jQuery('span.function-name', this).text();
-            that.current_application = func_name;
-            that.update_application();
+            that.push_application(func_name);
+//            that.current_application = func_name;
+//            that.update_application();
         });
 //    jQuery(this.sel+' div.application').hide();
 	function render_signature(sig) {
@@ -74,6 +75,37 @@ RuleBuilder.prototype.init = function () {
 					});
 			return content;
 	}
+
+    this.focus(this.top_context);
+};
+
+RuleBuilder.prototype.push_application = function(func_name) {
+    this.current_ctx.set_application(func_name, this.functions[func_name]);
+};
+
+
+RuleBuilder.prototype.push_expression = function(expression) {
+    this.current_ctx.set_expression(expression);
+};
+
+RuleBuilder.prototype.focus = function(ctx) {
+    if( this.current_ctx )
+        jQuery(this.current_ctx.element).removeClass('current');
+
+    this.current_ctx = ctx;
+    jQuery(this.current_ctx.element).addClass('current');
+    var type = this.current_ctx.expected_type;
+    jQuery('.functions .return-type', this.ebuilder).removeClass('matched')
+                                                     .addClass('unmatched');
+
+    jQuery('.expressions .return-type', this.ebuilder).removeClass('matched')
+                                                   .addClass('unmatched');
+
+    jQuery('.functions .return-type:contains('+type+')', this.ebuilder)
+    .addClass('matched');
+
+    jQuery('.expressions .return-type:contains('+type+')', this.ebuilder)
+    .addClass('matched');
 };
 
 RuleBuilder.prototype.update_expressions = function() {
@@ -93,23 +125,14 @@ RuleBuilder.prototype.update_expressions = function() {
     jQuery.each(this.expressions,
                 function(idx, val) {
                     jQuery._div({'class': 'expression ret_'+val.type})
-                            ._span_({ 'class': 'type' }).text(val.type)
+                            ._span_({ 'class': 'return-type' }).text(val.type)
                             ._span_({ 'class': 'expression-text' }).text(val.expression)
                           .div_().click(function(e) {
-                        if (that.current_application_param != null) {
-                            if (val.type != 
-                                that.functions[that.current_application].parameters[that.current_application_param].type) {
-                                alert("type mismatch: "+val.type+" vs "+that.functions[that.current_application].parameters[that.current_application_param].type);
-                                return;
-                            }
-                            jQuery(that.sel+ ' .param:eq('+parseInt(idx)+')').removeClass('param-placeholder').text(val.expression);
-                        }
-                        else {
-                            alert("must select param first");
-                        }
+                              that.push_expression(val);
                           })
                     .appendTo(expressions_div);
                 });
+
 
     var options = {
         onClick: function(e,item) { x=e;y=item },
@@ -123,14 +146,14 @@ RuleBuilder.prototype.update_expressions = function() {
         {src: 'test2', subMenu: [   {src: 'sub 1'},
                                     {src: 'sub 2' },
                                     {src: 'sub 3'}]}];
-    jQuery('.ret_'+e_sel('RT::Model::Ticket')).menu(options, items);
+//    jQuery('.ret_'+e_sel('RT::Model::Ticket')).menu(options, items);
 };
 
 
 RuleBuilder.prototype.update_application = function () {
     /* might be an expression too */
     jQuery(this.sel+' div.application-function').html(this.current_application);
-    jQuery(this.sel+' div.application').show();
+    jQuery(this.sel+' div.application-old').show();
     jQuery(this.sel+' div.application-params').html('');
     var params = jQuery(this.sel+' div.application-params');
     var that = this;
@@ -180,5 +203,87 @@ RuleBuilder.prototype.filter_return_type = function (type) {
 RuleBuilder.prototype.filter_expression_type = function (type) {
     jQuery(this.sel+' .expression').show();
     jQuery(this.sel+' .expression:not(.ret_'+e_sel(type)+')').hide();
+};
+
+
+RuleBuilder.Context = function(expected_type, element, parent, rb) {
+    this.expected_type = expected_type;
+    this.element = element;
+    this.parent = parent;
+    this.rb = rb;
+
+    var that = this;
+    jQuery(this.element).click(function(e) { rb.focus(that); return false });
+    jQuery._span_({ 'class': 'return-type'})
+          .text(expected_type)
+          .appendTo(this.element);
+};
+
+RuleBuilder.Context.prototype.update_return_type = function(type) {
+    if (this.expected_type == type) {
+        jQuery("span.return-type", this.element).removeClass("unmatched").addClass("matched");
+    }
+    else {
+        jQuery("span.return-type", this.element).removeClass("matched").addClass("unmatched");
+    }
+}
+
+RuleBuilder.Context.prototype.clear = function() {
+    jQuery('div.application', this.element).remove();
+    jQuery('span.expression', this.element).remove();
+}
+
+RuleBuilder.Context.prototype.set_expression = function(expression) {
+    this.clear();
+    this.expression = expression.expression;
+    this.update_return_type(expression.type);
+
+    jQuery._span_({ 'class': 'expression'})
+          .text(this.expression)
+          .appendTo(this.element);
+}
+
+
+
+RuleBuilder.Context.prototype.set_application = function(func_name, func) {
+    this.clear();
+    this.func = func;
+    this.children = [];
+    this.update_return_type(func.return_type);
+    jQuery._div({'class': 'application'})
+            ._div_({'class': 'application-function function'})
+           ._div_({'class': 'application-params signature'})
+          .div_()
+        .appendTo(this.element);
+
+//    jQuery(this.sel+' div.application-function').html(this.current_application);
+    jQuery('div.application-function',this.element).html(func_name);
+    jQuery('div.application', this.element).show();
+    jQuery('div.application-params', this.element).html('');
+    var params = jQuery('div.application-params', this.element);
+    var that = this;
+    jQuery.each(func.parameters,
+                function(idx, val) {
+                    var x = jQuery._div_({'class': 'context'})
+                    .appendTo(params);
+
+                    var child = new RuleBuilder.Context(val.type, x, that, that.rb);
+                    that.children.push(child);
+                });
+    if (this.children.length) {
+        this.rb.focus(this.children[0]);
+    }
+};
+
+RuleBuilder.Context.prototype.init = function() {
+
+};
+
+jQuery.fn.sort = function() {
+    return this.pushStack( [].sort.apply( this, arguments ), []);
+};
+
+function sortAlpha(a,b){
+     return a.innerHTML > b.innerHTML ? 1 : -1;
 };
 
