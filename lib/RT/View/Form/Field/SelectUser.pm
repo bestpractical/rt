361ffbaf1852a07f54fcdf463a5e105d7de7d191
Shawn M Moore <sartak@bestpractical.com>
2009-10-21 18:04:54 -0400

    Transplant mason ShowUser logic into a jifty form field

--- lib/RT/View/Form/Field/SelectUser.pm
+++ lib/RT/View/Form/Field/SelectUser.pm
@@ -2,6 +2,7 @@ package RT::View::Form::Field::SelectUser;
 use warnings;
 use strict;
 use base 'Jifty::Web::Form::Field::Select';
+use Email::Address;
 
 sub _render_user {
     my $self = shift;
@@ -13,7 +14,58 @@ sub _render_user {
         $user = $user_object;
     }
 
-    return $user->name;
+    my $style = RT->config->get('username_format', Jifty->web->current_user);
+    return $self->_render_user_concise($user)
+        if $style eq 'concise';
+    return $self->_render_user_verbose($user);
+}
+
+sub _render_user_concise {
+    my $self = shift;
+    my $user = shift;
+
+    if ($user->privileged) {
+        return $user->real_name
+            || $user->nickname
+            || $user->name
+            || $user->email;
+    }
+
+    return $user->email
+        || $user->name
+        || $user->real_name
+        || $user->nickname;
+}
+
+sub _render_user_verbose {
+    my $self = shift;
+    my $user = shift;
+
+    my ($phrase, $comment);
+    my $addr = $user->email;
+
+    $phrase = $user->real_name
+        if $user->real_name
+        && lc $user->real_name ne lc $addr;
+
+    $comment = $user->name
+        if lc $user->name ne lc $addr;
+
+    $comment = "($comment)"
+        if defined $comment and length $comment;
+
+    my $address = Email::Address->new($phrase, $addr, $comment);
+
+    $address->comment('')
+        if $comment && lc $address->user eq lc $comment;
+
+    if ( $phrase and my ($l, $r) = ($phrase =~ /^(\w+) (\w+)$/) ) {
+        $address->phrase('')
+            if $address->user =~ /^\Q$l\E.\Q$r\E$/
+            || $address->user =~ /^\Q$r\E.\Q$l\E$/;
+    }
+
+    return $address->format;
 }
 
 sub _render_select_values {
