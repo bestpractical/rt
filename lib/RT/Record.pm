9307bb81790b903615d106226f3b1f71358c0945
sunnavy <sunnavy@bestpractical.com>
2009-11-03 09:49:00 +0800

    remove misleading comment

--- lib/RT/Record.pm
+++ lib/RT/Record.pm
@@ -1228,7 +1228,7 @@ sub FormatType{
 		 @_
 	       );
     $args{Type} =~ s/([A-Z])/" " . lc $1/ge;
-    $args{Type} =~ s/^\s+//; # in case the first letter is A-Z
+    $args{Type} =~ s/^\s+//;
     return $args{Type};
 }
 
