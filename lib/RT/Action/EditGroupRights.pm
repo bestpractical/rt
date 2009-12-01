670bd315c0ba97cd1ddf88f3f64a0e6c805eb3d6
sunnavy <sunnavy@bestpractical.com>
2009-12-01 13:49:07 +0800

    add EditGroupRights action

---
+++ lib/RT/Action/EditGroupRights.pm
@@ -0,0 +1,69 @@
+
+use strict;
+use warnings;
+
+package RT::Action::EditGroupRights;
+use base qw/RT::Action::EditRights/;
+use Scalar::Defer;
+
+sub arguments {
+    my $self = shift;
+    return {} unless $self->object;
+    my $args = {};
+    $args->{object_id} = {
+        render_as     => 'hidden',
+        default_value => $self->object->id,
+    };
+    $args->{object_type} = {
+        render_as     => 'hidden',
+        default_value => ref $self->object,
+    };
+
+    my @groups;
+
+    {
+        my $groups =
+          RT::Model::GroupCollection->new(
+            current_user => Jifty->web->current_user );
+        $groups->limit_to_system_internal_groups();
+        while ( my $group = $groups->next ) {
+            push @groups, $group;
+        }
+    }
+
+    {
+        my $groups =
+          RT::Model::GroupCollection->new(
+            current_user => Jifty->web->current_user );
+        $groups->limit_to_user_defined_groups();
+        while ( my $group = $groups->next ) {
+            push @groups, $group;
+        }
+    }
+
+    {
+        my $groups =
+          RT::Model::GroupCollection->new(
+            current_user => Jifty->web->current_user );
+        $groups->limit_to_roles( object => $self->object );
+        while ( my $group = $groups->next ) {
+            push @groups, $group;
+        }
+    }
+
+    for my $group ( @groups ) {
+        my $name = 'rights_' . $group->principal_id;
+        $args->{$name} = {
+            default_value    => defer {
+                $self->default_value($group->principal_id) },
+            available_values => defer { $self->available_values },
+            render_as        => 'Checkboxes',
+            multiple         => 1,
+            label => $group->name || $group->type,
+        };
+    }
+    return $args;
+}
+
+1;
+
