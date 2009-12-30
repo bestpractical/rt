1ad2b302ca99c80b1fec78c37612770367176a56
sunnavy <sunnavy@bestpractical.com>
2009-12-30 14:58:23 +0800

    refactor CreateQueue action to use schema

--- lib/RT/Action/CreateQueue.pm
+++ lib/RT/Action/CreateQueue.pm
@@ -2,38 +2,18 @@ package RT::Action::CreateQueue;
 use strict;
 use warnings;
 
-use base qw/Jifty::Action::Record::Create/;
+use base qw/Jifty::Action::Record::Create /;
 
 sub record_class { 'RT::Model::Queue' }
 
 use constant report_detailed_messages => 1;
 
-sub arguments {
-    my $self = shift;
-
-    my $args = $self->SUPER::arguments();
-    for (qw/sign encrypt/) {
-        $args->{$_} = {
-            default_value => 0,
-            render_as     => 'checkbox',
-            label         => _($_),
-        };
-    }
-    return $args;
-}
-
-=head2 take_action
-
-=cut
-
-sub take_action {
-    my $self = shift;
-    $self->SUPER::take_action;
-    for (qw/sign encrypt/) {
-        my $method = "set_$_";
-        $self->record->$method( $self->argument_value($_) );
-    }
-    return 1;
-}
+use Jifty::Param::Schema;
+use Jifty::Action schema {
+    param sign =>
+        render as 'Checkbox';
+    param encrypt =>
+        render as 'Checkbox',
+};
 
 1;
