7a7e813907743753773e7aade430f5e2869b2425
Ruslan Zakirov <ruz@bestpractical.com>
2010-01-21 22:55:56 +0300

    without parents we can not complete detection of some parts

--- lib/RT/Crypt/GnuPG.pm
+++ lib/RT/Crypt/GnuPG.pm
@@ -1032,7 +1032,7 @@ sub FindScatteredParts {
             push @res, {
                 Type      => 'signed',
                 Format    => 'Attachment',
-                Top       => $entity,
+                Top       => $args{'Parents'}{$data_part_in},
                 Data      => $data_part_in,
                 Signature => $sig_part,
             };
@@ -1052,7 +1052,7 @@ sub FindScatteredParts {
         push @res, {
             Type    => 'encrypted',
             Format  => 'Attachment',
-            Top     => $entity,
+            Top     => $args{'Parents'}{$part},
             Data    => $part,
         };
     }
