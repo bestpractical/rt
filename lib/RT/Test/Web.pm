f5ba076e3c959f57eb9d63b8312b084ad023095e
sunnavy <sunnavy@bestpractical.com>
2010-09-02 12:34:33 +0800

    tiny fix

--- lib/RT/Test/Web.pm
+++ lib/RT/Test/Web.pm
@@ -152,7 +152,7 @@ sub goto_create_ticket {
         die "not yet implemented";
     }
 
-    $self->get($self->rt_base_url . '/Ticket/Create.html?Queue='.$id);
+    $self->get($self->rt_base_url . 'Ticket/Create.html?Queue='.$id);
 
     return 1;
 }
