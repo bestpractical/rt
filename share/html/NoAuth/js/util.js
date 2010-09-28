cae63fc21aef7e836324cb2b969117b838abaf33
sunnavy <sunnavy@bestpractical.com>
2010-09-28 11:11:35 +0800

    change bookmark toggle from id to class so we can toggle more elements

--- share/html/NoAuth/js/util.js
+++ share/html/NoAuth/js/util.js
@@ -315,3 +315,26 @@ function checkboxToInput(target,checkbox,val){
     }
 }
 
+function toggleTicketBookmark( id, url ) {
+    var elements = $$("span.toggle-"+id);
+    if ( elements.length ) {
+        if ( elements.length == 1 ) {
+            new Ajax.Request(url, {
+                onSuccess: function(response) {
+                    $(elements[0]).replace(response.responseText);
+                }
+                }
+            );
+        }
+        else {
+            new Ajax.Request(url);
+            new Ajax.Request(url+'&Toggle=0', {
+                onSuccess: function(response) {
+                    elements.each( function( item ) {
+                        item.replace(response.responseText);
+                    })
+                }
+            });
+        }
+    }
+}
