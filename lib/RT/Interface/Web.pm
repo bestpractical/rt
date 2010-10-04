129baa225d589a3373435ca8b250423f6b5cdd99
Alex Vandiver <alexmv@bestpractical.com>
2010-10-04 14:32:06 -0400

    Ensure that all internal messages, including create txns, have message-ids

--- lib/RT/Interface/Web.pm
+++ lib/RT/Interface/Web.pm
@@ -1284,6 +1284,7 @@ sub MakeMIMEEntity {
     );
     my $Message = MIME::Entity->build(
         Type    => 'multipart/mixed',
+        "Message-Id" => RT::Interface::Email::GenMessageId,
         map { $_ => Encode::encode_utf8( $args{ $_} ) }
             grep defined $args{$_}, qw(Subject From Cc)
     );
