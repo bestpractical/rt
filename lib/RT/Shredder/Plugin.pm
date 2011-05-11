bestpractical/rt

commit 695d1acb59e44938ac9e18bf14a11886cfa03993

Replace 3384147 with a less error-prone explicit loop
An else-less 'if' inside a map produces the single value '' when the
if is false (the return value of the test).  In this context, this
would likely lead to an odd number of elements in the hashref, instead
of the pre-3384147 result of simply having a '' key.  Additionally,
perlcritic warns (not incorrectly) about the construct.

Replace the map with an explicit for loop, which makes perlcritic
happy, in addition to solving the "odd number of elements" bug above.

lib/RT/Shredder/Plugin.pm
@@ -127,7 +127,10 @@ sub List
         push @files, glob $mask;
     }
 
-    my %res = map { if ( $_ =~ m/([^\\\/]+)\.pm$/) { $1 => $_ } } reverse @files;
+    my %res;
+    for my $f (reverse @files) {
+        $res{$1} = $_ if $f =~ /([^\\\/]+)\.pm$/;
+    }
 
     return %res unless $type;
 

commit 5add26c8d76d8a25626ba02ebafe0734fb34619f

Fix RT::Shredder::Plugin->List to correctly return paths
The refactoring in 695d1ac broke the return value because of the use of
the $f loop variable.

lib/RT/Shredder/Plugin.pm
@@ -129,7 +129,7 @@ sub List
 
     my %res;
     for my $f (reverse @files) {
-        $res{$1} = $_ if $f =~ /([^\\\/]+)\.pm$/;
+        $res{$1} = $f if $f =~ /([^\\\/]+)\.pm$/;
     }
 
     return %res unless $type;