1af3c867110e2bd824a0f20ef0085bd0da5a8286
Alex Vandiver <alexmv@bestpractical.com>
2011-06-08 16:12:38 -0400

    Remove last mention of ModifyTicketStatus

--- lib/RT/Queue.pm
+++ lib/RT/Queue.pm
@@ -114,7 +114,6 @@ our $RIGHTS = {
     CommentOnTicket     => 'Comment on tickets',                                        # loc_pair
     OwnTicket           => 'Own tickets',                                               # loc_pair
     ModifyTicket        => 'Modify tickets',                                            # loc_pair
-    ModifyTicketStatus  => 'Modify ticket status',                                      # loc_pair
     DeleteTicket        => 'Delete tickets',                                            # loc_pair
     TakeTicket          => 'Take tickets',                                              # loc_pair
     StealTicket         => 'Steal tickets',                                             # loc_pair
