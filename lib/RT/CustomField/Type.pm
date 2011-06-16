f5fb8fdba6c67142e69f0f92666b5f3bd7b06981
Chia-liang Kao <clkao@bestpractical.com>
2011-06-16 22:25:55 +0800

    document pluggable CF type

--- lib/RT/CustomField/Type.pm
+++ lib/RT/CustomField/Type.pm
@@ -2,10 +2,62 @@ package RT::CustomField::Type;
 use strict;
 use warnings;
 
+=head1 NAME
+
+  RT::CustomField::Type - Custom Field Type definition
+
+=head1 DESCRIPTION
+
+You can define types for RT's custom field.  For example, if you want
+to create a type ImageWithCatpion, which consists of an uploaded
+image, and an user-entered caption.  You can define how the CF is
+stored, displayed, or even searched.
+
+To define a type, you want to register it in your subclass of
+L<RT::CustomField::Type>:
+
+  RT::CustomField->RegisterType(
+    ImageWithCaption => {
+        sort_order => 55,
+        selection_type => 0,
+        labels         => [
+                    'Upload multiple images with caption',   # loc
+                    'Upload one image with caption',         # loc
+                    'Upload up to [_1] images with caption', # loc
+                  ],
+        class => 'RT::CustomField::Type::ImageWithCaption',
+    }
+  );
+
+You should then override the following methods to customize the type-specific behavior.
+
+=head1 methods for storage and display
+
+for storage, per-object custom field values are stored as
+L<RT::ObjectCustomFieldValue> objects, and the commonly used fields
+are C<ContentType>, C<Content> and C<LargeContent>.
+
+=head2 $class->CanonicalizeForCreate($cf, $ocfv, $args)
+
+Called when the value is being validated or created.  You should
+example C<$args> and make modifications before it is passed to
+C<RT::ObjectCustomFieldValue-E<gt>Create>.  C<$ocfv> can be used to get
+CurrentUser, should you need to access it.
+
+=cut
+
 sub CanonicalizeForCreate {
     my ($self, $cf, $ocfv, $args) = @_;
     return wantarray ? (1) : 1;
 }
+
+=head2 $class->Stringify($ocfv)
+
+for displaying the custom field value in summary.  The default
+behavior pulls C<Content> if it exists, otherwise C<LargeContent> if
+C<ContentType> is text/plain.
+
+=cut
 
 sub Stringify {
     my ($self, $ocfv) = @_;
@@ -18,10 +70,27 @@ sub Stringify {
     }
 }
 
-sub CanonicalizeForSearch {
-    my ($self, $cf, $value, $op ) = @_;
-    return $value;
-}
+=head1 methods for web interaction
+
+The mason component F<share/html/Elements/EditCustomField$TypeName>
+will be used for creating or editing a custom field value of type
+C<$TypeName>.  F<share/html/Elements/ShowCustomField$TypeName> will be
+used for displaying the value.
+
+=head2 $class->CreateArgsFromWebArgs($cf, $web_args)
+
+This is used for processing cf values when creating a new ticket.
+
+The C<$web_args> are the form values without prefix, from the type's
+corresponding form.
+
+The value returned will be used as the value of the cf when calling
+C<RT::Ticket-E<gt>Create>.  An entry or an arrayref of entries can be
+returned.  Each entry can be a plaintext value, or a hashref for
+creating L<RT::ObjectCustomFieldValue> where the C<Content> field
+should be passed in as C<Value>, for historical reasons.
+
+=cut
 
 sub CreateArgsFromWebArgs {
     my ($self, $cf, $web_args) = @_;
@@ -40,7 +109,7 @@ sub CreateArgsFromWebArgs {
 
 =head2 ValuesFromWeb C<$args>
 
-Parse the args passed in from web
+Helper method for parsing the args passed in from web.
 
 =cut
 
@@ -65,10 +134,45 @@ sub ValuesFromWeb {
     } grep defined, @values;
 }
 
+=head1 methods for search and search builder
+
+By default the search behavor works on the custom field value's
+C<Content>, and provides some default op for the user to choose from
+in the search builder.  If you want to perform different types of
+search, you need to override some of the methods here.
+
+=head2 $class->CanonicalizeForSearch($cf, $value, $op)
+
+Canonicalize user-entered value for searching.
+
+=cut
+
+sub CanonicalizeForSearch {
+    my ($self, $cf, $value, $op ) = @_;
+    return $value;
+}
+
+=head2 $class->Limit($tickets, $field, $value, $op, %rest)
+
+If you want to perform search other than simple C<==> and C<!=> on
+C<Content> of the custom field value, you need to write your own
+DBIx::SearchBuilder rules here.  see
+L<RT::CustomField::Type::IPAddressRange> for examples.
+
+=cut
 
 sub Limit {
     return;
 }
+
+=head2 $class->SearchBuilderUIArguments($cf)
+
+Returns a hash where key C<Op> is a hashref of spec for the mason
+component for showing operators in search builder, and optionally key
+C<Value> for mason conponents for entering/selecting values during
+search.
+
+=cut
 
 sub SearchBuilderUIArguments {
     my ($self, $cf) = @_;
@@ -90,4 +194,11 @@ sub SearchBuilderUIArguments {
         });
 }
 
+=head1 SEE ALSO
+
+L<RT::CustomField::Type::IPAddress> and L<RT::CustomField::Type::Date> for examples.
+
+=cut
+
 1;
+
