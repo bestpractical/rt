4182d96e662d6c1cbb60c3a3c41d84e4a6267f75
Alex Vandiver <alexmv@bestpractical.com>
2011-07-06 19:39:48 -0400

    Resolve mixed high and low precedence boolean operators, in favor of low-precedence

--- lib/RT/I18N.pm
+++ lib/RT/I18N.pm
@@ -492,7 +492,7 @@ sub _FindOrGuessCharset {
         return _CanonicalizeCharset($charset);
     }
 
-    if ( !$head_only and $head->mime_type =~ m{^text/} ) {
+    if ( not $head_only and $head->mime_type =~ m{^text/} ) {
         my $body = $entity->bodyhandle or return;
         return _GuessCharset( $body->as_string );
     }
