14202ebc7a27c639c217a2dce5301180cd01719a
Alex Vandiver <alexmv@bestpractical.com>
2011-07-26 16:30:25 -0400

    Catch errors on close()

--- lib/RT/Crypt/GnuPG.pm
+++ lib/RT/Crypt/GnuPG.pm
@@ -443,7 +443,7 @@ sub CallGnuPG {
             } elsif (defined $content) {
                 $handle{'stdin'}->print( $content );
             }
-            close $handle{'stdin'} or die "Can't close gnupg handle: $!";
+            close $handle{'stdin'} or die "Can't close gnupg input handle: $!";
             $args{Callback}->(%handle) if $args{Callback};
         }
         waitpid $pid, 0;
@@ -451,7 +451,7 @@ sub CallGnuPG {
     my $err = $@;
     if ($args{Output}) {
         push @{$args{Output}}, readline $handle{stdout};
-        close $handle{stdout};
+        close $handle{stdout} or $err ||= "Can't close gnupg output handle: $!";
     }
 
     return Finalize( $err, \%handle );
@@ -467,7 +467,7 @@ sub Finalize {
     foreach ( qw(stderr logger status) ) {
         $res{$_} = do { local $/ = undef; readline $handle{$_} };
         delete $res{$_} unless $res{$_} && $res{$_} =~ /\S/s;
-        close $handle{$_};
+        close $handle{$_} or $err ||= "Can't close gnupg $_ handle: $!";
     }
     $RT::Logger->debug( $res{'status'} ) if $res{'status'};
     $RT::Logger->warning( $res{'stderr'} ) if $res{'stderr'};
@@ -2038,7 +2038,7 @@ sub Probe {
                 handles  => $handles
             )
         };
-        close $handle{'stdin'};
+        close $handle{'stdin'} or die "Can't close gnupg input handle: $!";
         waitpid $pid, 0;
     };
     if ( $@ ) {
@@ -2060,7 +2060,7 @@ sub Probe {
         foreach ( qw(stderr logger status) ) {
             my $tmp = do { local $/ = undef; readline $handle{$_} };
             next unless $tmp && $tmp =~ /\S/s;
-            close $handle{$_};
+            close $handle{$_} or $tmp .= "\nFailed to close: $!";
             $msg .= "\n$_:\n$tmp\n";
         }
         $RT::Logger->debug( $msg );
