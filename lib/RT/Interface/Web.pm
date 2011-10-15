9702bf5b50cdf14b5280f2dc815a2646e4d899dd
Jason May <jasonmay@bestpractical.com>
2011-10-14 21:11:45 -0400

    Check that the owner arg is an integer before comparing numerically
    This silences a warning when a user has AutocompleteOwners enabled and
    submits an owner that doesn't exist (which fails to load any sort of
    user and leaves the string untouched).

--- lib/RT/Interface/Web.pm
+++ lib/RT/Interface/Web.pm
@@ -2024,7 +2024,9 @@ sub ProcessTicketBasics {
     );
 
     # We special case owner changing, so we can use ForceOwnerChange
-    if ( $ARGSRef->{'Owner'} && ( $OrigOwner != $ARGSRef->{'Owner'} ) ) {
+    if ( $ARGSRef->{'Owner'}
+      && $ARGSRef->{'Owner'} !~ /\D/
+      && ( $OrigOwner != $ARGSRef->{'Owner'} ) ) {
         my ($ChownType);
         if ( $ARGSRef->{'ForceOwnerChange'} ) {
             $ChownType = "Force";
