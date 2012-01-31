8dc2f451dddbc2191dcf39d74670050c21654f81
Alex Vandiver <alexmv@bestpractical.com>
2012-01-31 13:55:28 -0500

    Articles may have links on them

--- lib/RT/Article.pm
+++ lib/RT/Article.pm
@@ -867,6 +867,17 @@ sub FindDependencies {
     my ($walker, $deps) = @_;
 
     $self->SUPER::FindDependencies($walker, $deps);
+
+    # Links
+    my $links = RT::Links->new( $self->CurrentUser );
+    $links->Limit(
+        SUBCLAUSE       => "either",
+        FIELD           => $_,
+        VALUE           => $self->URI,
+        ENTRYAGGREGATOR => 'OR'
+    ) for qw/Base Target/;
+    $deps->Add( in => $links );
+
     $deps->Add( out => $self->ClassObj );
     $deps->Add( in => $self->Topics );
 }
