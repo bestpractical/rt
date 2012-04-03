82c9189f529dc65c1874a15e5379f5f9d11593f7
Alex Vandiver <alexmv@bestpractical.com>
2012-04-02 22:17:35 -0400

    Consistently escape all possibly suspect characters in JS strings
    This resolves part of CVE-2011-2083.

--- lib/RT/Interface/Web.pm
+++ lib/RT/Interface/Web.pm
@@ -110,6 +110,25 @@ sub EscapeURI {
 
 # }}}
 
+sub _encode_surrogates {
+    my $uni = $_[0] - 0x10000;
+    return ($uni /  0x400 + 0xD800, $uni % 0x400 + 0xDC00);
+}
+
+sub EscapeJS {
+    my $ref = shift;
+    return unless defined $$ref;
+
+    $$ref = "'" . join('',
+                 map {
+                     chr($_) =~ /[a-zA-Z0-9]/ ? chr($_) :
+                     $_  <= 255   ? sprintf("\\x%02X", $_) :
+                     $_  <= 65535 ? sprintf("\\u%04X", $_) :
+                     sprintf("\\u%X\\u%X", _encode_surrogates($_))
+                 } unpack('U*', $$ref))
+        . "'";
+}
+
 # {{{ WebCanonicalizeInfo
 
 =head2 WebCanonicalizeInfo();
