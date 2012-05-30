bestpractical/rt

commit b4d0382ef252eba9478e80de48ad0ae2d2c3ed41

Ensure that we are email-unique, as well

lib/RT/User.pm
@@ -2347,10 +2347,16 @@ sub PreInflate {
     my $disabled      = delete $data->{Disabled};
 
     my $obj = RT::User->new( RT->SystemUser );
-    $obj->Load( $data->{Name} );
-
+    $obj->LoadByCols( Name => $data->{Name} );
+    $obj->LoadByCols( Email => $data->{Email} ) unless $obj->Id;
     if ($obj->Id) {
         # User already exists -- merge
+
+        # XXX: We might be merging a privileged user into an unpriv one,
+        # in which case we should probably promote the unpriv user to
+        # being privileged.  Of course, we don't know if the user being
+        # imported is privileged yet, as its group memberships show up
+        # later in the stream...
         $importer->MergeValues($obj, $data);
         $importer->SkipTransactions( $uid );
 

commit 91f640a3b3ac77f8c684df4d47045fe2b704449e

The column is called EmailAddress
The change in b4d0382 caused failures during import on users, as it
mis-named the column to attempt to load on.

lib/RT/User.pm
@@ -2348,7 +2348,7 @@ sub PreInflate {
 
     my $obj = RT::User->new( RT->SystemUser );
     $obj->LoadByCols( Name => $data->{Name} );
-    $obj->LoadByCols( Email => $data->{Email} ) unless $obj->Id;
+    $obj->LoadByEmail( $data->{EmailAddress} ) unless $obj->Id;
     if ($obj->Id) {
         # User already exists -- merge
 