b550503edbc50261655d467999124c383a7f5c0e
Thomas Sibley <trs@bestpractical.com>
2012-06-28 13:46:51 -0700

    Backport a jQuery UI Mouse patch to fix sliders on IE 9
    The bug most obviously affected the hour and minute sliders in the time
    picker.
    
    The upstream fix was first released with jQuery UI 1.8.6 as a result of
    these three commits: 8fcf58e2, 7eaedaad, ec73a59c.  Patching in-place is
    preferred because any version of jQuery UI we update to in the future
    will already contain the fix.

--- devel/third-party/jquery.ui.mouse-1.8.4.js
+++ devel/third-party/jquery.ui.mouse-1.8.4.js
@@ -97,7 +97,7 @@ $.widget("ui.mouse", {
 
 	_mouseMove: function(event) {
 		// IE mouseup check - mouseup happened when mouse was out of window
-		if ($.browser.msie && !event.button) {
+		if ($.browser.msie && !(document.documentMode >= 9) && !event.button) {
 			return this._mouseUp(event);
 		}
 
