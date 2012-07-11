673337b97b9a3c81959dd38ab970f44d40a2a6ce
Thomas Sibley <trs@bestpractical.com>
2012-07-11 09:11:40 -0700

    WebExternalOnly was renamed to WebFallbackToInternalAuth
    Although it was caught in 21874f2c back in 2006, that commit was
    accidentally reverted in 3027bf06 during a merge.
    
    This commit applies effectively the same diff as 21874f2c.
    (cherry picked from commit 71c59d6157d08be514a00b359290d7f7486a0f92)

--- lib/RT/Interface/Web.pm
+++ lib/RT/Interface/Web.pm
@@ -586,7 +586,7 @@ sub AttemptExternalAuth {
             delete $HTML::Mason::Commands::session{'CurrentUser'};
             $user = $orig_user;
 
-            if ( RT->Config->Get('WebExternalOnly') ) {
+            unless ( RT->Config->Get('WebFallbackToInternalAuth') ) {
                 TangentForLoginWithError('You are not an authorized user');
             }
         }
