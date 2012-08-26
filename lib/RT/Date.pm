92c131a6193d637dc8a21ca412deee3f2e42808e
Ruslan Zakirov <ruz@bestpractical.com>
2012-08-26 23:41:43 +0400

    separate elsif branch for one SQL/ISO format
    This format we use when fetch from DB. All fields in the string
    are mandatory and it's treated as UTC time.
    
    For this particular and very common case we can do
    faster conversion to unix time. Other elsif branch
    still handles more variants.
    
    Our existing tests dictate that we normalize old dates
    (negative unix time) to zero, as well as, invalid dates.
    I think that we can avoid validation and use timegm_nocheck.

--- lib/RT/Date.pm
+++ lib/RT/Date.pm
@@ -171,6 +171,15 @@ sub Set {
     if ( $format eq 'unix' ) {
         return $self->Unix( $args{'Value'} );
     }
+    elsif (
+        ($format eq 'sql' || $format eq 'iso')
+        && $args{'Value'} =~ /^(\d{4})-(\d\d)-(\d\d) (\d\d):(\d\d):(\d\d)$/
+    ) {
+        local $@;
+        my $u = eval { Time::Local::timegm($6, $5, $4, $3, $2-1, $1) } || 0;
+        $RT::Logger->warning("Invalid date $args{'Value'}: $@") if $@ && !$u;
+        return $self->Unix( $u > 0 ? $u : 0 );
+    }
     elsif ( $format =~ /^(sql|datemanip|iso)$/ ) {
         $args{'Value'} =~ s!/!-!g;
 
