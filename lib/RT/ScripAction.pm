373ae8264f3bede0f562e22a6ab094e3d872ca95
Ruslan Zakirov <ruz@bestpractical.com>
2012-09-13 13:41:21 +0400

    tidy, no code change, tiny doc changes
    retab, indent, drop useless space only and empty lines,
    drop some comments, several very small doc changes

--- lib/RT/ScripAction.pm
+++ lib/RT/ScripAction.pm
@@ -48,21 +48,13 @@
 
 =head1 NAME
 
-  RT::ScripAction - RT Action object
-
-=head1 SYNOPSIS
-
-  use RT::ScripAction;
-
+RT::ScripAction - RT Action object
 
 =head1 DESCRIPTION
 
-This module should never be called directly by client code. it's an internal module which
-should only be accessed through exported APIs in other modules.
-
-
-
-=head1 METHODS
+This module should never be called directly by client code. it's an
+internal module which should only be accessed through exported APIs
+in other modules.
 
 =cut
 
@@ -76,29 +68,29 @@ use base 'RT::Record';
 
 sub Table {'ScripActions'}
 
-
-
 use RT::Template;
 
 sub _Accessible  {
     my $self = shift;
-    my %Cols = ( Name  => 'read',
-		 Description => 'read',
-		 ExecModule  => 'read',
-		 Argument  => 'read',
-		 Creator => 'read/auto',
-		 Created => 'read/auto',
-		 LastUpdatedBy => 'read/auto',
-		 LastUpdated => 'read/auto'
-       );
+    my %Cols = (
+        Name  => 'read',
+        Description => 'read',
+        ExecModule  => 'read',
+        Argument  => 'read',
+        Creator => 'read/auto',
+        Created => 'read/auto',
+        LastUpdatedBy => 'read/auto',
+        LastUpdated => 'read/auto'
+    );
     return($self->SUPER::_Accessible(@_, %Cols));
 }
 
 
+=head1 METHODS
+
 =head2 Create
 
-Takes a hash. Creates a new Action entry.  should be better
-documented.
+Takes a hash. Creates a new Action entry.
 
 =cut
 
@@ -110,7 +102,6 @@ sub Create  {
 
 sub Delete  {
     my $self = shift;
-    
     return (0, "ScripAction->Delete not implemented");
 }
 
@@ -126,25 +117,23 @@ Returns: Id, Error Message
 sub Load  {
     my $self = shift;
     my $identifier = shift;
-    
+
     if (!$identifier) {
-	return (0, $self->loc('Input error'));
-    }	    
-    
+        return (0, $self->loc('Input error'));
+    }
+
     my ($ok, $msg);
     if ($identifier !~ /\D/) {
-	($ok, $msg) = $self->SUPER::Load($identifier);
+        ($ok, $msg) = $self->SUPER::Load($identifier);
     }
     else {
-	($ok, $msg) = $self->LoadByCol('Name', $identifier);
-	
+        ($ok, $msg) = $self->LoadByCol('Name', $identifier);
     }
 
     if (@_) {
-	# Set the template Id to the passed in template    
-	my $template = shift;
-	
-	$self->{'Template'} = $template;
+        # Set the template Id to the passed in template
+        my $template = shift;
+        $self->{'Template'} = $template;
     }
 
     return ($ok, $msg);
@@ -153,42 +142,42 @@ sub Load  {
 
 =head2 LoadAction HASH
 
-  Takes a hash consisting of TicketObj and TransactionObj.  Loads an RT::Action:: module.
+Takes a hash consisting of TicketObj and TransactionObj.  Loads an RT::Action:: module.
 
 =cut
 
 sub LoadAction  {
     my $self = shift;
-    my %args = ( TransactionObj => undef,
-		 TicketObj => undef,
-		 @_ );
+    my %args = (
+        TransactionObj => undef,
+        TicketObj => undef,
+        ScripObj => undef,
+        @_
+    );
 
     $self->{_TicketObj} = $args{TicketObj};
-    
-    #TODO: Put this in an eval  
+
     $self->ExecModule =~ /^(\w+)$/;
     my $module = $1;
     my $type = "RT::Action::". $module;
- 
+
     eval "require $type" || die "Require of $type failed.\n$@\n";
-    
-    $self->{'Action'}  = $type->new ( Argument => $self->Argument,
-                                      CurrentUser => $self->CurrentUser,
-                                      ScripActionObj => $self, 
-                                      ScripObj => $args{'ScripObj'},
-                                      TemplateObj => $self->TemplateObj,
-                                      TicketObj => $args{'TicketObj'},
-                                      TransactionObj => $args{'TransactionObj'},
-				    );
+
+    return $self->{'Action'} = $type->new(
+        Argument => $self->Argument,
+        CurrentUser => $self->CurrentUser,
+        ScripActionObj => $self,
+        ScripObj => $args{'ScripObj'},
+        TemplateObj => $self->TemplateObj,
+        TicketObj => $args{'TicketObj'},
+        TransactionObj => $args{'TransactionObj'},
+    );
 }
 
 
 =head2 TemplateObj
 
 Return this action's template object
-
-TODO: Why are we not using the Scrip's template object?
-
 
 =cut
 
@@ -217,27 +206,20 @@ sub TemplateObj {
     return ( $self->{'TemplateObj'} );
 }
 
-# The following methods call the action object
-
-
 sub Prepare  {
     my $self = shift;
     $self->{_Message_ID} = 0;
-    return ($self->Action->Prepare());
-  
+    return $self->Action->Prepare();
 }
 
 sub Commit  {
     my $self = shift;
-    return($self->Action->Commit());
-    
-    
+    return $self->Action->Commit();
 }
 
 sub Describe  {
     my $self = shift;
-    return ($self->Action->Describe());
-    
+    return $self->Action->Describe();
 }
 
 =head2 Action
@@ -248,7 +230,7 @@ Return the actual RT::Action object for this scrip.
 
 sub Action {
     my $self = shift;
-    return ($self->{'Action'});
+    return $self->{'Action'};
 }
 
 sub DESTROY {
@@ -258,15 +240,6 @@ sub DESTROY {
     $self->{'TemplateObj'} = undef;
 }
 
-=head2 TODO
-
-Between this, RT::Scrip and RT::Action::*, we need to be able to get rid of a 
-class. This just reeks of too much complexity -- jesse
-
-=cut
-
-
-
 
 =head2 id
 
@@ -274,25 +247,16 @@ Returns the current value of id.
 (In the database, id is stored as int(11).)
 
 
-=cut
-
-
 =head2 Name
 
 Returns the current value of Name.
 (In the database, Name is stored as varchar(200).)
 
-
-
 =head2 SetName VALUE
-
 
 Set Name to VALUE.
 Returns (1, 'Status message') on success and (0, 'Error Message') on failure.
 (In the database, Name will be stored as a varchar(200).)
-
-
-=cut
 
 
 =head2 Description
@@ -300,17 +264,11 @@ Returns (1, 'Status message') on success and (0, 'Error Message') on failure.
 Returns the current value of Description.
 (In the database, Description is stored as varchar(255).)
 
-
-
 =head2 SetDescription VALUE
-
 
 Set Description to VALUE.
 Returns (1, 'Status message') on success and (0, 'Error Message') on failure.
 (In the database, Description will be stored as a varchar(255).)
-
-
-=cut
 
 
 =head2 ExecModule
@@ -318,17 +276,11 @@ Returns (1, 'Status message') on success and (0, 'Error Message') on failure.
 Returns the current value of ExecModule.
 (In the database, ExecModule is stored as varchar(60).)
 
-
-
 =head2 SetExecModule VALUE
-
 
 Set ExecModule to VALUE.
 Returns (1, 'Status message') on success and (0, 'Error Message') on failure.
 (In the database, ExecModule will be stored as a varchar(60).)
-
-
-=cut
 
 
 =head2 Argument
@@ -336,17 +288,11 @@ Returns (1, 'Status message') on success and (0, 'Error Message') on failure.
 Returns the current value of Argument.
 (In the database, Argument is stored as varbinary(255).)
 
-
-
 =head2 SetArgument VALUE
-
 
 Set Argument to VALUE.
 Returns (1, 'Status message') on success and (0, 'Error Message') on failure.
 (In the database, Argument will be stored as a varbinary(255).)
-
-
-=cut
 
 
 =head2 Creator
@@ -354,59 +300,45 @@ Returns (1, 'Status message') on success and (0, 'Error Message') on failure.
 Returns the current value of Creator.
 (In the database, Creator is stored as int(11).)
 
-
-=cut
-
-
 =head2 Created
 
 Returns the current value of Created.
 (In the database, Created is stored as datetime.)
-
-
-=cut
-
 
 =head2 LastUpdatedBy
 
 Returns the current value of LastUpdatedBy.
 (In the database, LastUpdatedBy is stored as int(11).)
 
-
-=cut
-
-
 =head2 LastUpdated
 
 Returns the current value of LastUpdated.
 (In the database, LastUpdated is stored as datetime.)
 
-
 =cut
-
 
 
 sub _CoreAccessible {
     {
 
         id =>
-		{read => 1, sql_type => 4, length => 11,  is_blob => 0,  is_numeric => 1,  type => 'int(11)', default => ''},
+                {read => 1, sql_type => 4, length => 11,  is_blob => 0,  is_numeric => 1,  type => 'int(11)', default => ''},
         Name =>
-		{read => 1, write => 1, sql_type => 12, length => 200,  is_blob => 0,  is_numeric => 0,  type => 'varchar(200)', default => ''},
+                {read => 1, write => 1, sql_type => 12, length => 200,  is_blob => 0,  is_numeric => 0,  type => 'varchar(200)', default => ''},
         Description =>
-		{read => 1, write => 1, sql_type => 12, length => 255,  is_blob => 0,  is_numeric => 0,  type => 'varchar(255)', default => ''},
+                {read => 1, write => 1, sql_type => 12, length => 255,  is_blob => 0,  is_numeric => 0,  type => 'varchar(255)', default => ''},
         ExecModule =>
-		{read => 1, write => 1, sql_type => 12, length => 60,  is_blob => 0,  is_numeric => 0,  type => 'varchar(60)', default => ''},
+                {read => 1, write => 1, sql_type => 12, length => 60,  is_blob => 0,  is_numeric => 0,  type => 'varchar(60)', default => ''},
         Argument =>
-		{read => 1, write => 1, sql_type => 12, length => 255,  is_blob => 0,  is_numeric => 0,  type => 'varbinary(255)', default => ''},
+                {read => 1, write => 1, sql_type => 12, length => 255,  is_blob => 0,  is_numeric => 0,  type => 'varbinary(255)', default => ''},
         Creator =>
-		{read => 1, auto => 1, sql_type => 4, length => 11,  is_blob => 0,  is_numeric => 1,  type => 'int(11)', default => '0'},
+                {read => 1, auto => 1, sql_type => 4, length => 11,  is_blob => 0,  is_numeric => 1,  type => 'int(11)', default => '0'},
         Created =>
-		{read => 1, auto => 1, sql_type => 11, length => 0,  is_blob => 0,  is_numeric => 0,  type => 'datetime', default => ''},
+                {read => 1, auto => 1, sql_type => 11, length => 0,  is_blob => 0,  is_numeric => 0,  type => 'datetime', default => ''},
         LastUpdatedBy =>
-		{read => 1, auto => 1, sql_type => 4, length => 11,  is_blob => 0,  is_numeric => 1,  type => 'int(11)', default => '0'},
+                {read => 1, auto => 1, sql_type => 4, length => 11,  is_blob => 0,  is_numeric => 1,  type => 'int(11)', default => '0'},
         LastUpdated =>
-		{read => 1, auto => 1, sql_type => 11, length => 0,  is_blob => 0,  is_numeric => 0,  type => 'datetime', default => ''},
+                {read => 1, auto => 1, sql_type => 11, length => 0,  is_blob => 0,  is_numeric => 0,  type => 'datetime', default => ''},
 
  }
 };
