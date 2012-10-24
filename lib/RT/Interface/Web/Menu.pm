bestpractical/rt

commit 9b8d1425f8f9905e2a2cacf7e544e6fbb0efa74d

Squash multiple consecutive slashes into single slashes in menu paths
The previous regex targeted a single failure case where the path is /
and the parent's path is / and then we tacked on a trailing /.

A more common case is the parent's path already containing a trailing
slash, leading to a double slash which wasn't canonicalized away.

lib/RT/Interface/Web/Menu.pm
@@ -153,7 +153,7 @@ sub path {
         $self->{path} = shift;
         $self->{path} = URI->new_abs($self->{path}, $self->parent->path . "/")->as_string
             if defined $self->{path} and $self->parent and $self->parent->path;
-        $self->{path} =~ s!///!/! if $self->{path};
+        $self->{path} =~ s!/+!/!g if $self->{path};
     }
     return $self->{path};
 }

commit 74816edd89c3c79404c0d3f09be65fb39470ae3b

Canonicalize menu paths in a less haphazard way
Instead of generating double or triple slashes and then removing them
with a regex, simply avoid generating the extra slashes in the first
place by skipping the append if the parent path already has a trailing
slash.

Switch to always making an absolute (although often schemeless) URI
object, even if there's no parent or parent path.  If no parent path
exists, simply use "/".  This ensures the path correctly concatenates
with WebPath.

Resolves [rt3 #21304] in which overzealous slash smashing in 9b8d142
broke menu paths with schemes after 4.0.7.

lib/RT/Interface/Web/Menu.pm
@@ -150,10 +150,12 @@ treated as relative to it's parent's path, and made absolute.
 sub path {
     my $self = shift;
     if (@_) {
-        $self->{path} = shift;
-        $self->{path} = URI->new_abs($self->{path}, $self->parent->path . "/")->as_string
-            if defined $self->{path} and $self->parent and $self->parent->path;
-        $self->{path} =~ s!/+!/!g if $self->{path};
+        if (defined($self->{path} = shift)) {
+            my $base  = ($self->parent and $self->parent->path) ? $self->parent->path : "";
+               $base .= "/" unless $base =~ m{/$};
+            my $uri = URI->new_abs($self->{path}, $base);
+            $self->{path} = $uri->as_string;
+        }
     }
     return $self->{path};
 }