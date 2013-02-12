bestpractical/rt

commit 6a9d0bff0f6aaafc222e324ed2cc50d57a4e4ea0

Split OCFV normalization into a separate function for general use
Ideally this would live in RT::CustomField, you'd call it on an object,
and life would be simpler.  Due to file uploads, however, the
normalization still has a foot planted in the web interface.

lib/RT/Interface/Web.pm
@@ -2545,6 +2545,9 @@ sub ProcessObjectCustomFieldUpdates {
                 }
                 push @results,
                     _ProcessObjectCustomFieldUpdates(
+                    # XXX FIXME: Prefix is not quite right, as $id almost
+                    # certainly started as blank for new objects and is now 0.
+                    # Only Image/Binary CFs on new objects should be affected.
                     Prefix      => "Object-$class-$id-CustomField-$cf-",
                     Object      => $Object,
                     CustomField => $CustomFieldObj,
@@ -2607,22 +2610,14 @@ sub _ProcessObjectCustomFieldUpdates {
             $args{'ARGS'}->{'Values'} = undef;
         }
 
-        my @values = ();
-        if ( ref $args{'ARGS'}->{$arg} eq 'ARRAY' ) {
-            @values = @{ $args{'ARGS'}->{$arg} };
-        } elsif ( $cf_type =~ /text/i ) {    # Both Text and Wikitext
-            @values = ( $args{'ARGS'}->{$arg} );
-        } else {
-            @values = split /\r*\n/, $args{'ARGS'}->{$arg}
-                if defined $args{'ARGS'}->{$arg};
-        }
-        @values = grep length, map {
-            s/\r+\n/\n/g;
-            s/^\s+//;
-            s/\s+$//;
-            $_;
-            }
-            grep defined, @values;
+        my @values = _NormalizeObjectCustomFieldValue(
+            CustomField => $cf,
+            Param       => $args{'Prefix'} . $arg,
+            Value       => $args{'ARGS'}->{$arg}
+        );
+
+        # "Empty" values still don't mean anything for Image and Binary fields
+        next if $cf_type =~ /^(?:Image|Binary)$/ and not @values;
 
         if ( $arg eq 'AddValue' || $arg eq 'Value' ) {
             foreach my $value (@values) {
@@ -2633,8 +2628,7 @@ sub _ProcessObjectCustomFieldUpdates {
                 push( @results, $msg );
             }
         } elsif ( $arg eq 'Upload' ) {
-            my $value_hash = _UploadedFile( $args{'Prefix'} . $arg ) or next;
-            my ( $val, $msg ) = $args{'Object'}->AddCustomFieldValue( %$value_hash, Field => $cf, );
+            my ( $val, $msg ) = $args{'Object'}->AddCustomFieldValue( %{$values[0]}, Field => $cf, );
             push( @results, $msg );
         } elsif ( $arg eq 'DeleteValues' ) {
             foreach my $value (@values) {
@@ -2719,6 +2713,33 @@ sub _ProcessObjectCustomFieldUpdates {
     return @results;
 }
 
+sub _NormalizeObjectCustomFieldValue {
+    my %args    = (@_);
+    my $cf_type = $args{CustomField}->Type;
+    my @values  = ();
+
+    if ( ref $args{'Value'} eq 'ARRAY' ) {
+        @values = @{ $args{'Value'} };
+    } elsif ( $cf_type =~ /text/i ) {    # Both Text and Wikitext
+        @values = ( $args{'Value'} );
+    } else {
+        @values = split /\r*\n/, $args{'Value'}
+            if defined $args{'Value'};
+    }
+    @values = grep length, map {
+        s/\r+\n/\n/g;
+        s/^\s+//;
+        s/\s+$//;
+        $_;
+        }
+        grep defined, @values;
+
+    if ($cf_type eq 'Image' or $cf_type eq 'Binary') {
+        @values = _UploadedFile( $args{'Param'} ) || ();
+    }
+
+    return @values;
+}
 
 =head2 ProcessTicketWatchers ( TicketObj => $Ticket, ARGSRef => \%ARGS );
 

commit b8dcceec67c985169601c986d59200d5bc782406

Failing tests showing that removing values for an image CF is broken
Broken by 6a9d0bf.

t/web/cf_image.t
@@ -1,0 +1,61 @@
+use strict;
+use warnings;
+
+use RT::Test tests => 'no_declare';
+
+my (undef, $m) = RT::Test->started_ok;
+$m->login;
+$m->follow_link( id => 'tools-config-custom-fields-create' );
+$m->submit_form_ok({
+    form_name => "ModifyCustomField",
+    fields    => {
+        Name          => 'Images',
+        TypeComposite => 'Image-1',
+        LookupType    => 'RT::Queue-RT::Ticket',
+    },
+});
+$m->content_contains("Object created");
+my $cfid = $m->form_name('ModifyCustomField')->value('id');
+ok $cfid, "Created CF correctly";
+
+$m->follow_link_ok( {id => "page-applies-to"} );
+$m->form_with_fields( "AddCustomField-1" );
+$m->tick( "AddCustomField-1", 0 );
+$m->click_ok( "UpdateObjs" );
+$m->content_contains("Object created");
+
+
+$m->submit_form_ok({
+    form_name => "CreateTicketInQueue",
+    fields    => { Queue => 'General' },
+});
+$m->content_contains("Upload one image");
+$m->submit_form_ok({
+    form_name => "TicketCreate",
+    fields    => {
+        Subject => 'Test ticket',
+        Content => 'test',
+    },
+});
+$m->content_like( qr/Ticket \d+ created/,
+                  "a ticket is created succesfully" );
+
+$m->follow_link_ok( {id => "page-basics"} );
+$m->content_contains("Upload one image");
+$m->submit_form_ok({
+    form_name => "TicketModify",
+    fields    => {
+        "Object-RT::Ticket-1-CustomField-1-Upload" =>
+            RT::Test::get_relocatable_file('bpslogo.png', '..', 'data'),
+    },
+});
+$m->content_contains("bpslogo.png added");
+$m->content_contains("/Download/CustomFieldValue/1/bpslogo.png");
+
+$m->form_name("TicketModify");
+$m->tick("Object-RT::Ticket-1-CustomField-1-DeleteValueIds", 1);
+$m->click_ok("SubmitTicket");
+$m->content_lacks("/Download/CustomFieldValue/1/bpslogo.png");
+
+undef $m;
+done_testing;