8447e8b59192ceedf65d401a394d5d9bfe4d3039
sunnavy <sunnavy@bestpractical.com>
2013-06-18 23:23:09 +0800

    compare ref type first to make sure $entry_ref is not weirdly overloaded
    one snippet:
    
        use Math::BigInt;
        no strict 'refs';
        # $round_mode = bless( do{\(my $o = 'even')}, 'Math::BigInt' );
        my $round_mode = *{'Math::BigInt::round_mode'}{SCALAR};
        print 'ok' if $round_mode;
    
    because Math::BigInt overloads "bool" operation, the "if $round_mode" dies
    with message "Not a HASH reference".
    
    we do similar GLOB thing in RT::Config::__GetNameByRef, but before perl 5.18,
    we can always find the right Name before checking "Math::Big::Int::round_mode"
    thanks to the consistent hash's keys() behavior.
    
    hash's keys() behavior has been changed since perl 5.18:
    
       Hash randomization
        The seed used by Perl's hash function is now random. This means that the
        order which keys/values will be returned from functions like "keys()",
        "values()", and "each()" will differ from run to run.
    
    i.e. we might reach "Math::Big::Int::round_mode" in RT::Config::__GetNameByRef
    since perl 5.18, so we need to make sure it won't die, that's the reason we do
    the extra check before the "bool" operation("next unless $entry_ref;")
    
    Math::BigInt is require by GnuPG::Interface.

--- lib/RT/Config.pm
+++ lib/RT/Config.pm
@@ -1220,6 +1220,7 @@ sub SetFromConfig {
             $ref_type = 'SCALAR' if $ref_type eq 'REF';
 
             my $entry_ref = *{$entry}{ $ref_type };
+            next if ref $entry_ref && ref $entry_ref ne ref $ref;
             next unless $entry_ref;
 
             # if references are equal then we've found
