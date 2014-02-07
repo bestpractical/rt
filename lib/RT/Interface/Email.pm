7f449ab079772a20377e3682f95a3755ca3f6998
Alex Vandiver <alexmv@bestpractical.com>
2014-02-07 01:11:15 -0500

    Return values of ApplyBeforeDecode plugins' GetCurrentUser are irrelevant
    Remove the values passed to return() in ApplyBeforeDecode plugins'
    GetCurrentUser, which are irrelevant.  This now makes clear that errors
    decrypting or verifying do _not_ actually reject the message -- instead,
    they merely skip the decryption and verification steps.

--- lib/RT/Interface/Email.pm
+++ lib/RT/Interface/Email.pm
@@ -185,7 +185,7 @@ sub Gateway {
             no strict 'refs';
             *{ $class . "::GetCurrentUser" }{CODE};
         };
-        my ($status, $msg) = $Code->(
+        $Code->(
             Message       => $Message,
             RawMessageRef => \$args{'message'},
             Queue         => $SystemQueueObj,
