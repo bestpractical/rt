8ae942ffef8812dec4ee448eec3eb2e4d8bb480b
Alex Vandiver <alexmv@bestpractical.com>
2014-02-12 02:00:43 -0500

    Move NewSubject to where it is used

--- lib/RT/Interface/Email.pm
+++ lib/RT/Interface/Email.pm
@@ -200,10 +200,6 @@ sub Gateway {
 
     $args{'ticket'} ||= ExtractTicketId( $Message );
 
-    # ExtractTicketId may have been overridden, and edited the Subject
-    my $NewSubject = Encode::decode( "UTF-8", $Message->head->get('Subject') );
-    chomp $NewSubject;
-
     my $SystemTicket = RT::Ticket->new( RT->SystemUser );
     $SystemTicket->Load( $args{'ticket'} ) if ( $args{'ticket'} ) ;
     my $Right;
@@ -276,6 +272,10 @@ sub Gateway {
                 QueueObj    => $SystemQueueObj
             );
         }
+
+        # ExtractTicketId may have been overridden, and edited the Subject
+        my $NewSubject = Encode::decode( "UTF-8", $head->get('Subject') );
+        chomp $NewSubject;
 
         my ( $id, $Transaction, $ErrStr ) = $Ticket->Create(
             Queue     => $SystemQueueObj->Id,
