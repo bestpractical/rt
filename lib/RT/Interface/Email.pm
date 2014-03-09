2f24bb090933b084e635f01be480608ea6108a06
Alex Vandiver <alexmv@bestpractical.com>
2014-03-09 12:12:23 -0400

    Plugins may alter @actions; ensure action is valid prior to calling

--- lib/RT/Interface/Email.pm
+++ lib/RT/Interface/Email.pm
@@ -367,7 +367,11 @@ sub HandleAction {
         @_
     );
 
-    my ($code) = Plugins(Method => "Handle" . ucfirst(delete $args{Action}));
+    my $action = delete $args{Action};
+    my ($code) = Plugins(Method => "Handle" . ucfirst($action));
+    TMPFAIL( "Invalid 'action' parameter $action for queue ".$args{Queue}->Name )
+        unless $code;
+
     $code->(%args);
 }
 
