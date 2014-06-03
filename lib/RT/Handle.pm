7eefc4a3a8577a2ed5724e489ab104404a7d5cef
Alex Vandiver <alexmv@bestpractical.com>
2014-06-03 16:43:07 -0400

    Partially revert 487df4d92f0dcc6fd197e60b99719fcbe979cbcc
    Switching directly from Queue to ObjectId is not correct, as it fails to
    provide the ObjectType/LookupType hint.

--- lib/RT/Handle.pm
+++ lib/RT/Handle.pm
@@ -1058,7 +1058,7 @@ sub InsertData {
                 $object = RT::CustomField->new( RT->SystemUser );
                 my @columns = ( Name => $item->{'CF'} );
                 push @columns, LookupType => $item->{'LookupType'} if $item->{'LookupType'};
-                push @columns, ObjectId => $item->{'Queue'} if $item->{'Queue'} and not ref $item->{'Queue'};
+                push @columns, Queue => $item->{'Queue'} if $item->{'Queue'} and not ref $item->{'Queue'};
                 my ($ok, $msg) = $object->LoadByName( @columns );
                 unless ( $ok ) {
                     RT->Logger->error("Unable to load CF ".$item->{CF}.": $msg");
