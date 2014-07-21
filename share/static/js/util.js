6beec0e48798847738904d8de270c1d9818d8010
Wallace Reis <wreis@bestpractical.com>
2014-07-21 10:01:11 -0300

    Make 'select all' check/uncheck bi-directional
    If I uncheck "All $thing", and then I re-check all of the items by hand,
    then "All $thing" pops back to being checked as soon as I re-checked
    the last item.

--- share/static/js/util.js
+++ share/static/js/util.js
@@ -80,14 +80,18 @@ function set_rollup_state(e,e2,state) {
 
 /* other utils */
 
+function getClosestInputElements(input) {
+    // Find inputs within the current form or collection list, whichever is closest.
+    var container = jQuery(input).closest("form, table.collection-as-table").get(0);
+    return container.getElementsByTagName('input');
+}
+
 function setCheckbox(input, name, val) {
     if (val == null) val = input.checked;
     var allfield = jQuery('input[name=' + input.name + ']');
     allfield.prop('checked', val);
 
-    // Find inputs within the current form or collection list, whichever is closest.
-    var container = jQuery(input).closest("form, table.collection-as-table").get(0);
-    var myfield   = container.getElementsByTagName('input');
+    var myfield = getClosestInputElements(input);
     for ( var i = 0; i < myfield.length; i++ ) {
         if ( myfield[i].type != 'checkbox' ) continue;
         if ( name ) {
@@ -101,6 +105,37 @@ function setCheckbox(input, name, val) {
         }
 
         myfield[i].checked = val;
+    }
+}
+
+function checkboxSetEvent(input, name, allname) {
+    var myfield = getClosestInputElements(input);
+    var checked_count = 0;
+    var field_count = 0;
+    for ( var i = 0; i < myfield.length; i++ ) {
+        if ( myfield[i].type != 'checkbox' ) continue;
+        if ( name ) {
+            if ( name instanceof RegExp ) {
+                if ( ! myfield[i].name.match( name ) ) continue;
+            }
+            else {
+                if ( myfield[i].name != name ) continue;
+            }
+
+        }
+
+        field_count++;
+        if ( myfield[i].checked ) {
+            checked_count++;
+        }
+    }
+
+    var allfield = jQuery('input[name=' + allname + ']');
+    if (field_count == checked_count) {
+        allfield.prop('checked', true);
+    }
+    else {
+        allfield.prop('checked', false);
     }
 }
 
