4f49c8d04cf57d55c5a2594d3ba0c9985ff9d446
Wallace Reis <wreis@bestpractical.com>
2014-08-08 16:33:02 -0300

    Merge checkbox click event handlers
    Merge "setCheckbox" and "checkboxSetEvent" handlers into single one
    function which sets up everything bi-directionally (on both the "All"
    checkbox, as well as the individual ones).

--- share/static/js/util.js
+++ share/static/js/util.js
@@ -88,55 +88,51 @@ function getClosestInputElements(input) {
 
 function setCheckbox(input, name, val) {
     if (val == null) val = input.checked;
-    var allfield = jQuery('input[name=' + input.name + ']');
-    allfield.prop('checked', val);
 
-    var myfield = getClosestInputElements(input);
-    for ( var i = 0; i < myfield.length; i++ ) {
-        if ( myfield[i].type != 'checkbox' ) continue;
-        if ( name ) {
-            if ( name instanceof RegExp ) {
-                if ( ! myfield[i].name.match( name ) ) continue;
-            }
-            else {
-                if ( myfield[i].name != name ) continue;
-            }
-
-        }
-
-        myfield[i].checked = val;
-    }
-}
-
-function checkboxSetEvent(input) {
-    var name = input.name;
-    var myfield = getClosestInputElements(input);
-    var checked_count = 0;
-    var field_count = 0;
-    for ( var i = 0; i < myfield.length; i++ ) {
-        if ( myfield[i].type != 'checkbox' ) continue;
-        if ( name ) {
-            if ( name instanceof RegExp ) {
-                if ( ! myfield[i].name.match( name ) ) continue;
-            }
-            else {
-                if ( myfield[i].name != name ) continue;
-            }
-
-        }
-
-        field_count++;
-        if ( myfield[i].checked ) {
-            checked_count++;
-        }
-    }
-
-    var allfield = jQuery('input[name=' + name + 'All' + ']');
-    if (field_count == checked_count) {
-        allfield.prop('checked', true);
+    var is_set_event = false;
+    if ( !name ) {
+        name = input.name;
+        is_set_event = true;
     }
     else {
-        allfield.prop('checked', false);
+        var allfield = jQuery('input[name=' + input.name + ']');
+        allfield.prop('checked', val);
+    }
+
+    var checked_count = 0;
+    var field_count = 0;
+    var myfield = getClosestInputElements(input);
+    for ( var i = 0; i < myfield.length; i++ ) {
+        if ( myfield[i].type != 'checkbox' ) continue;
+        if ( name ) {
+            if ( name instanceof RegExp ) {
+                if ( ! myfield[i].name.match( name ) ) continue;
+            }
+            else {
+                if ( myfield[i].name != name ) continue;
+            }
+
+        }
+
+        if ( is_set_event ) {
+            field_count++;
+            if ( myfield[i].checked ) {
+                checked_count++;
+            }
+        }
+        else {
+            myfield[i].checked = val;
+        }
+    }
+
+    if ( is_set_event ) {
+        var allfield = jQuery('input[name=' + name + 'All' + ']');
+        if (field_count == checked_count) {
+            allfield.prop('checked', true);
+        }
+        else {
+            allfield.prop('checked', false);
+        }
     }
 }
 
