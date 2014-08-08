0afbecaa9759e20d7bbfcfebb3c4e1ac4ab72c25
Alex Vandiver <alexmv@bestpractical.com>
2014-08-08 14:05:39 -0400

    Add RT::Util::assert_bytes checks to _EncodeLOB and _DecodeLOB
    929b4231 specifically documented _EncodeLOB and _DecodeLOB to take
    bytes; enforce that by checking the arguments they are passed using
    RT::Util::assert_bytes.

--- lib/RT/Record.pm
+++ lib/RT/Record.pm
@@ -754,6 +754,8 @@ sub _EncodeLOB {
 
     my $ContentEncoding = 'none';
 
+    RT::Util::assert_bytes( $Body );
+
     #get the max attachment length from RT
     my $MaxSize = RT->Config->Get('MaxAttachmentSize');
 
@@ -800,16 +802,12 @@ sub _EncodeLOB {
 
     # if we need to mimencode the attachment
     if ( $ContentEncoding eq 'base64' ) {
-
         # base64 encode the attachment
-        Encode::_utf8_off($Body);
         $Body = MIME::Base64::encode_base64($Body);
 
     } elsif ($ContentEncoding eq 'quoted-printable') {
-        Encode::_utf8_off($Body);
         $Body = MIME::QuotedPrint::encode($Body);
     }
-
 
     return ($ContentEncoding, $Body, $MIMEType, $Filename );
 }
@@ -847,6 +845,8 @@ sub _DecodeLOB {
     my $ContentEncoding = shift || 'none';
     my $Content         = shift;
 
+    RT::Util::assert_bytes( $Content );
+
     if ( $ContentEncoding eq 'base64' ) {
         $Content = MIME::Base64::decode_base64($Content);
     }
@@ -863,7 +863,7 @@ sub _DecodeLOB {
         my $charset = RT::I18N::_FindOrGuessCharset($entity);
         $charset = 'utf-8' if not $charset or not Encode::find_encoding($charset);
 
-        $Content = Encode::decode($charset,$Content,Encode::FB_PERLQQ) unless Encode::is_utf8($Content);
+        $Content = Encode::decode($charset,$Content,Encode::FB_PERLQQ);
     }
     return ($Content);
 }
