162e8bc75c4f0f0171d458d9fc5388f3eaf71b98
Kevin Falcone <falcone@bestpractical.com>
2014-10-01 11:47:51 -0400

    Avoid invoking the shell when both signing and encrypting a message
    When a request came in to sign *and* encrypt, we joined the openssl
    invocation to sign together with the openssl invocation to encrypt using
    a pipe.  This caused run3 to invoke the shell (because of shell
    metacharacters).  This leaves RT vulnerable to CVE-2014-7169,
    CVE-2014-7186, CVE-2014-7187, CVE-2014-6277, CVE-2014-6271 collectively
    known as "Shellshock."  Because it requires both signing and encryption,
    this vulnerability is only exploitable with an unpatched bash, and by
    privileged users. CVE-2014-7227 has been assigned for this
    vulnerability.
    
    Switch to using the list form of run3 which avoids any use of the shell.
    
    To emulate the pipe, use a loop which takes the STDOUT in $out from the
    signing and hands it back to the encryption run as $buf.  The final
    output will then be put back into $buf to be returned upstream and
    mailed out.

--- lib/RT/Crypt/SMIME.pm
+++ lib/RT/Crypt/SMIME.pm
@@ -327,7 +327,7 @@ sub _SignEncrypt {
 
     my $opts = RT->Config->Get('SMIME');
 
-    my @command;
+    my @commands;
     if ( $args{'Sign'} ) {
         my $file = $self->CheckKeyring( Key => $args{'Signer'} );
         unless ($file) {
@@ -343,14 +343,14 @@ sub _SignEncrypt {
         $args{'Passphrase'} = $self->GetPassphrase( Address => $args{'Signer'} )
             unless defined $args{'Passphrase'};
 
-        push @command, join ' ', shell_quote(
+        push @commands, [
             $self->OpenSSLPath, qw(smime -sign),
             -signer => $file,
             -inkey  => $file,
             (defined $args{'Passphrase'} && length $args{'Passphrase'})
                 ? (qw(-passin env:SMIME_PASS))
                 : (),
-        );
+        ];
     }
     if ( $args{'Encrypt'} ) {
         foreach my $key ( @keys ) {
@@ -359,23 +359,32 @@ sub _SignEncrypt {
             close $key_file;
             $key = $key_file;
         }
-        push @command, join ' ', shell_quote(
+        push @commands, [
             $self->OpenSSLPath, qw(smime -encrypt -des3),
             map { $_->filename } @keys
-        );
+        ];
     }
 
-    my ($buf, $err) = ('', '');
-    {
-        local $ENV{'SMIME_PASS'} = $args{'Passphrase'};
-        local $SIG{'CHLD'} = 'DEFAULT';
-        safe_run_child { run3(
-            join( ' | ', @command ),
-            $args{'Content'},
-            \$buf, \$err
-        ) };
+    my $buf = ${ $args{'Content'} };
+    for my $command (@commands) {
+        my ($out, $err) = ('', '');
+        {
+            local $ENV{'SMIME_PASS'} = $args{'Passphrase'};
+            local $SIG{'CHLD'} = 'DEFAULT';
+            safe_run_child { run3(
+                $command,
+                \$buf,
+                \$out, \$err
+            ) };
+        }
+
+        $RT::Logger->debug( "openssl stderr: " . $err ) if length $err;
+
+        # copy output from the first command to the second command
+        # similar to the pipe we used to use to pipe signing -> encryption
+        # Using the pipe forced us to invoke the shell, this avoids any use of shell.
+        $buf = $out;
     }
-    $RT::Logger->debug( "openssl stderr: " . $err ) if length $err;
 
     if ($buf) {
         $res{'status'} .= $self->FormatStatus({
