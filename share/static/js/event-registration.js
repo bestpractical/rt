2355f0f0109943f721b21d10f7839e21807e64cc
Alex Vandiver <alexmv@bestpractical.com>
2014-10-28 13:07:09 -0400

    Apply red background to all UpdateType/MessageBox locations
    This adds the behavior to Jumbo and Bulk Update pages.

--- share/static/js/event-registration.js
+++ share/static/js/event-registration.js
@@ -100,3 +100,11 @@ jQuery( function() {
         }
     });
 });
+
+jQuery(function() {
+    jQuery("#UpdateType").change(function(ev) {
+        jQuery(".messagebox-container")
+            .removeClass("action-response action-private")
+            .addClass("action-"+ev.target.value);
+    });
+});
