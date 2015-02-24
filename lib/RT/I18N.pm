626097574ebdb5e18477e6c04a8b7859b1fe4555
Alex Vandiver <alexmv@bestpractical.com>
2015-02-24 15:08:51 -0500

    Remove vestiges from when RT tried to be taint-safe
    Notable because it restricts the paths that RT can be installed under --
    for instance, paths containing unusual symbols (?, or @ from AFS) fail
    the given regex.
    
    As the rest of RT is no longer taint-safe, remove this vestigal code.

--- lib/RT/I18N.pm
+++ lib/RT/I18N.pm
@@ -101,12 +101,6 @@ sub Init {
 
     # Load language-specific functions
     foreach my $file ( File::Glob::bsd_glob(substr(__FILE__, 0, -3) . "/*.pm") ) {
-        unless ( $file =~ /^([-\w\s\.\/\\~:]+)$/ ) {
-            warn("$file is tainted. not loading");
-            next;
-        }
-        $file = $1;
-
         my ($lang) = ($file =~ /([^\\\/]+?)\.pm$/);
         next unless grep $_ eq '*' || $_ eq $lang, @lang;
         require $file;
