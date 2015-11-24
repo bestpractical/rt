719354847438bcb8148e8461d4fa5138335feedf
Shawn M Moore <shawn@bestpractical.com>
2015-11-24 20:28:28 +0000

    Add an RT::Date->Strftime method
        Fixes: I#31435

--- lib/RT/Date.pm
+++ lib/RT/Date.pm
@@ -1179,8 +1179,25 @@ sub DateTimeObj {
         second     => $sec,
         nanosecond => 0,
     );
+}
 
-    return $dt;
+=head3 Strftime FORMAT, [Timezone => 'user']
+
+Stringify the RT::Date according to the specified format. See
+L<DateTime/strftime Patterns>.
+
+=cut
+
+sub Strftime {
+    my $self = shift;
+    my $format = shift;
+    my %args = (
+        Timezone => 'user',
+        @_,
+    );
+
+    my $dt = $self->DateTimeObj(%args);
+    return $dt->strftime($format);
 }
 
 RT::Base->_ImportOverlays();
