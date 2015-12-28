c9382810ebe631fb1f2f1c8d3cd710fd54ea903a
Shawn M Moore <shawn@bestpractical.com>
2015-12-28 18:18:02 +0000

    Add a way to deprecate specific callbacks

--- lib/RT/Interface/Web/Request.pm
+++ lib/RT/Interface/Web/Request.pm
@@ -55,6 +55,9 @@ use HTML::Mason::PSGIHandler;
 use base qw(HTML::Mason::Request::PSGI);
 use Params::Validate qw(:all);
 
+my %deprecated = (
+);
+
 sub new {
     my $class = shift;
     $class->valid_params( %{ $class->valid_params },cgi_request => { type => OBJECT, optional => 1 } );
@@ -131,6 +134,15 @@ sub callback {
         }
 
         $cache{ $CacheKey } = $callbacks unless RT->Config->Get('DevelMode');
+
+        if (@{ $callbacks } && $deprecated{$page}{$name}) {
+            RT->Deprecated(
+                Message => "The callback $name on page $page is deprecated",
+                Detail  => "Callback list:\n" . join("\n", @{ $callbacks }),
+                Stack   => 0,
+                %{ $deprecated{$page}{$name} },
+            );
+        }
     }
 
     my @rv;
