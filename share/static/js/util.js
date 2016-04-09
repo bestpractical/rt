cf8793ca768f5e43d9f75d93d6be5c327c3a5c21
Todd Wade <todd@bestpractical.com>
2016-04-09 03:19:51 -0400

    force .load to POST instead of GET
    AJAXifying the preview scrips with .load() via .serialize() does a GET
    request with the Display.html form, and the query string can end up too
    long for the server to accept it.
    
    This commit adds a helper to serialize the form as an object instead of
    query string, which tells .load to POST instead of GET. It looks like
    theres no built in jQuery interface to serialize a form as an object so
    the helper is needed.
    
    Fixes issues#31874

--- share/static/js/util.js
+++ share/static/js/util.js
@@ -565,3 +565,19 @@ function scrollToJQueryObject(obj) {
     }
 }
 
+// https://css-tricks.com/snippets/jquery/serialize-form-to-json/
+jQuery.fn.serializeObject = function() {
+   var o = {};
+   var a = this.serializeArray();
+   jQuery.each(a, function() {
+       if (o[this.name]) {
+           if (!o[this.name].push) {
+               o[this.name] = [o[this.name]];
+           }
+           o[this.name].push(this.value || '');
+       } else {
+           o[this.name] = this.value || '';
+       }
+   });
+   return o;
+};
