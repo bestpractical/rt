5d851b77f275252578ce29c21a652c88a6663ab6
Dustin Graves <dustin@bestpractical.com>
2016-05-27 19:49:03 +0000

    add Assets to type of CustomField object that supports DefaultValues

--- lib/RT/CustomField.pm
+++ lib/RT/CustomField.pm
@@ -2056,7 +2056,7 @@ sub BasedOnObj {
 sub SupportDefaultValues {
     my $self = shift;
     return 0 unless $self->id;
-    return 0 unless $self->LookupType =~ /RT::(?:Ticket|Transaction)$/;
+    return 0 unless $self->LookupType =~ /RT::(?:Ticket|Transaction|Asset)$/;
     return $self->Type !~ /^(?:Image|Binary)$/;
 }
 
