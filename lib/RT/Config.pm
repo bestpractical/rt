8303ea9a1882d4bab32c9456c010e392c23f147b
Alex Vandiver <alex@chmrr.net>
2016-07-27 01:56:28 -0700

    Re-format RT/Config.pm so the `# loc` comment parses correctly
    The trailing `# loc` comment can only deal with a limited set of
    possible trailing characters; it fails to extract if followed by a
    comma and an extra expression.
    
    Make `./devel/tools/rt-message-catalog extract` happy by re-wrapping
    the expression.

--- lib/RT/Config.pm
+++ lib/RT/Config.pm
@@ -340,10 +340,16 @@ our %META;
 
                 for my $value (@values) {
                     if ($value % 60 == 0) {
-                        $labels{$value} = ['Refresh search results every [quant,_1,minute,minutes].', $value / 60]; # loc
+                        $labels{$value} = [
+                            'Refresh search results every [quant,_1,minute,minutes].', #loc
+                            $value / 60
+                        ];
                     }
                     else {
-                        $labels{$value} = ['Refresh search results every [quant,_1,second,seconds].', $value]; # loc
+                        $labels{$value} = [
+                            'Refresh search results every [quant,_1,second,seconds].', #loc
+                            $value
+                        ];
                     }
                 }
 
@@ -370,10 +376,16 @@ our %META;
 
                 for my $value (@values) {
                     if ($value % 60 == 0) {
-                        $labels{$value} = ['Refresh home page every [quant,_1,minute,minutes].', $value / 60]; # loc
+                        $labels{$value} = [
+                            'Refresh home page every [quant,_1,minute,minutes].', #loc
+                            $value / 60
+                        ];
                     }
                     else {
-                        $labels{$value} = ['Refresh home page every [quant,_1,second,seconds].', $value]; # loc
+                        $labels{$value} = [
+                            'Refresh home page every [quant,_1,second,seconds].', #loc
+                            $value
+                        ];
                     }
                 }
 
