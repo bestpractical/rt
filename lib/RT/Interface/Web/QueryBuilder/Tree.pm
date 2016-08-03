bestpractical/rt

commit b68c84f0c915e558cbd127f94ef7740c1ec3c359

Switch to parsing into a parse tree as an IR

lib/RT/Interface/Web/QueryBuilder/Tree.pm
@@ -113,7 +113,7 @@ sub GetReferencedQueues {
             return unless $clause->{Key} eq 'Queue';
             return unless $clause->{Op} eq '=';
 
-            $queues->{ $clause->{RawValue} } = 1;
+            $queues->{ $clause->{Value} } = 1;
         }
     );
 
@@ -207,10 +207,20 @@ sub __LinearizeTree {
         } else {
 
             my $clause = $node->getNodeValue;
-            $str .= $clause->{Key};
-            $str .= " ". $clause->{Op};
-            $str .= " ". $clause->{Value};
+            my $key = $clause->{Key};
+            $key .= "." . $clause->{Subkey} if defined $clause->{Subkey};
+            if ($key =~ s/(['\\])/\\$1/g or $key =~ /[^{}\w\.]/) {
+                $key = "'$key'";
+            }
+            my $value = $clause->{Value};
+            if ( $clause->{Op} =~ /^IS( NOT)?$/i ) {
+                $value = 'NULL';
+            } elsif ( $value !~ /^[+-]?[0-9]+$/ ) {
+                $value =~ s/(['\\])/\\$1/g;
+                $value = "'$value'";
+            }
 
+            $str .= $key ." ". $clause->{Op} . " " . $value;
         }
         $str =~ s/^\s+|\s+$//;
 
@@ -255,32 +265,20 @@ sub ParseSQL {
     $callback{'EntryAggregator'} = sub { $node->setNodeValue( $_[0] ) };
     $callback{'Condition'} = sub {
         my ($key, $op, $value) = @_;
-        my $rawvalue = $value;
 
-        my ($main_key) = split /[.]/, $key;
+        my ($main_key, $subkey) = split /[.]/, $key, 2;
 
-        my $class;
-        if ( exists $lcfield{ lc $main_key } ) {
-            $key =~ s/^[^.]+/ $lcfield{ lc $main_key } /e;
-            ($main_key) = split /[.]/, $key;  # make the case right
-            $class = $field{ $main_key }->[0];
-        }
-        unless( $class ) {
+        unless( $lcfield{ lc $main_key} ) {
             push @results, [ $args{'CurrentUser'}->loc("Unknown field: [_1]", $key), -1 ]
         }
+        $main_key = $lcfield{ lc $main_key };
 
-        if ( lc $op eq 'is' || lc $op eq 'is not' ) {
-            $value = 'NULL'; # just fix possible mistakes here
-        } elsif ( $value !~ /^[+-]?[0-9]+$/ ) {
-            $value =~ s/(['\\])/\\$1/g;
-            $value = "'$value'";
-        }
+        # Hardcode value for IS / IS NOT
+        $value = 'NULL' if $op =~ /^IS( NOT)?$/i;
 
-        if ($key =~ s/(['\\])/\\$1/g or $key =~ /[^{}\w\.]/) {
-            $key = "'$key'";
-        }
-
-        my $clause = { Key => $key, Op => $op, Value => $value, RawValue => $rawvalue };
+        my $clause = { Key => $main_key, Subkey => $subkey,
+                       Meta => $field{ $main_key },
+                       Op => $op, Value => $value };
         $node->addChild( __PACKAGE__->new( $clause ) );
     };
     $callback{'Error'} = sub { push @results, @_ };

lib/RT/Tickets.pm
@@ -2928,52 +2928,45 @@ failure.
 
 sub _parser {
     my ($self,$string) = @_;
+
+    require RT::Interface::Web::QueryBuilder::Tree;
+    my $tree = RT::Interface::Web::QueryBuilder::Tree->new;
+    $tree->ParseSQL(
+        Query => $string,
+        CurrentUser => $self->CurrentUser,
+    );
+
     my $ea = '';
+    $tree->traverse(
+        sub {
+            my $node = shift;
+            $ea = $node->getParent->getNodeValue if $node->getIndex > 0;
+            return $self->_OpenParen unless $node->isLeaf;
 
-    my %callback;
-    $callback{'OpenParen'} = sub {
-      $self->_OpenParen;
-    };
-    $callback{'CloseParen'} = sub {
-      $self->_CloseParen;
-    };
-    $callback{'EntryAggregator'} = sub {
-      $ea = $_[0] || '';
-    };
-    $callback{'Condition'} = sub {
-        my ($key, $op, $value) = @_;
+            my ($key, $subkey, $meta, $op, $value)
+                = @{$node->getNodeValue}{qw/Key Subkey Meta Op Value/};
 
-        # key has dot then it's compound variant and we have subkey
-        my $subkey = '';
-        ($key, $subkey) = ($1, $2) if $key =~ /^([^\.]+)\.(.+)$/;
+            # normalize key and get class (type)
+            my $class = $meta->[0];
 
-        # normalize key and get class (type)
-        my $class;
-        if (exists $LOWER_CASE_FIELDS{lc $key}) {
-            $key = $LOWER_CASE_FIELDS{lc $key};
-            $class = $FIELD_METADATA{$key}->[0];
+            # replace __CurrentUser__ with id
+            $value = $self->CurrentUser->id if $value eq '__CurrentUser__';
+
+            my $sub = $dispatch{ $class }
+                or die "No dispatch method for class '$class'";
+
+            # A reference to @res may be pushed onto $sub_tree{$key} from
+            # above, and we fill it here.
+            $sub->( $self, $key, $op, $value,
+                    ENTRYAGGREGATOR => $ea,
+                    SUBKEY          => $subkey,
+                  );
+        },
+        sub {
+            my $node = shift;
+            return $self->_CloseParen unless $node->isLeaf;
         }
-        die "Unknown field '$key' in '$string'" unless $class;
-
-        # replace __CurrentUser__ with id
-        $value = $self->CurrentUser->id if $value eq '__CurrentUser__';
-
-
-        unless( $dispatch{ $class } ) {
-            die "No dispatch method for class '$class'"
-        }
-        my $sub = $dispatch{ $class };
-
-        # A reference to @res may be pushed onto $sub_tree{$key} from
-        # above, and we fill it here.
-        $sub->( $self, $key, $op, $value,
-                SUBCLAUSE       => '',  # don't need anymore
-                ENTRYAGGREGATOR => $ea,
-                SUBKEY          => $subkey,
-              );
-        $ea = '';
-    };
-    RT::SQL::Parse($string, \%callback);
+    );
 }
 
 sub FromSQL {

share/html/Search/Build.html
@@ -230,30 +230,11 @@ foreach my $arg ( keys %ARGS ) {
     for ( my $i = 0; $i < @ops; $i++ ) {
         my ( $op, $value ) = ( $ops[$i], $values[$i] );
         next if !defined $value || $value eq '';
-        my $rawvalue = $value;
-
-        if ( $value =~ /^NULL$/i && $op =~ /=/ ) {
-            if ( $op eq '=' ) {
-                $op = "IS";
-            }
-            elsif ( $op eq '!=' ) {
-                $op = "IS NOT";
-            }
-        }
-        elsif ($value =~ /\D/) {
-            $value =~ s/(['\\])/\\$1/g;
-            $value = "'$value'";
-        }
-
-        if ($keyword =~ s/(['\\])/\\$1/g or $keyword =~ /[^{}\w\.]/) {
-            $keyword = "'$keyword'";
-        }
 
         my $clause = {
             Key   => $keyword,
             Op    => $op,
             Value => $value,
-            RawValue => $rawvalue,
         };
 
         push @new_values, RT::Interface::Web::QueryBuilder::Tree->new($clause);

commit 7d8d5f6838ff707d4bc8c8c787868718a75562ff

fix RT::Tickets->FromSQL query validation
commit b68c84f0 broke this feature by changing FromSQL to use the
RT::SQL::Parse call in RT::Interface::Web::QueryBuilder::Tree->ParseSQL
which defines an Error callback

this callback swallows the expected exceptions and causes FromSQL to
erroneously return 'Valid Query'

this commit adds a way to skip defining the error callback when calling
RT::Interface::Web::QueryBuilder::Tree->ParseSQL

Fixes: I#32231

lib/RT/Interface/Web/QueryBuilder/Tree.pm
@@ -246,6 +246,7 @@ sub ParseSQL {
     my %args = (
         Query => '',
         CurrentUser => '', #XXX: Hack
+        UseErrorCallback => 1,
         @_
     );
     my $string = $args{'Query'};
@@ -281,7 +282,7 @@ sub ParseSQL {
                        Op => $op, Value => $value };
         $node->addChild( __PACKAGE__->new( $clause ) );
     };
-    $callback{'Error'} = sub { push @results, @_ };
+    $callback{'Error'} = sub { push @results, @_ } if $args{UseErrorCallback};
 
     require RT::SQL;
     RT::SQL::Parse($string, \%callback);

lib/RT/Tickets.pm
@@ -2983,6 +2983,7 @@ sub _parser {
     $tree->ParseSQL(
         Query => $string,
         CurrentUser => $self->CurrentUser,
+        UseErrorCallback => 0,
     );
 
     state ( $active_status_node, $inactive_status_node );