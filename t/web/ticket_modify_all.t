bestpractical/rt

commit 1bb578722e7d0a7c57134db82b7ed801cf9d79e8

Support processing transaction cfs on ticket create and update
The change here is scoped only to the changes that include
TimeWorked. This was originally developed as part of
RT::Extension::TimeTracking and supports the transaction
custom field functionality provided there. Support for
additional transaction cf updates may require a more
comprehensive change.

lib/RT/Interface/Web.pm
@@ -2930,7 +2930,6 @@ sub ProcessTicketBasics {
         FinalPriority
         Priority
         TimeEstimated
-        TimeWorked
         TimeLeft
         Type
         Status
@@ -2959,6 +2958,12 @@ sub ProcessTicketBasics {
         Object        => $TicketObj,
         ARGSRef       => $ARGSRef,
     );
+
+    if ( $ARGSRef->{'TimeWorked'} ) {
+        my ( $val, $msg, $txn ) = $TicketObj->SetTimeWorked( $ARGSRef->{'TimeWorked'} );
+        push( @results, $msg );
+        $txn->UpdateCustomFields( %$ARGSRef) if $txn;
+    }
 
     # We special case owner changing, so we can use ForceOwnerChange
     if ( $ARGSRef->{'Owner'}
@@ -3360,7 +3365,14 @@ sub ProcessObjectCustomFieldUpdatesForCreate {
                     );
             }
 
-            $parsed{"CustomField-$cfid"} = \@values if @values;
+            if (@values) {
+                if ( $class eq 'RT::Transaction' ) {
+                    $parsed{"Object-RT::Transaction--CustomField-$cfid"} = \@values;
+                }
+                else {
+                    $parsed{"CustomField-$cfid"} = \@values if @values;
+                }
+            }
         }
     }
 

lib/RT/Ticket.pm
@@ -2650,7 +2650,7 @@ sub _Set {
     # just made the ticket unreadable to us
     $trans->{ _object_is_readable } = 1;
 
-    return ( $ret, scalar $trans->BriefDescription );
+    return ( $ret, scalar $trans->BriefDescription, $trans );
 }
 
 

commit be85a7aef6e55346e681964ac281776a347e6a35

Avoid spurious "That is already the current value" for time worked
The mere presence of the argument (which can happen when you submit
Modify.html after having logged time to a ticket) means RT
unconditionally updates the TimeWorked field, which means it
unconditionally adds "That is already the current value" to the results
array.

Instead, only update time worked if the value submitted is not the same
as the current value of TimeWorked on the ticket.

Special consideration is given to setting time worked to the value "0",
which should adjust the time worked on the ticket to 0 rather than being
silently ignored.

This regression was introduced in
1bb578722e7d0a7c57134db82b7ed801cf9d79e8; TimeWorked being an ordinary
field in @attribs means that RT::Record::Update would skip updating that
field because the existing value was the same as the submitted value.
But since TimeWorked no longer uses the Update codepath, there was no
provision to skip it when it's submitted unchanged.

lib/RT/Interface/Web.pm
@@ -2960,7 +2960,7 @@ sub ProcessTicketBasics {
         ARGSRef       => $ARGSRef,
     );
 
-    if ( $ARGSRef->{'TimeWorked'} ) {
+    if ( defined($ARGSRef->{'TimeWorked'}) && ($ARGSRef->{'TimeWorked'} || 0) != $TicketObj->TimeWorked ) {
         my ( $val, $msg, $txn ) = $TicketObj->SetTimeWorked( $ARGSRef->{'TimeWorked'} );
         push( @results, $msg );
         $txn->UpdateCustomFields( %$ARGSRef) if $txn;

t/web/ticket_modify_all.t
@@ -31,10 +31,15 @@ $m->get_ok($url . '/Ticket/ModifyAll.html?id=' . $ticket->id);
 
 $m->form_name('TicketModifyAll');
 $m->field(Owner => 'root');
+$m->field(TimeWorked => 120);
 $m->click('SubmitTicket');
+
+$m->text_contains('Owner changed from Nobody to root');
+$m->text_contains('Worked 2 hours (120 minutes)');
 
 $m->form_name('TicketModifyAll');
 is($m->value('Owner'), 'root', 'owner was successfully changed to root');
+is($m->value('TimeWorked'), 120, 'logged 2 hours');
 
 $m->get_ok($url . "/Ticket/ModifyAll.html?id=" . $ticket->id);
 
@@ -96,6 +101,18 @@ $m->text_contains(
     "root is already Requestor",
     'no duplicate watchers',
 );
+
+$m->get( $url . '/Ticket/ModifyAll.html?id=' . $ticket->id );
+$m->form_name('TicketModifyAll');
+$m->click('SubmitTicket');
+$m->content_lacks("That is already the current value", 'no spurious messages');
+
+$m->form_name('TicketModifyAll');
+$m->field(TimeWorked => 0);
+$m->click('SubmitTicket');
+$m->text_contains('Adjusted time worked by -120 minutes');
+$m->form_name('TicketModifyAll');
+is($m->value('TimeWorked'), "", 'no time worked');
 
 undef $m;
 done_testing;