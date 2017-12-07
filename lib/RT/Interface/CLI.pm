77715acfbb6ecd83ba58cc98bfb18a07da2a765f
Stefan Bühler <buehler@cert.uni-stuttgart.de>
2017-12-07 17:58:32 +0100

    don't export removed CleanEnv

--- lib/RT/Interface/CLI.pm
+++ lib/RT/Interface/CLI.pm
@@ -53,7 +53,7 @@ use warnings;
 use RT::Base;
 
 use base 'Exporter';
-our @EXPORT_OK = qw(CleanEnv GetCurrentUser debug loc Init);
+our @EXPORT_OK = qw(GetCurrentUser debug loc Init);
 
 =head1 NAME
 
