bestpractical/rt

commit 8862bd94d3ddc84ac858f36cff2a032e51fdf67c

Switch to using ->Atomic for all ticket updates via the UI
This reduces the chance of other deadlocks that may be lurking
elsewhere, by serializing all updates to a given ticket.  This
replaces an existing pattern of simply calling
`ApplyTransactionBatch`, introduced in b5aedb8d and bd456fc5.

lib/RT/Ticket.pm
@@ -1639,6 +1639,10 @@ usual commit step.  If the subroutine dies, this function will abort
 the transaction (unless it is already aborted or committed, per
 above), and will re-die with the error.
 
+This method should be used to lock, and operate atomically on, all
+ticket changes via the UI
+(e.g. L<RT::Interface::Web/ProcessTicketBasics>).
+
 =cut
 
 sub Atomic {

share/html/Search/Bulk.html
@@ -285,9 +285,12 @@ my @linkresults;
 $Tickets->RedoSearch();
 
 unless ( $ARGS{'AddMoreAttach'} ) {
+    $RT::Handle->BeginTransaction;
     while ( my $Ticket = $Tickets->Next ) {
         my $tid = $Ticket->id;
         next unless grep $tid == $_, @UpdateTicket;
+
+        $Ticket->LockForUpdate;
 
         #Update the links
         $ARGS{'id'} = $Ticket->id;
@@ -324,6 +327,7 @@ unless ( $ARGS{'AddMoreAttach'} ) {
 
         @results = ( @results, @tempresults );
     }
+    $RT::Handle->Commit;
 
     delete $session{'Attachments'}{ $ARGS{'Token'} };
 

share/html/SelfService/Display.html
@@ -122,22 +122,24 @@ if ( ($id[0]||'') eq 'new' ) {
 else {
     $Ticket = LoadTicket($ARGS{'id'});
 
-    push @results, ProcessUpdateMessage(
-        ARGSRef   => \%ARGS,
-        TicketObj => $Ticket
-    );
+    $Ticket->Atomic(sub{
+        push @results, ProcessUpdateMessage(
+            ARGSRef   => \%ARGS,
+            TicketObj => $Ticket
+        );
 
-    my @cfupdates = ProcessObjectCustomFieldUpdates(Object => $Ticket, ARGSRef => \%ARGS);
-    push (@results, @cfupdates);
+        my @cfupdates = ProcessObjectCustomFieldUpdates(Object => $Ticket, ARGSRef => \%ARGS);
+        push (@results, @cfupdates);
 
-    #Update the status
-    if (    ( defined $ARGS{'Status'} )
-        and $ARGS{'Status'}
-        and ( $ARGS{'Status'} ne $Ticket->Status ) )
-    {
-        my ($code, $msg) = $Ticket->SetStatus( $ARGS{'Status'} );
-        push @results, "$msg";
-    }
+        #Update the status
+        if (    ( defined $ARGS{'Status'} )
+            and $ARGS{'Status'}
+            and ( $ARGS{'Status'} ne $Ticket->Status ) )
+        {
+            my ($code, $msg) = $Ticket->SetStatus( $ARGS{'Status'} );
+            push @results, "$msg";
+        }
+    });
 
 }
 

share/html/Ticket/Display.html
@@ -160,12 +160,13 @@ if ($ARGS{'id'} eq 'new') {
 
     my $SkipProcessing;
 
-    $m->callback( CallbackName => 'BeforeProcessArguments',
-        TicketObj => $TicketObj,
-        ActionsRef => \@Actions, ARGSRef => \%ARGS,
-        SkipProcessing => \$SkipProcessing );
+    $TicketObj->Atomic(sub{
+        $m->callback( CallbackName => 'BeforeProcessArguments',
+            TicketObj => $TicketObj,
+            ActionsRef => \@Actions, ARGSRef => \%ARGS,
+            SkipProcessing => \$SkipProcessing );
+        return if $SkipProcessing;
 
-    if ( !$SkipProcessing ) {
         if ( defined $ARGS{'Action'} ) {
             if ($ARGS{'Action'} =~ /^(Steal|Delete|Take|SetTold)$/) {
                 my $action = $1;
@@ -174,11 +175,11 @@ if ($ARGS{'id'} eq 'new') {
             }
         }
 
-        $m->callback(CallbackName => 'ProcessArguments', 
-                Ticket => $TicketObj, 
-                ARGSRef => \%ARGS, 
+        $m->callback(CallbackName => 'ProcessArguments',
+                Ticket => $TicketObj,
+                ARGSRef => \%ARGS,
                 Actions => \@Actions);
-        
+
         push @Actions, ProcessUpdateMessage(
             ARGSRef   => \%ARGS,
             Actions   => \@Actions,
@@ -192,7 +193,8 @@ if ($ARGS{'id'} eq 'new') {
         push @Actions, ProcessTicketDates(   ARGSRef => \%ARGS, TicketObj => $TicketObj );
         push @Actions, ProcessObjectCustomFieldUpdates(ARGSRef => \%ARGS, TicketObj => $TicketObj );
         push @Actions, ProcessTicketReminders( ARGSRef => \%ARGS, TicketObj => $TicketObj );
-
+    });
+    if ( !$SkipProcessing ) {
         unless ($TicketObj->CurrentUserHasRight('ShowTicket')) {
             if (@Actions) {
                 Abort("A change was applied successfully, but you no longer have permissions to view the ticket", Actions => \@Actions);

share/html/Ticket/Modify.html
@@ -94,13 +94,13 @@ $m->callback( TicketObj => $TicketObj, CustomFields => $CustomFields, ARGSRef =>
 }
 
 unless ($skip_update) {
-    push @results, ProcessTicketBasics(TicketObj => $TicketObj, ARGSRef => \%ARGS);
-    push @results, ProcessTicketWatchers(TicketObj => $TicketObj, ARGSRef => \%ARGS);
-    push @results, ProcessObjectCustomFieldUpdates(Object => $TicketObj, ARGSRef => \%ARGS);
-    $m->callback( CallbackName => 'ProcessUpdates', TicketObj => $TicketObj,
-                  ARGSRef => \%ARGS, Results => \@results );
-
-    $TicketObj->ApplyTransactionBatch;
+    $TicketObj->Atomic(sub{
+        push @results, ProcessTicketBasics(TicketObj => $TicketObj, ARGSRef => \%ARGS);
+        push @results, ProcessTicketWatchers(TicketObj => $TicketObj, ARGSRef => \%ARGS);
+        push @results, ProcessObjectCustomFieldUpdates(Object => $TicketObj, ARGSRef => \%ARGS);
+        $m->callback( CallbackName => 'ProcessUpdates', TicketObj => $TicketObj,
+                      ARGSRef => \%ARGS, Results => \@results );
+    });
 
     MaybeRedirectForResults(
         Actions   => \@results,

share/html/Ticket/ModifyAll.html
@@ -191,14 +191,14 @@ if ( ref ($ARGS{'Owner'} )) {
 }
 
 unless ($skip_update or $OnlySearchForPeople or $OnlySearchForGroup or $ARGS{'AddMoreAttach'} ) {
-    push @results, ProcessTicketWatchers( TicketObj => $Ticket, ARGSRef => \%ARGS);
-    push @results, ProcessObjectCustomFieldUpdates( Object => $Ticket, ARGSRef => \%ARGS);
-    push @results, ProcessTicketDates( TicketObj => $Ticket, ARGSRef => \%ARGS);
-    push @results, ProcessUpdateMessage( TicketObj => $Ticket, ARGSRef=>\%ARGS );
-    push @results, ProcessTicketBasics( TicketObj => $Ticket, ARGSRef => \%ARGS );
-    push @results, ProcessTicketLinks( TicketObj => $Ticket, ARGSRef => \%ARGS);
-
-    $Ticket->ApplyTransactionBatch;
+    $Ticket->Atomic(sub {
+        push @results, ProcessTicketWatchers( TicketObj => $Ticket, ARGSRef => \%ARGS);
+        push @results, ProcessObjectCustomFieldUpdates( Object => $Ticket, ARGSRef => \%ARGS);
+        push @results, ProcessTicketDates( TicketObj => $Ticket, ARGSRef => \%ARGS);
+        push @results, ProcessUpdateMessage( TicketObj => $Ticket, ARGSRef=>\%ARGS );
+        push @results, ProcessTicketBasics( TicketObj => $Ticket, ARGSRef => \%ARGS );
+        push @results, ProcessTicketLinks( TicketObj => $Ticket, ARGSRef => \%ARGS);
+    });
 
     MaybeRedirectForResults(
         Actions   => \@results,

share/html/Ticket/ModifyDates.html
@@ -67,9 +67,10 @@
 my $TicketObj = LoadTicket($id);
 my @results;
 $m->callback( TicketObj => $TicketObj, ARGSRef => \%ARGS, results => \@results );
-push @results, ProcessTicketDates( TicketObj => $TicketObj, ARGSRef => \%ARGS);
-push @results, ProcessObjectCustomFieldUpdates(Object => $TicketObj, ARGSRef => \%ARGS);
-$TicketObj->ApplyTransactionBatch;
+$TicketObj->Atomic(sub{
+    push @results, ProcessTicketDates( TicketObj => $TicketObj, ARGSRef => \%ARGS);
+    push @results, ProcessObjectCustomFieldUpdates(Object => $TicketObj, ARGSRef => \%ARGS);
+});
 
 $TicketObj->CurrentUser->AddRecentlyViewedTicket($TicketObj);
 

share/html/Ticket/ModifyLinks.html
@@ -72,11 +72,12 @@
 <%INIT>
 my $Ticket = LoadTicket($id);
 
-my @results;  
-$m->callback( TicketObj => $Ticket, ARGSRef => \%ARGS, Results => \@results );
-push @results, ProcessTicketLinks( TicketObj => $Ticket, ARGSRef => \%ARGS );
-push @results, ProcessObjectCustomFieldUpdates( TicketObj => $Ticket, ARGSRef => \%ARGS );
-$Ticket->ApplyTransactionBatch;
+my @results;
+$Ticket->Atomic(sub{
+    $m->callback( TicketObj => $Ticket, ARGSRef => \%ARGS, Results => \@results );
+    push @results, ProcessTicketLinks( TicketObj => $Ticket, ARGSRef => \%ARGS );
+    push @results, ProcessObjectCustomFieldUpdates( TicketObj => $Ticket, ARGSRef => \%ARGS );
+});
 
 MaybeRedirectForResults(
     Actions     => \@results,

commit 66ed2a33f47f147d3796ef07a9910a5b3c93655d

Apply TransactionBatch at end of top ->Atomic call
8862bd94d introduced a regression, seen at least when calling
Ticket/Display.html?id=X&Action=YYY&Status=ZZZ. TransactionBatch scrips
are triggered for Owner change (Take here) but not for Status change.

All changes are wrapped inside an Atomic call. Action args is processed
first and trigger a $TicketObj->Take which calls ->SetOwner. This last
one wrap itself inside another Atomic call.

In ->Atomic, the first commited transaction trigger application of
TransactionBatch which itself forbid further execution through
->RanTransactionBatch flag.

This commit apply TransactionBatch only after end of all transactions.

It is also executed after txn commit to be consistent for all
transactions such as nested ones (which was old behaviour).

lib/RT/Ticket.pm
@@ -1783,8 +1783,8 @@ sub Atomic {
     }
 
     if ($RT::Handle->TransactionDepth == $depth) {
-        $self->ApplyTransactionBatch;
         $RT::Handle->Commit;
+        $self->ApplyTransactionBatch if ( $depth == 0 );
     }
 
     return $context ? @ret : $ret[0];