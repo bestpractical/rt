16a71b6b82c1bb79122b6aa5ea5dea5b78c19cdc
Craig Kaiser <craig@bestpractical.com>
2018-10-29 16:24:06 -0400

    Load Content rather than Attribute object for RecentlyViewedTickets
    Previously, if the RecentlyViewedTickets content somehow was set
    to undef, $content still evaluated to true because it was an
    RT::Attribute object. Calling the Content method then resulted in:
    
        Can't use an undefined value as an ARRAY reference
    
    This caused a 500 error for the end-user.
    
    Loading the content provides the intended true/false value and handles
    the empty content case.

--- lib/RT/User.pm
+++ lib/RT/User.pm
@@ -2081,9 +2081,14 @@ Returns a list of two-element (ticket id, timestamp) array references ordered by
 =cut
 
 sub RecentlyViewedTickets {
-    my $self = shift;
-    my $content = $self->FirstAttribute('RecentlyViewedTickets');
-    return $content ? @{$content->Content} : ();
+    my $self    = shift;
+
+    my $attribute = $self->FirstAttribute('RecentlyViewedTickets');
+    my $content;
+
+    $content = $attribute->Content if $attribute;
+
+    return $content ? @{$content} : ();
 }
 
 =head2 AddRecentlyViewedTicket TICKET
@@ -2102,7 +2107,11 @@ sub AddRecentlyViewedTicket {
     return unless $ticket->Id;
 
     my @recentTickets;
-    my $content = $self->FirstAttribute('RecentlyViewedTickets');
+    my $attribute = $self->FirstAttribute('RecentlyViewedTickets');
+    my $content;
+
+    $content = $attribute->Content if $attribute;
+
     $content = $content ? $content->Content : [];
     if (defined $content) {
         @recentTickets = @$content;
