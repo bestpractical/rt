6a7f36053f5b127bcfe2326b4722801f29cd8779
sunnavy <sunnavy@bestpractical.com>
2019-04-30 04:25:06 +0800

    Add chartjs-plugin-colorschemes.min.js 0.03 dependency

--- lib/RT/Interface/Web.pm
+++ lib/RT/Interface/Web.pm
@@ -142,6 +142,7 @@ sub JSFiles {
         fontawesome.min.js
         rights-inspector.js
         Chart.min.js
+        chartjs-plugin-colorschemes.min.js
         }, RT->Config->Get('JSFiles');
 }
 
