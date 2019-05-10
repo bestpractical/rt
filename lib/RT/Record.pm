e5291bb8969a5388bb3e91b7f84c8445308f7760
michel <michel@bestpractical.com>
2019-05-10 18:42:40 +0200

    Add a control to reverse the time order on the current ticket
    Added the option in the template.
    Passed the new parameter through the variousi layers of templates.
    Added an option to SortedTransactions to change the original order

--- lib/RT/Record.pm
+++ lib/RT/Record.pm
@@ -1729,13 +1729,20 @@ sub Transactions {
 Returns the result of L</Transactions> ordered per the
 I<OldestTransactionsFirst> preference/option.
 
+The second argument reverses the order if true, to 
+provide the "Reverse Transaction Order" feature.
+
 =cut
 
 sub SortedTransactions {
     my $self  = shift;
+    my $reverse_order = shift || 0;
+
     my $txns  = $self->Transactions;
-    my $order = RT->Config->Get("OldestTransactionsFirst", $self->CurrentUser)
-        ? 'ASC' : 'DESC';
+
+    # parens required because the priority of 'xor' is lower than the one of '='
+    my $order_param = ( RT->Config->Get("OldestTransactionsFirst", $self->CurrentUser) xor $reverse_order );
+    my $order = $order_param ? 'ASC' : 'DESC';
     $txns->OrderByCols(
         { FIELD => 'Created',   ORDER => $order },
         { FIELD => 'id',        ORDER => $order },
