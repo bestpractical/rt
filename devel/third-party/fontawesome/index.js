9e6d02a22338ad27d2e3617d8bb27b795d3b8059
Jim Brandt <jbrandt@bestpractical.com>
2019-09-26 16:43:00 -0400

    Add fontawesome SVG build docs and files

---
+++ devel/third-party/fontawesome/index.js
@@ -0,0 +1,43 @@
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+
+// Import solid weight icons
+import {
+    faCog as fasCog,
+    faEdit as fasEdit,
+    faBookmark as fasBookmark,
+} from '@fortawesome/free-solid-svg-icons'
+
+
+// Import regular weight icons
+import {
+    faEdit as farEdit,
+    faBookmark as farBookmark,
+    faClock as farClock,
+    faCalendarAlt as farCalendarAlt
+} from '@fortawesome/free-regular-svg-icons'
+
+/*
+// Import brand icons
+import {
+    faLinux as fabLinux
+} from '@fortawesome/free-brands-svg-icons';
+*/
+
+// Add icons to library
+library.add(
+    // Solid
+    fasCog,
+    fasEdit,
+    fasBookmark,
+    // Regular
+    farEdit,
+    farBookmark,
+    farClock,
+    farCalendarAlt
+    // Brands
+//    fabLinux
+)
+
+// Replace any existing <i> tags with <svg> and set up a MutationObserver to
+// continue doing this as the DOM changes.
+dom.watch()
