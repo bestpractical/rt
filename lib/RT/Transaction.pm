93ccb9844398f6eda63ac05d1c6a1b91d1db0827
Aaron Trevena <ast@bestpractical.com>
2020-03-20 15:56:42 +0000

    Schema updates for tracking db configuration changes in transactions

--- lib/RT/Transaction.pm
+++ lib/RT/Transaction.pm
@@ -2013,7 +2013,7 @@ sub _CoreAccessible {
         Type =>
                 {read => 1, write => 1, sql_type => 12, length => 20,  is_blob => 0,  is_numeric => 0,  type => 'varchar(20)', default => ''},
         Field =>
-                {read => 1, write => 1, sql_type => 12, length => 40,  is_blob => 0,  is_numeric => 0,  type => 'varchar(40)', default => ''},
+                {read => 1, write => 1, sql_type => 12, length => 255,  is_blob => 0,  is_numeric => 0,  type => 'varchar(255)', default => ''},
         OldValue =>
                 {read => 1, write => 1, sql_type => 12, length => 255,  is_blob => 0,  is_numeric => 0,  type => 'varchar(255)', default => ''},
         NewValue =>
