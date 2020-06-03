46053b0546ccf7eb8a08331a813b0b68b76d20ad
craig kaiser <craig@bestpractical.com>
2020-06-03 11:08:33 -0400

    'use' REST2 in web handler as PSGIWrap there needs it
    We do "require" RT::REST2 in RT::InitClasses, but the method is not
    called in installer mode. This is to fix the error in installer mode:
    
        Undefined subroutine &RT::REST2::PSGIWrap called
    
    Fixes: I#36547

--- lib/RT/Interface/Web/Handler.pm
+++ lib/RT/Interface/Web/Handler.pm
@@ -59,6 +59,7 @@ use HTML::Scrubber;
 use RT::Interface::Web;
 use RT::Interface::Web::Request;
 use RT::ObjectCustomFieldValues;
+use RT::REST2;
 use File::Path qw( rmtree );
 use File::Glob qw( bsd_glob );
 use File::Spec::Unix;
