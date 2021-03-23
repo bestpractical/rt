9b34a0f8e1f476606d43dce8775ca40fd40fe268
sunnavy <sunnavy@bestpractical.com>
2021-03-23 08:02:41 +0800

    Sync one-time checkboxes to text inputs in a consistent way
    The approach checkboxesToInput uses takes care of duplicates and empty
    values, it's good to use it for single checkboxToInput too.

--- share/static/js/util.js
+++ share/static/js/util.js
@@ -200,20 +200,23 @@ function checkAllObjects()
 function checkboxToInput(target,checkbox,val){    
     var tar = jQuery('#' + escapeCssSelector(target));
     var box = jQuery('#' + escapeCssSelector(checkbox));
+
+    var emails = jQuery.grep(tar.val().split(/,\s*/), function(email) {
+        return email.match(/\S/) ? true : false;
+    });
+
     if(box.prop('checked')){
-        if (tar.val()==''){
-            tar.val(val);
-        }
-        else{
-            tar.val( val+', '+ tar.val() );        
+        if ( emails.indexOf(val) == -1 ) {
+            emails.unshift(val);
         }
     }
     else{
-        tar.val(tar.val().replace(val+', ',''));
-        tar.val(tar.val().replace(val,''));
+        emails = jQuery.grep(emails, function(email) {
+            return email != val;
+        });
     }
     jQuery('#UpdateIgnoreAddressCheckboxes').val(true);
-    tar.change();
+    tar.val(emails.join(', ')).change();
 }
 
 function checkboxesToInput(target,checkboxes) {
