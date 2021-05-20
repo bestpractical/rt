d95fe2d665bb12ba6af73c1607b409aee856d3fa
sunnavy <sunnavy@bestpractical.com>
2021-05-21 04:01:39 +0800

    Show single custom role's name in the result message of adding members
    This is for asset custom roles, tickets don't have this issue as the
    result message was customized in RT::Ticket already.

--- lib/RT/Group.pm
+++ lib/RT/Group.pm
@@ -1045,7 +1045,7 @@ sub _AddMember {
     }
 
     return (1, $self->loc("[_1] set to [_2]",
-                          $self->loc($self->Name), $new_member_obj->Object->Name) )
+                          $self->Label, $new_member_obj->Object->Name) )
         if $self->SingleMemberRoleGroup;
 
     return ( 1, $self->loc("Member added: [_1]", $new_member_obj->Object->Name) );
