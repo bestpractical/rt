0e9fbc695c913172ec592a059ec9afdaac43a26a
Steven Burr <steve@bestpractical.com>
2021-06-21 14:52:02 -0400

    Allow clearing roles on bulk updates page
    The bulk update page currently allows one to specify specific
    principals to add/remove to/from ticket roles, but there is no
    way to completely clear all members from a role without knowing
    all of their identities in advance.
    
    This change adds a checkbox to optionally clear each core and/or
    custom role.

--- lib/RT/Interface/Web.pm
+++ lib/RT/Interface/Web.pm
@@ -3607,6 +3607,23 @@ sub ProcessTicketWatchers {
             push @results, $msg;
         }
 
+        # Clear all watchers in the simple style demanded by the bulk manipulator
+        elsif ( $key =~ /^Clear(Requestor|Cc|AdminCc|RT::CustomRole-\d+)$/ ) {
+            my ( $role_group, $msg ) = $Ticket->RoleGroup($1);
+            if ( $role_group ) {
+                while (my $group_member = $role_group->MembersObj->Next) {
+                    my ( $code, $msg ) = $Ticket->DeleteWatcher(
+                        PrincipalId => $group_member->MemberId,
+                        Type  => $1
+                    );
+                    push @results, $msg;
+                }
+            } else {
+                RT::Logger->error("Could not load RoleGroup for $1");
+                push @results, $msg;
+            }
+        }
+
         # Add new watchers by email address
         elsif ( ( $ARGSRef->{$key} || '' ) =~ /^(?:AdminCc|Cc|Requestor|RT::CustomRole-\d+)$/
             and $key =~ /^WatcherTypeEmail(\d*)$/ )
