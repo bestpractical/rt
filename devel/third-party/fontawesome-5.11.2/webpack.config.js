4dfda7ef22911e9ce4d547147e99347665a98142
sunnavy <sunnavy@bestpractical.com>
2021-06-28 22:54:11 +0800

    Drop obsolete babel-minify-webpack-plugin
    It seems unnecessary too as the generated fontawesome is already
    minified.

--- devel/third-party/fontawesome-5.11.2/webpack.config.js
+++ devel/third-party/fontawesome-5.11.2/webpack.config.js
@@ -1,13 +1,7 @@
-const BabelMinifyPlugin = require('babel-minify-webpack-plugin')
 module.exports = {
     mode: 'production',
     entry: './index.js',
     output: {
       filename: './fontawesome.js'
-    },
-    optimization: {
-      minimizer: [
-        new BabelMinifyPlugin()
-      ]
     }
 }
