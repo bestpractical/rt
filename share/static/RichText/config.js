5c0bfa959d07178caabcb8914d94b1dba840653f
Jim Brandt <jbrandt@bestpractical.com>
2021-09-09 11:30:53 -0400

    Allow div as a format tag for CKEditor
    The format_tags configuration was originally set to simplify the
    format menu in the editor, but excluding the div tag also caused
    the editor to not use it when showing quoted content, breaking
    the blockquote tags we add there.
    
    The default format_tags configuration adds only a few more
    format options, which doesn't really make the Format menu any less
    usable, so remove the custom configuration and use the default,
    which also restores div and fixes the blockquote issue.

--- share/static/RichText/config.js
+++ share/static/RichText/config.js
@@ -30,9 +30,6 @@ CKEDITOR.editorConfig = function( config ) {
 	// not needed in the Standard(s) toolbar.
 //	config.removeButtons = 'Underline,Subscript,Superscript';
 
-	// Set the most common block elements.
-	config.format_tags = 'p;h1;h2;h3;pre';
-
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
     if ( RT.Config.WebDefaultStylesheet.match(/dark/) ) {
