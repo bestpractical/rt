41536e799a022c40f298c1116ebc525a4851fb25
sunnavy <sunnavy@bestpractical.com>
2021-09-17 04:37:43 +0800

    Add clipboard.js to RT web

--- lib/RT/Interface/Web.pm
+++ lib/RT/Interface/Web.pm
@@ -143,6 +143,7 @@ sub JSFiles {
         Chart.min.js
         chartjs-plugin-colorschemes.min.js
         jquery.jgrowl.min.js
+        clipboard.min.js
         }, RT->Config->Get('JSFiles');
 }
 
