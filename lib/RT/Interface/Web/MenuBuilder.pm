4c57cb255ce873090e7abaea7bbd269829fc1585
Jim Brandt <jbrandt@bestpractical.com>
2023-04-27 10:13:28 -0400

    Render menu link icon as inline svg rather than CSS class
    When loading search results, the page menu would load
    without the link icon, all search results would load,
    then the JS would run and the link icon would jump into
    the menu. At the top of the screen, it was very
    noticeable.
    
    Convert to inline svg to load when the page loads and
    still retain the ability to style the icon. The
    new link svg is from Bootstrap icons.

--- lib/RT/Interface/Web/MenuBuilder.pm
+++ lib/RT/Interface/Web/MenuBuilder.pm
@@ -710,7 +710,7 @@ sub BuildMainNav {
                 $current_search_menu->child(
                     'permalink',
                     sort_order   => 2, # Put it between "Edit Search" and "Advanced"
-                    title        => '<span class="fas fa-link"></span>',
+                    title        => GetSVGImage('link-45deg'),
                     escape_title => 0,
                     class        => 'permalink',
                     path         => "$request_path?sc=$short_query{sc}",
@@ -1759,6 +1759,39 @@ sub BuildSelfServiceNav {
 
     # due to historical reasons of always having been in /Elements/Tabs
     $HTML::Mason::Commands::m->callback( CallbackName => 'SelfService', Path => $request_path, ARGSRef => \%args, CallbackPage => '/Elements/Tabs' );
+}
+
+# If you want to apply CSS, like colors, to SVG icons, they must be inlined
+# with a svg tag. Currently this is the only way to allow regular CSS
+# cascading to work, so this is a better option than using <img> tags
+# and not being able to match colors for different themes, for
+# example. This has some drawbacks, like making each page a bit
+# heavier and not being able to cache the icons. We accept this to
+# be able to use SVGs.
+
+# Inline content also avoids seeing images jump onto the page last
+# when added via CSS+JS as provided with some icon libraries.
+
+# This is a single place to store and manage the SVG images and icons
+# rather than pasting svg markup in many different places in templates.
+
+our %SVG_IMAGES = (
+    'link-45deg' => '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-link-45deg" viewBox="0 0 16 16"><path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z"/><path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z"/>
+</svg>'
+);
+
+sub GetSVGImage {
+    my $image_name = shift;
+
+    my $svg;
+    if ( exists $SVG_IMAGES{$image_name} ) {
+        $svg = $SVG_IMAGES{$image_name};
+    }
+    else {
+        $svg = "<span class=\"error\">SVG $image_name not found</span>";
+    }
+
+    return $svg;
 }
 
 1;
