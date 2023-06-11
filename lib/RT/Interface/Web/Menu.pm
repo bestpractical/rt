f86b72470c493acc690f1b14c00cab17907969b4
Ruslan Zakirov <ruz@bestpractical.com>
2023-06-11 14:24:42 +0300

    Remove trailing artefacts before adding query part
    Old code could add query part, eg '?foo=bar' to $base_path, so
    removing trailing 'index.html' and '/' would never work.
    
    So remove trailing parts first and only after that add query string
    if it's not empty.
    
    It only influence whether child is marked as active or not.
    
    It is hard to see if it affects production code or not, but new
    code is more correct than the old one.

--- lib/RT/Interface/Web/Menu.pm
+++ lib/RT/Interface/Web/Menu.pm
@@ -237,12 +237,13 @@ sub child {
         # Activate it
         if ( defined $path and length $path ) {
             my $base_path = $HTML::Mason::Commands::r->path_info;
-            my $query     = URI->new(RT::Interface::Web::RequestENV('REQUEST_URI'))->query;
             $base_path =~ s!/+!/!g;
-            $base_path .= "?$query" if defined $query and length $query;
-
             $base_path =~ s/index\.html$//;
             $base_path =~ s/\/+$//;
+
+            my $query = URI->new(RT::Interface::Web::RequestENV('REQUEST_URI'))->query;
+            $base_path .= "?$query" if defined $query and length $query;
+
             $path =~ s/index\.html$//;
             $path =~ s/\/+$//;
 
