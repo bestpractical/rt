9ed20c3425c0ececc51ccacaa34eee15ef444112
Nick Reilingh <nreiling@bard.edu>
2023-06-23 20:29:18 -0400

    Correct POD headers for CustomField methods

--- lib/RT/CustomField.pm
+++ lib/RT/CustomField.pm
@@ -723,7 +723,7 @@ sub DeleteValue {
     return ($ok, $self->loc("Custom field value deleted"));
 }
 
-=head2 ValidateValue Value
+=head2 ValidateValue VALUE
 
 Make sure that the supplied value is valid
 
@@ -736,7 +736,7 @@ sub ValidateValue {
     return $ret;
 }
 
-=head2 ValidateQueue Queue
+=head2 ValidateName NAME
 
 Make sure that the name specified is valid
 
@@ -751,7 +751,7 @@ sub ValidateName {
     return $self->SUPER::ValidateName($value);
 }
 
-=head2 ValidateQueue Queue
+=head2 ValidateQueue QUEUE
 
 Make sure that the queue specified is a valid queue name
 
