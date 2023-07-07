38f543b32db1523806a27a7abc39b3835b7b4027
sunnavy <sunnavy@bestpractical.com>
2023-07-07 23:46:01 +0800

    Reload scrubber rules for current process that changes configs
    In 19327d95ba we reloaded scrubber rules for all the other processes.
    This commit completes it to make the same change to the current process.

--- lib/RT/Config.pm
+++ lib/RT/Config.pm
@@ -2987,6 +2987,7 @@ sub ApplyConfigChangeToAllServerProcesses {
 
     # first apply locally
     $self->LoadConfigFromDatabase();
+    $HTML::Mason::Commands::ReloadScrubber = 1;
     $self->PostLoadCheck;
 
     # then notify other servers
