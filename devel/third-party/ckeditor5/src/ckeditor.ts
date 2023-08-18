2d1cbafda62178334716ae5b344a56628fd1206a
sunnavy <sunnavy@bestpractical.com>
2023-08-18 15:27:57 -0400

    Upgrade ckeditor5 to 40.2.0
    This is based on current doc of customizing builds:
    
        https://ckeditor.com/docs/ckeditor5/latest/installation/getting-started/quick-start-other.html#customizing-builds

---
+++ devel/third-party/ckeditor5/src/ckeditor.ts
@@ -0,0 +1,133 @@
+/**
+ * @license Copyright (c) 2014-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
+
+import { Alignment } from '@ckeditor/ckeditor5-alignment';
+import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
+import {
+	Bold,
+	Code,
+	Italic,
+	Strikethrough,
+	Subscript,
+	Superscript
+} from '@ckeditor/ckeditor5-basic-styles';
+import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
+import { CodeBlock } from '@ckeditor/ckeditor5-code-block';
+import type { EditorConfig } from '@ckeditor/ckeditor5-core';
+import { Essentials } from '@ckeditor/ckeditor5-essentials';
+import { FontBackgroundColor, FontColor, FontFamily, FontSize } from '@ckeditor/ckeditor5-font';
+import { Heading } from '@ckeditor/ckeditor5-heading';
+import {
+	Image,
+	ImageCaption,
+	ImageStyle,
+	ImageToolbar,
+	ImageUpload
+} from '@ckeditor/ckeditor5-image';
+import { Indent, IndentBlock } from '@ckeditor/ckeditor5-indent';
+import { Link } from '@ckeditor/ckeditor5-link';
+import { List, TodoList } from '@ckeditor/ckeditor5-list';
+import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed';
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
+import { SourceEditing } from '@ckeditor/ckeditor5-source-editing';
+import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
+import { TextTransformation } from '@ckeditor/ckeditor5-typing';
+import { Undo } from '@ckeditor/ckeditor5-undo';
+import { Base64UploadAdapter } from '@ckeditor/ckeditor5-upload';
+
+// You can read more about extending the build with additional plugins in the "Installing plugins" guide.
+// See https://ckeditor.com/docs/ckeditor5/latest/installation/plugins/installing-plugins.html for details.
+
+class Editor extends ClassicEditor {
+	public static override builtinPlugins = [
+		Alignment,
+		Autoformat,
+		Base64UploadAdapter,
+		BlockQuote,
+		Bold,
+		Code,
+		CodeBlock,
+		Essentials,
+		FontBackgroundColor,
+		FontColor,
+		FontFamily,
+		FontSize,
+		Heading,
+		Image,
+		ImageCaption,
+		ImageStyle,
+		ImageToolbar,
+		ImageUpload,
+		Indent,
+		IndentBlock,
+		Italic,
+		Link,
+		List,
+		MediaEmbed,
+		Paragraph,
+		PasteFromOffice,
+		SourceEditing,
+		Strikethrough,
+		Subscript,
+		Superscript,
+		Table,
+		TableToolbar,
+		TextTransformation,
+		TodoList,
+		Undo
+	];
+
+	public static override defaultConfig: EditorConfig = {
+		toolbar: {
+			items: [
+            'undo',                'redo',        '|',            'heading',
+            '|',                   'fontfamily',  'fontsize',     'fontColor',
+            'fontBackgroundColor', '|',           'bold',         'italic',
+            'strikethrough',       'subscript',   'superscript',  '|',
+            'link',                'imageUpload', 'mediaEmbed',   '|',
+            'code',                'blockQuote',  'codeBlock',    '|',
+            'insertTable',         'alignment',   '|',            'bulletedList',
+            'numberedList',        'todoList',    '|',            'outdent',
+            'indent',              '|',           'sourceEditing'
+			]
+		},
+		language: 'en',
+		image: {
+			toolbar: [
+				'imageTextAlternative',
+				'toggleImageCaption',
+				'imageStyle:inline',
+				'imageStyle:block',
+				'imageStyle:side'
+			]
+		},
+		table: {
+			contentToolbar: [
+				'tableColumn',
+				'tableRow',
+				'mergeTableCells'
+			]
+		},
+		mediaEmbed: {
+			// Remove providers that are not previewable.
+		    removeProviders: [ 'instagram', 'twitter', 'googleMaps', 'flickr', 'facebook' ],
+		    previewsInData: true
+		},
+		ui: {
+		    poweredBy: {
+		        position: 'border',
+		        side: 'right',
+		        label: null,
+		        verticalOffset: 5,
+		        horizontalOffset: 5
+		    }
+		}
+	};
+}
+
+export default Editor;
