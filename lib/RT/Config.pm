3d1d2ab5f5d4d42c519c4e16456e52c1d24470bc
Ronaldo Richieri <ronaldo@bestpractical.com>
2024-01-02 10:37:58 -0300

    Add option to set number of rows in  dashboard subscriptions
    In some situations, it is desirable to limit the number of rows
    returned by a dashboard subscriptions.  This patch adds a new
    option to the dashboard subscriptions to allow the administrator
    to change the available options for the number of rows returned.

--- lib/RT/Config.pm
+++ lib/RT/Config.pm
@@ -1086,6 +1086,9 @@ our %META;
     },
     ReferrerWhitelist => { Type => 'ARRAY' },
     EmailDashboardLanguageOrder  => { Type => 'ARRAY' },
+    EmailDashboardRows => {
+        Type    => 'ARRAY',
+    },
     CustomFieldValuesCanonicalizers => { Type => 'ARRAY' },
     CustomFieldValuesValidations => {
         Type => 'ARRAY',
