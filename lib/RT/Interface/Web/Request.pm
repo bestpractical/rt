2d1276c7253b07b84f4f0c4bd1e7e1ec56144b05
sunnavy <sunnavy@bestpractical.com>
2024-07-13 11:00:04 -0400

    Support to customize widgets on asset display

--- lib/RT/Interface/Web/Request.pm
+++ lib/RT/Interface/Web/Request.pm
@@ -72,6 +72,16 @@ my %deprecated = (
             Instead => 'BeforeWidget in /Ticket/Widgets/History',
         },
     },
+    '/Asset/Display.html' => {
+        AfterShowSummary => {
+            Remove  => 6.2,
+            Instead => 'AfterWidget in /Asset/Widgets/Summary',
+        },
+        AfterShowHistory => {
+            Remove  => 6.2,
+            Instead => 'AfterWidget in /Asset/Widgets/History',
+        },
+    },
 );
 
 sub new {
