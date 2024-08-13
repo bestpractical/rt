99feecb15afc5c96686e3d77c190fcf7ef842c69
Jim Brandt <jbrandt@bestpractical.com>
2024-08-13 16:00:52 -0400

    Create new Times widget for time metadata

--- lib/RT/CustomField.pm
+++ lib/RT/CustomField.pm
@@ -228,7 +228,7 @@ __PACKAGE__->RegisterLookupType( 'RT::Queue'  => "Queues", );                   
 __PACKAGE__->RegisterLookupType( 'RT::Group' => "Groups", );                          #loc
 
 __PACKAGE__->RegisterBuiltInGroupings(
-    'RT::Ticket'    => [ qw(Basics Dates Links People QuickCreate) ],
+    'RT::Ticket'    => [ qw(Basics Dates Links People QuickCreate Times) ],
     'RT::User'      => [ 'Identity', 'Access control', 'Location', 'Phones' ],
     'RT::Group'     => [ 'Basics' ],
 );
