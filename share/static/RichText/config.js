06f15b4fa08614a8ead82c64e59ca9815b9b76a6
Craig Kaiser <craig@bestpractical.com>
2024-10-17 09:31:48 -0400

    Update CKEditor to include font plugin

--- share/static/RichText/config.js
+++ share/static/RichText/config.js
@@ -1,5 +1,5 @@
 /**
- * @license Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.
+ * @license Copyright (c) 2003-2024, CKSource Holding sp. z o.o. All rights reserved.
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
@@ -8,29 +8,31 @@ CKEDITOR.editorConfig = function( config ) {
 	// For complete reference see:
 	// https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
 
-    	config.toolbarGroups = [
-    		{ name: 'styles', groups: [ 'styles' ] },
-    		{ name: 'colors', groups: [ 'colors' ] },
-    		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-    		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
-    		{ name: 'links', groups: [ 'links' ] },
-    		{ name: 'insert', groups: [ 'insert' ] },
-    		{ name: 'forms', groups: [ 'forms' ] },
-    		{ name: 'tools', groups: [ 'tools' ] },
-    		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
-    		{ name: 'others', groups: [ 'others' ] },
-    		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-    		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
-    		{ name: 'about', groups: [ 'about' ] }
-    	];
+	// The toolbar groups arrangement, optimized for two toolbar rows.
+	config.toolbarGroups = [
+		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+		{ name: 'links' },
+		{ name: 'insert' },
+		{ name: 'forms' },
+		{ name: 'tools' },
+		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'others' },
+		'/',
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+		{ name: 'styles' },
+		{ name: 'colors' },
+		{ name: 'about' }
+	];
 
-    	config.removeButtons = 'Underline,Subscript,Superscript,About,Link,Image,HorizontalRule,SpecialChar,Source,DocProps,Unlink,Anchor,Strike,Cut,Copy,Outdent,Indent';
+	// Remove some buttons provided by the standard plugins, which are
+	// not needed in the Standard(s) toolbar.
+	config.removeButtons = 'Underline,Subscript,Superscript';
+
+	// Set the most common block elements.
+	config.format_tags = 'p;h1;h2;h3;pre';
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
-    if ( RT.Config.WebDefaultStylesheet.match(/dark/) ) {
-        config.contentsCss = [ RT.Config.WebPath + '/static/RichText/contents.css', RT.Config.WebPath + '/static/RichText/contents-dark.css' ];
-    }
-
-    config.disableNativeSpellChecker = false;
 };
