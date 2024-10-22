79ea2c6700081bb19d474c3603a52a3af74a51d4
Craig Kaiser <craig@bestpractical.com>
2024-10-22 16:20:48 -0400

    Add support for writing REST2 test results to open API schema
        perl devel/tools/rt-json-to-open-api

--- lib/RT/Test/REST2.pm
+++ lib/RT/Test/REST2.pm
@@ -53,6 +53,13 @@ use warnings;
 
 use base 'RT::Test';
 use Test::WWW::Mechanize::PSGI;
+use JSON qw( to_json decode_json);
+use URI;
+use File::Slurp;
+use Try::Tiny;
+
+# Store requests
+my @requests_log;
 
 sub import {
     my $class = shift;
@@ -104,13 +111,140 @@ sub mech { RT::Test::REST2::Mechanize->new }
             Name => 'test',
             Password => 'password',
             EmailAddress => 'test@rt.example',
-            Privileged => 1,
+            Privileged   => 1,
         );
         return $u;
     }
+
+    sub normalize_url {
+        my ($url) = @_;
+
+        # Prepend "http://" if no scheme is present
+        $url = "http://$url" unless $url =~ m{^https?://};
+
+        my $uri = URI->new($url);
+
+        # Normalize any path segments that contain hard-coded digits
+        my $path = $uri->path;
+
+        # Replace any hard-coded digit in the path with {id}
+        $path =~ s{(?<=/)\d+(?=/|$)}{\{id\}}g;
+
+        # Return the normalized URL as a string
+        return $path;
+    }
+
+    # Method to export requests log to OpenAPI YAML format
+    sub export_requests_to_yaml {
+        require YAML::XS;
+
+        my $log_file = 'requests_log.yaml';    # Path to the request log file
+        my %unique_endpoints;
+
+        # Process existing requests from the log file if it exists
+        if ( -e $log_file && -s $log_file > 0 ) {
+            my $existing_content = read_file($log_file);
+            try {
+                my $existing_requests = YAML::XS::Load($existing_content);
+
+                # Populate %unique_endpoints with existing requests
+                foreach my $endpoint (@$existing_requests) {
+                    my $key = $endpoint->{method} . '|' . $endpoint->{url};
+
+                    $unique_endpoints{$key} = {
+                        method     => $endpoint->{method},
+                        url        => $endpoint->{url},
+                        parameters => $endpoint->{parameters},
+                        headers    => $endpoint->{headers},
+                        query      => $endpoint->{query},
+                        responses  => $endpoint->{responses}
+                    };
+                }
+            };
+
+            # Catch and warn about any YAML errors
+            if ($@) {
+                warn
+                    "Warning: Could not decode existing YAML in the log file: $@";
+            }
+        }
+
+        foreach my $req (@requests_log) {
+            my $normalized_endpoint = normalize_url( $req->{url} );
+            $req->{url} = $normalized_endpoint;
+
+            # Use the HTTP method and normalized endpoint as a unique key
+            my $key = $req->{method} . '|' . $normalized_endpoint;
+
+            # Handle non-GET request parameters (e.g., POST body)
+            my $params = try {
+                if ( defined $req->{params} && !ref $req->{params} ) {
+                    decode_json( $req->{params} );
+                } else {
+                    $req->{params} || {};
+                }
+            } catch {
+                $req->{params} || {};
+            };
+
+            # If the endpoint hasn't been recorded before, initialize it
+            if ( !exists $unique_endpoints{$key} ) {
+                $unique_endpoints{$key} = {
+                    method     => $req->{method},
+                    url        => $normalized_endpoint,
+                    parameters => $params,
+                    query      => $req->{query},
+                    headers    => $req->{headers},
+                    responses => {}, # Use a hash to store unique status codes
+                };
+            } else {
+                if ( ref $params eq 'HASH' ) {
+                    if (  !exists $unique_endpoints{$key}->{parameters}
+                        || ref $unique_endpoints{$key}->{parameters} eq
+                        'HASH' )
+                    {
+
+                        # Merge new parameters with existing ones
+                        foreach my $param_key ( keys %{$params} ) {
+
+                            # If the parameter doesn't already exist, add it
+                            if ( !exists $unique_endpoints{$key}->{parameters}
+                                ->{$param_key} )
+                            {
+                                $unique_endpoints{$key}->{parameters}
+                                    ->{$param_key} = $params->{$param_key};
+                            }
+                        }
+                    }
+                }
+            }
+
+            # Log the response
+            $unique_endpoints{$key}->{responses}->{ $req->{response}->{code} }
+                = {
+                status  => $req->{response}->{code},
+                content => $req->{response}->{content},
+                headers => $req->{response}->{headers},
+                };
+        }
+
+        # Convert the unique endpoints data to YAML using YAML::XS
+        my @unique_requests = values %unique_endpoints;
+        my $yaml_output     = YAML::XS::Dump( \@unique_requests );
+
+        # Write the updated data back to the log file
+        open my $fh, '>', $log_file or die "Could not open '$log_file': $!";
+        print $fh $yaml_output;
+        close $fh;
+
+        return
+            $yaml_output;  # Optionally return the YAML output for further use
+    }
+
 }
 
 {
+
     package RT::Test::REST2::Mechanize;
     use parent 'Test::WWW::Mechanize::PSGI';
 
@@ -119,7 +253,7 @@ sub mech { RT::Test::REST2::Mechanize->new }
 
     sub new {
         my $class = shift;
-        my %args = (
+       my %args = (
             app => RT::REST2->PSGIWrap(sub { die "Requested non-REST path" }),
             @_,
         );
@@ -171,8 +305,105 @@ sub mech { RT::Test::REST2::Mechanize->new }
         Test::More::like($res->header('content-type'),
             qr{^application/json(?:; charset="?utf-8"?)?$});
 
-        return $json->decode($res->content);
+        return $json->decode( $res->content );
     }
+
+    # Generalized method to handle HTTP requests and logging
+    sub request_with_logging {
+        my ( $self, $method, $url, @params ) = @_;
+
+        # Dispatch table to map methods to the corresponding SUPER methods
+        my %http_methods = (
+            get    => sub { $self->SUPER::get( $url, @params ) },
+            post   => sub { $self->SUPER::post( $url, @params ) },
+            put    => sub { $self->SUPER::put( $url, @params ) },
+            delete => sub { $self->SUPER::delete( $url, @params ) },
+        );
+
+        # Extract query parameters if the method is GET
+        my %query_params;
+        if ( $method eq 'get' && $url =~ /\?(.*)$/ ) {
+            my $query_string = $1;
+            my @pairs        = split /&/, $query_string;
+            foreach my $pair (@pairs) {
+                my ( $key, $value ) = split /=/, $pair;
+                $query_params{$key} = $value;
+            }
+        }
+
+        # Perform the request using the appropriate method
+        my $response = $http_methods{$method}->();
+
+# Handle different cases where request parameters differ (like POST with Content)
+        my $logged_params;
+        my %params_hash;
+
+        for ( my $i = 0; $i < @params; $i++ ) {
+            my $key = $params[$i];
+
+            if ( ref $key eq 'HASH' ) {
+
+                # If it's a hash reference, store it as-is in the hash
+                $params_hash{"Data"} = $key;
+            } elsif ( ref $key eq 'ARRAY' ) {
+
+                # If it's an array reference, process it as a special case
+                my $sub_array = $params[ $i + 1 ];
+                $params_hash{$key} = $sub_array;
+                $i++;    # Move the index forward to skip the next element
+            } else {
+
+                # Assume this is a scalar key, so pair it with the next value
+                $params_hash{$key} = $params[ $i + 1 ];
+                $i++
+                    ; # Move the index forward to skip the value we just paired
+            }
+        }
+
+        my $content = $params_hash{Content};
+
+        # If we need to remove content
+        delete $params_hash{Content};    # Separate headers if any
+
+        # Log both the request and the response details together
+        push @requests_log, {
+            method  => uc($method),
+            url     => $url,
+            query   => \%query_params, # Log query parameters for GET requests
+            params  => $content,
+            headers => \%params_hash,
+            response => {
+                code    => $response->code,               # HTTP status code
+                content => $response->decoded_content,    # Response content
+                headers => $response
+                    ->headers_as_string,    # Response headers as a string
+            }
+        };
+
+        return $response;    # Return the response after logging
+    }
+
+    # Wrapper methods for each HTTP verb
+    sub get {
+        my ( $self, $url, @params ) = @_;
+        return $self->request_with_logging( 'get', $url, @params );
+    }
+
+    sub post {
+        my ( $self, $url, @params ) = @_;
+        return $self->request_with_logging( 'post', $url, @params );
+    }
+
+    sub put {
+        my ( $self, $url, @params ) = @_;
+        return $self->request_with_logging( 'put', $url, @params );
+    }
+
+    sub delete {
+        my ( $self, $url, @params ) = @_;
+        return $self->request_with_logging( 'delete', $url, @params );
+    }
+
 }
 
 1;
