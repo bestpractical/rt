ab4f0c5f1da082e7db19b03579817939e152fdd1
Brad Embree <brad@bestpractical.com>
2024-12-02 14:57:33 -0800

    Add time tracking fields metadata

--- lib/RT/Transactions.pm
+++ lib/RT/Transactions.pm
@@ -185,6 +185,9 @@ our %FIELD_METADATA = (
     Data          => ['STRING'],                 #loc_left_pair
     Created       => [ 'DATE' => 'Created' ],    #loc_left_pair
 
+    TimeWorker     => [ 'ENUM' => 'User' ],              #loc_left_pair
+    TimeWorkedDate => [ 'DATE' => 'TimeWorkedDate' ],    #loc_left_pair
+
     Content     => ['ATTACHCONTENT'],            #loc_left_pair
     ContentType => ['ATTACHFIELD'],              #loc_left_pair
     Filename    => ['ATTACHFIELD'],              #loc_left_pair
