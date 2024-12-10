bestpractical/rt

commit 73bddfc5aff96c5d54808161e51c490d088e964d

Align query builder inputs after label/value changes

share/html/Search/Elements/ConditionRow
@@ -46,13 +46,13 @@
 %#
 %# END BPS TAGGED BLOCK }}}
 <div class="row mt-2 <% $Condition->{Class} || '' %>">
-  <div class="label col-4">
+  <div class="col-4">
     <% $handle_block->( $Condition->{'Field'}, $Condition->{'Name'} .'Field' ) |n %>
   </div>
   <div class="col-2 operator">
     <% $handle_block->( $Condition->{'Op'}, $Condition->{'Name'} .'Op') |n %>
   </div>
-  <div class="value col-6">
+  <div class="col-6">
     <% $handle_block->( $Condition->{'Value'}, 'ValueOf'. $Condition->{'Name'} ) |n %>
   </div>
 </div>

share/html/Search/Elements/PickCriteria
@@ -69,8 +69,8 @@
 % }
 
 <div class="row mt-2">
-  <div class="label col-3"><&|/l&>Aggregator</&></div>
-  <div class="col-9 operator"><& SelectAndOr, Name => "AndOr" &></div>
+  <div class="pt-1 col-4"><&|/l&>Aggregator</&></div>
+  <div class="col-8 operator"><& SelectAndOr, Name => "AndOr" &></div>
 </div>
 
 </&>

share/html/Search/Elements/SelectPersonType
@@ -45,7 +45,7 @@
 %# those contributions and any derivatives thereof.
 %#
 %# END BPS TAGGED BLOCK }}}
-<div class="row mt-2 my-0">
+<div class="row my-0">
   <div class="col-<% $Suffix ? 12 : 6 %>">
 
 <select id="<%$Name%>" name="<%$Name%>" class="form-control selectpicker">

commit 7df86b79b7d3ac720e5e3d8c872c045018ccef71

Fix custom field select condition rows in query builder
We dropped label/value css classes in 73bddfc5af, which broke cf condition
rows.

Here we add specific rt-search-... classes instead to target elements easily
while also avoids unwanted style changes.

share/html/Search/Elements/ConditionRow
@@ -46,13 +46,13 @@
 %#
 %# END BPS TAGGED BLOCK }}}
 <div class="row mt-2 <% $Condition->{Class} || '' %>">
-  <div class="col-4">
+  <div class="col-4 rt-search-name">
     <% $handle_block->( $Condition->{'Field'}, $Condition->{'Name'} .'Field' ) |n %>
   </div>
-  <div class="col-2 operator">
+  <div class="col-2 rt-search-operator">
     <% $handle_block->( $Condition->{'Op'}, $Condition->{'Name'} .'Op') |n %>
   </div>
-  <div class="col-6">
+  <div class="col-6 rt-search-value">
     <% $handle_block->( $Condition->{'Value'}, 'ValueOf'. $Condition->{'Name'} ) |n %>
   </div>
 </div>

share/static/js/util.js
@@ -1002,8 +1002,8 @@ htmx.onLoad(function(elt) {
         var val = jQuery(this).val();
 
         var new_operator = form.find(':input[name="' + val + 'Op"]:first').clone();
-        row.children('div.operator').children().remove();
-        row.children('div.operator').append(new_operator);
+        row.children('div.rt-search-operator').children().remove();
+        row.children('div.rt-search-operator').append(new_operator);
 
         var new_value = form.find(':input[name="ValueOf' + val + '"]:first');
         if ( new_value.hasClass('ui-autocomplete-input') ) {
@@ -1016,8 +1016,8 @@ htmx.onLoad(function(elt) {
         }
 
         new_value.attr('id', null);
-        row.children('div.value').children().remove();
-        row.children('div.value').append(new_value);
+        row.children('div.rt-search-value').children().remove();
+        row.children('div.rt-search-value').append(new_value);
         if ( new_value.hasClass('datepicker') ) {
             new_value.removeClass('hasDatepicker');
             initDatePicker(row);