429384d0155136dc5bdefa7aed7f3b7c6e87125b
sunnavy <sunnavy@bestpractical.com>
2025-01-02 16:02:43 -0500

    Drop unnecessary jQuery UI

--- lib/RT/Interface/Web.pm
+++ lib/RT/Interface/Web.pm
@@ -125,9 +125,6 @@ sub JSFiles {
         htmx.min.js
         jquery-3.6.0.min.js
         jquery_noconflict.js
-        jquery-ui.min.js
-        jquery-ui-timepicker-addon.js
-        jquery-ui-patch-datepicker.js
         tom-select.complete.min.js
         bootstrap.bundle.min.js
         bootstrap-combobox.js
