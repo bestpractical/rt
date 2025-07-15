88ffff24d2b3f4f1e869363d438dad2cf4b36e4b
Jim Brandt <jbrandt@bestpractical.com>
2025-07-15 09:36:49 -0400

    Fix ShowSummary template compilation errors and add deprecation test
    The deprecated ShowSummary template had compilation errors due to missing
    $dates_url and $links_url variables. Fixed these to ensure the template
    can still be called by end users who haven't migrated yet.
    
    Add a test to confirm the deprecation message is properly shown.
    It can be removed along with the code in 6.2.

---
+++ t/data/plugins/Deprecated/lib/Deprecated.pm
@@ -0,0 +1,21 @@
+package Deprecated;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Deprecated - Test plugin for deprecated code testing
+
+=head1 DESCRIPTION
+
+This test plugin provides Mason templates for testing deprecated RT components.
+It should be removed when the deprecated code is removed.
+
+=cut
