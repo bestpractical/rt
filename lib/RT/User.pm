26252c6627c4492c031320815c89cc0babad4f08
Jim Brandt <jbrandt@bestpractical.com>
2026-04-30 14:07:57 -0400

    Update AuthToken access configuration to restrict read and write
    In the RT web UI, SuperUsers have no way to read the RSS AuthToken
    of a user, so it's not necessary to provide it via any other
    interface. Update the read flag to 0 to enforce this restriction.
    
    Internally, RT generates the AuthToken value that is set. It should
    never be set by a caller from the published method, so remove it from
    the standard list of writable methods in RT::User.

--- lib/RT/User.pm
+++ lib/RT/User.pm
@@ -96,6 +96,7 @@ sub _OverlayAccessible {
 
           Name                  => { public => 1,  admin => 1 },    # loc_left_pair
           Password              => { read   => 0 },
+          AuthToken             => { read   => 0 },
           EmailAddress          => { public => 1 },                 # loc_left_pair
           Organization          => { public => 1,  admin => 1 },    # loc_left_pair
           RealName              => { public => 1 },                 # loc_left_pair
@@ -109,6 +110,15 @@ sub _OverlayAccessible {
           Image                 => { public => 1 },                 # loc_left_pair
           ImageContentType      => { public => 1 },                 # loc_left_pair
     }
+}
+
+# AuthToken is settable internally (GenerateAuthToken / GenerateAuthString /
+# SetCanonicalUserInfo go through SetAuthToken), but no client-facing entry
+# point should let a caller write it directly. Drop it from the allow-list
+# used by REST 2 update_record, the web UI autocreate path, and rt-config.
+sub WritableAttributes {
+    my $self = shift;
+    return grep { $_ ne 'AuthToken' } $self->SUPER::WritableAttributes(@_);
 }
 
 
